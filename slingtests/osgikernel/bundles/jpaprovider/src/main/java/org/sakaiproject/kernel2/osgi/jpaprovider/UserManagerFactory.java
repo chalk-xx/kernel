9f486b30899569cb5da6542a41088a8c6e9ea897
reciva-at <codders@octomonkey.org.uk>
2009-04-01 13:02:15 +0100

    Fixed up the provider to work with eclipselink (and not access the datasource on startup)

--- slingtests/osgikernel/bundles/jpaprovider/src/main/java/org/sakaiproject/kernel2/osgi/jpaprovider/UserManagerFactory.java
+++ slingtests/osgikernel/bundles/jpaprovider/src/main/java/org/sakaiproject/kernel2/osgi/jpaprovider/UserManagerFactory.java
@@ -1,14 +1,22 @@
 package org.sakaiproject.kernel2.osgi.jpaprovider;
 
+import org.eclipse.persistence.jpa.osgi.PersistenceProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class UserManagerFactory {
 
   public static EntityManager getUserManager()
   {
-    EntityManagerFactory factory = Persistence.createEntityManagerFactory("user");
+    Map<String, Object> properties = new HashMap<String, Object>();
+    properties.put("eclipselink.ddl-generation", "drop-and-create-tables");
+    properties.put("eclipselink.ddl-generation.output-mode", "database");
+    properties.put("eclipselink.classloader", EntityManager.class.getClassLoader());
+    EntityManagerFactory factory = new PersistenceProvider().createEntityManagerFactory("user", properties);
     return factory.createEntityManager();
   }
 }
