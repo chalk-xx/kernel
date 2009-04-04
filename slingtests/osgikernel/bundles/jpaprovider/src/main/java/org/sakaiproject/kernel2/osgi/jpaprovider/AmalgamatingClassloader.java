ba65ac4fbd5eb01bcb4bb637d7e8643336919e15
Arthur Taylor <codders@octomonkey.org.uk>
2009-04-04 09:37:17 +0100

    Fixed up licenses, formatting

--- slingtests/osgikernel/bundles/jpaprovider/src/main/java/org/sakaiproject/kernel2/osgi/jpaprovider/AmalgamatingClassloader.java
+++ slingtests/osgikernel/bundles/jpaprovider/src/main/java/org/sakaiproject/kernel2/osgi/jpaprovider/AmalgamatingClassloader.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package org.sakaiproject.kernel2.osgi.jpaprovider;
 
 import com.thoughtworks.xstream.XStream;
@@ -48,8 +66,7 @@ public class AmalgamatingClassloader extends ClassLoader {
   public Class<?> loadClass(String name) throws ClassNotFoundException {
     BundleGatheringResourceFinder finder = PersistenceBundleMonitor.getBundleResourceFinder();
     Class<?> result = finder.loadClass(name);
-    if (result == null)
-    {
+    if (result == null) {
       return super.loadClass(name);
     }
     return result;
@@ -125,7 +142,8 @@ public class AmalgamatingClassloader extends ClassLoader {
 
     // The base directory must be empty since JPA will scan it searching for
     // classes.
-    File file = new File(System.getProperty("java.io.tmpdir") + "/sakai" + System.currentTimeMillis() + "/" + filename);
+    File file = new File(System.getProperty("java.io.tmpdir") + "/sakai"
+        + System.currentTimeMillis() + "/" + filename);
     if (file.getParentFile().mkdirs()) {
       LOG.debug("Created " + file);
     }
