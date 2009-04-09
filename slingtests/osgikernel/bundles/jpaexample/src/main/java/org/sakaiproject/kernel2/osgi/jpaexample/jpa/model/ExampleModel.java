63e0da2fc3dd5cfa0dc68102f7e4e971670d3fbd
Arthur Taylor <codders@octomonkey.org.uk>
2009-04-09 10:38:53 +0100

    Fixed up JPA examples to work

--- slingtests/osgikernel/bundles/jpaexample/src/main/java/org/sakaiproject/kernel2/osgi/jpaexample/jpa/model/ExampleModel.java
+++ slingtests/osgikernel/bundles/jpaexample/src/main/java/org/sakaiproject/kernel2/osgi/jpaexample/jpa/model/ExampleModel.java
@@ -22,6 +22,8 @@ import java.io.Serializable;
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -30,6 +32,7 @@ public class ExampleModel implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
   private long id;
 
   @Basic
