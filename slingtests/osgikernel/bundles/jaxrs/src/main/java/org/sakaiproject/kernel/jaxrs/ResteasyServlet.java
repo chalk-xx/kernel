896b0a3ce523d19c2c7518e6db5b1712ce8f7b9e
Ian Boston <ieb@tfd.co.uk>
2009-04-16 10:10:27 +0100

    Fixed startup of the JAXRS servlet that was using a context classloader internally and so could not find the correct implementation of JAXRS It will become important to ensure that implementation jars dont leak exports as if they do config like this will also leak.

--- slingtests/osgikernel/bundles/jaxrs/src/main/java/org/sakaiproject/kernel/jaxrs/ResteasyServlet.java
+++ slingtests/osgikernel/bundles/jaxrs/src/main/java/org/sakaiproject/kernel/jaxrs/ResteasyServlet.java
@@ -37,7 +37,7 @@ import javax.servlet.ServletException;
 
 /**
  * The <code>ResteasyServlet</code> TODO
- *
+ * 
  * @scr.service interface="javax.servlet.Servlet"
  * @scr.component immediate="true"
  * @scr.property name="service.vendor" value="The Sakai Foundation"
@@ -45,9 +45,8 @@ import javax.servlet.ServletException;
  * @scr.property name="sling.servlet.paths" value="/system/jaxrs"
  */
 
-public class ResteasyServlet extends HttpServletDispatcher  {
-  
-  
+public class ResteasyServlet extends HttpServletDispatcher {
+
   /**
    *
    */
@@ -61,7 +60,7 @@ public class ResteasyServlet extends HttpServletDispatcher  {
    *
    */
   public static final String SERVLET_URL_MAPPING = SERVLET_PATH + "/*";
-  
+
   /**
    *
    */
@@ -84,41 +83,90 @@ public class ResteasyServlet extends HttpServletDispatcher  {
    */
   private ServiceTracker jaxRsResourceTracker;
 
+  private ComponentContext componentContext;
+
   /**
    * {@inheritDoc}
+   * 
    * @see org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher#init(javax.servlet.ServletConfig)
    */
   public void init(ServletConfig servletConfig) throws ServletException {
-    // Wrap the servlet config and context objects, since some http service impls (e.g. pax) don't
+    // Wrap the servlet config and context objects, since some http service impls (e.g.
+    // pax) don't
     // handle these parts of the servlet spec correctly
     this.servletConfig = new ServletConfigWrapper(servletConfig);
     this.servletContext = this.servletConfig.getServletContext();
+
+    bootstrap(componentContext);
 
     
     super.init(this.servletConfig);
   }
 
-  
   public ServletConfig getServletConfig() {
     return servletConfig;
   }
-  
+
   /**
    * {@inheritDoc}
+   * 
    * @see javax.servlet.GenericServlet#getServletContext()
    */
   public ServletContext getServletContext() {
     return servletContext;
   }
-  
-  
+
   /**
    * Component Activation
+   * 
    * @param context
    */
   protected void activate(ComponentContext componentContext) {
-    bootstrap();
-    BundleContext context= componentContext.getBundleContext();
+    this.componentContext = componentContext;
+    
+
+  }
+
+  /**
+   * Deactivate this component
+   * 
+   * @param context
+   */
+  protected void deactivate(ComponentContext context) {
+    jaxRsResourceTracker.close();
+
+  }
+
+  /**
+   * @param componentContext
+   * 
+   */
+  private void bootstrap(ComponentContext componentContext) {
+    ClassLoader bundleClassloader = this.getClass().getClassLoader();
+    ClassLoader contextClassloader = Thread.currentThread().getContextClassLoader();
+    ResteasyProviderFactory defaultInstance = null;
+    try {
+      Thread.currentThread().setContextClassLoader(bundleClassloader);
+      defaultInstance = ResteasyProviderFactory.getInstance();
+    } finally {
+      Thread.currentThread().setContextClassLoader(contextClassloader);
+    }
+    ResteasyProviderFactory.setInstance(new ThreadLocalResteasyProviderFactory(
+        defaultInstance));
+
+    servletContext.setAttribute(ResteasyProviderFactory.class.getName(), factory);
+    dispatcher = new SynchronousDispatcher(factory);
+    registry = dispatcher.getRegistry();
+    servletContext.setAttribute(Dispatcher.class.getName(), dispatcher);
+    servletContext.setAttribute(Registry.class.getName(), registry);
+    RegisterBuiltin.register(factory);
+    // Can't seem to get the resteasy input stream provider to work
+    factory.addMessageBodyWriter(new InputStreamProvider());
+    
+
+    // add in the trackers and register any outstanding beans
+    
+    BundleContext context = componentContext.getBundleContext();
     ServiceReference[] jaxRsRefs = null;
     try {
       jaxRsRefs = context.getAllServiceReferences(JaxRestService.class.getName(), null);
@@ -131,24 +179,26 @@ public class ResteasyServlet extends HttpServletDispatcher  {
       }
     }
 
-
     // Track JAX-RS Resources that are added and removed
     jaxRsResourceTracker = new ServiceTracker(componentContext.getBundleContext(),
         JaxRestService.class.getName(), null) {
       /**
        * {@inheritDoc}
+       * 
        * @see org.osgi.util.tracker.ServiceTracker#addingService(org.osgi.framework.ServiceReference)
        */
       @Override
       public Object addingService(ServiceReference reference) {
-        JaxRestService jaxRsResource = (JaxRestService)context.getService(reference);
+        JaxRestService jaxRsResource = (JaxRestService) context.getService(reference);
         registry.addSingletonResource(jaxRsResource);
         return super.addingService(reference);
       }
 
       /**
        * {@inheritDoc}
-       * @see org.osgi.util.tracker.ServiceTracker#removedService(org.osgi.framework.ServiceReference, java.lang.Object)
+       * 
+       * @see org.osgi.util.tracker.ServiceTracker#removedService(org.osgi.framework.ServiceReference,
+       *      java.lang.Object)
        */
       @Override
       public void removedService(ServiceReference reference, Object service) {
@@ -158,35 +208,7 @@ public class ResteasyServlet extends HttpServletDispatcher  {
     };
     jaxRsResourceTracker.open();
 
-  }
-  
-  /**
-   * Deactivate this component
-   * @param context
-   */
-  protected void deactivate(ComponentContext context) {
-    jaxRsResourceTracker.close();
     
-  }
-  
-  
-  /**
-   * 
-   */
-  private void bootstrap() {
-    ResteasyProviderFactory defaultInstance = ResteasyProviderFactory.getInstance();
-    ResteasyProviderFactory.setInstance(new ThreadLocalResteasyProviderFactory(defaultInstance));
-
-    servletContext.setAttribute(ResteasyProviderFactory.class.getName(), factory);
-    dispatcher = new SynchronousDispatcher(factory);
-    registry = dispatcher.getRegistry();
-    servletContext.setAttribute(Dispatcher.class.getName(), dispatcher);
-    servletContext.setAttribute(Registry.class.getName(), registry);
-    RegisterBuiltin.register(factory);
-    // Can't seem to get the resteasy input stream provider to work
-    factory.addMessageBodyWriter(new InputStreamProvider());
   }
 
 }
-
-
