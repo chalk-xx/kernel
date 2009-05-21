2d7598c7988e256459dadd38e16b3d7bce23dd56
Carl Hall <carl.hall@et.gatech.edu>
2009-05-21 17:07:02 -0400

    Updated the javadoc a bit. Changed path to /trusted.  Not sure this is correct but stands out more than /. Added constant for session attribute. Added TrustedAuthentication inner class to make it harder for other sources to fake the authentication response. Now putting TrustedAuthentication on the request after constructing it.

--- slingtests/osgikernel/bundles/trustedauth/src/main/java/org/sakaiproject/kernel/auth/trusted/TrustedAuthenticationHandler.java
+++ slingtests/osgikernel/bundles/trustedauth/src/main/java/org/sakaiproject/kernel/auth/trusted/TrustedAuthenticationHandler.java
@@ -28,16 +28,30 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Authentication handler for internally trusted users.
+ * Authentication handler for trusted authentication sources. These sources will
+ * authenticate users externally and eventually pass through this handler to
+ * establish a trusted relationship continuing into the container.
  *
  * @scr.component immediate="false" label="%auth.http.name"
  *                description="%auth.http.description"
  * @scr.service interface="org.apache.sling.engine.auth.AuthenticationHandler"
  */
 public class TrustedAuthenticationHandler implements AuthenticationHandler {
+  /**
+   * Authentication type name
+   */
   public static final String TRUSTED_AUTH = TrustedAuthenticationHandler.class.getName();
 
-  /** @scr.property value="/" */
+  /**
+   * Attribute name for storage of authentication
+   */
+  static final String USER_CREDENTIALS = TrustedAuthentication.class.getName();
+
+  /**
+   * Path on which this authentication should be activated.
+   *
+   * @scr.property value="/trusted"
+   */
   static final String PATH_PROPERTY = AuthenticationHandler.PATH_PROPERTY;
 
   /** @scr.property value="Trusted Authentication Handler" */
@@ -53,26 +67,45 @@ public class TrustedAuthenticationHandler implements AuthenticationHandler {
    *      javax.servlet.http.HttpServletResponse)
    */
   public AuthenticationInfo authenticate(HttpServletRequest request, HttpServletResponse response) {
-    HttpSession session = request.getSession();
+    HttpSession session = request.getSession(false);
     AuthenticationInfo authInfo = null;
 
-    if (session.getAttribute("") != null) {
-      authInfo = (AuthenticationInfo) session.getAttribute("");
-    } else {
-      String user = null;
+    TrustedAuthentication notes = null;
+    String user = null;
 
+    // check for existing authentication already in session
+    if (session != null && session.getAttribute(USER_CREDENTIALS) != null) {
+      notes = (TrustedAuthentication) session.getAttribute(USER_CREDENTIALS);
+      user = notes.getUser();
+    }
+    // nothing in session, get the user from the request
+    else {
       if (request.getUserPrincipal() != null) {
         user = request.getUserPrincipal().getName();
       } else if (request.getRemoteUser() != null) {
         user = request.getRemoteUser();
       }
+    }
 
-      if (user != null) {
-        request.getSession().setAttribute("trusted", Boolean.TRUE);
-        SimpleCredentials cred = new SimpleCredentials(user, null);
-        authInfo = new AuthenticationInfo(TRUSTED_AUTH, cred);
+    // if a user is available, construct the authentication info and store where
+    // appropriate
+    if (user != null) {
+      SimpleCredentials cred = new SimpleCredentials(user, null);
+      authInfo = new AuthenticationInfo(TRUSTED_AUTH, cred);
+      // make sure the session is available and the authentication wasn't
+      // previously retrieved from the session
+      if (session != null && notes == null) {
+        notes = new TrustedAuthentication(user);
+        session.setAttribute(USER_CREDENTIALS, notes);
+      }
+
+      // if the authentication is available, store it on the request for
+      // downstream use
+      if (notes != null) {
+        request.setAttribute(USER_CREDENTIALS, notes);
       }
     }
+
     return authInfo;
   }
 
@@ -87,4 +120,20 @@ public class TrustedAuthenticationHandler implements AuthenticationHandler {
     return false;
   }
 
+  /**
+   * Authentication information for storage in session and/or request. By being
+   * an inner, non-static class, it is harder for an external source to inject
+   * into the authentication chain.
+   */
+  final class TrustedAuthentication {
+    private final String user;
+
+    private TrustedAuthentication(String user) {
+      this.user = user;
+    }
+
+    String getUser() {
+      return user;
+    }
+  }
 }
