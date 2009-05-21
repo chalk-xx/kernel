1748b024ed8ff81ce39e4c22858e812260fe5e3b
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2009-05-21 09:26:09 -0400

    made scripts executable, cleaned up output

--- slingtests/osgikernel/testscripts/testCreateGroups.sh
+++ slingtests/osgikernel/testscripts/testCreateGroups.sh
@@ -3,8 +3,9 @@ n=`date "+%Y%m%d%H%M%S"`
 x=1
 while [[ $x -lt 1000 ]]
 do
-  curl -F:name=testgroup${n}-${x} --basic -u admin:admin http://localhost:8080/system/userManager/group.create.html
-  echo Created testgroup${n}-${x}
+  ((curl -F:name=testgroup${n}-${x} --basic -u admin:admin http://localhost:8080/system/userManager/group.create.html >/dev/null 2>/dev/null \
+  && echo Created testgroup${n}-${x}) \
+  || echo Error creating testgroup${n}-${x})
   let x=x+1
 done
 
