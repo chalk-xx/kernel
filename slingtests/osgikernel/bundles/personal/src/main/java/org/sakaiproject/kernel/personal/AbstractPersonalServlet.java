af17600b33e6a7ff38e1bf24201c0ad4ab33cf23
Ian Boston <ieb@tfd.co.uk>
2009-06-04 01:50:26 +0100

    Refactored the personal bundle to use the request dispatch method.

---
+++ slingtests/osgikernel/bundles/personal/src/main/java/org/sakaiproject/kernel/personal/AbstractPersonalServlet.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.kernel.personal;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.sakaiproject.kernel.api.user.UserFactoryService;
+import org.sakaiproject.kernel.util.PathUtils;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+/**
+ * 
+ */
+public abstract class AbstractPersonalServlet extends SlingAllMethodsServlet {
+
+
+  
+  /**
+   *
+   */
+  private static final long serialVersionUID = -7892996718559864951L;
+  
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.sakaiproject.kernel.message.AbstractMessageServlet#handleOperation(org.apache.sling.api.SlingHttpServletRequest,
+   *      org.apache.sling.api.servlets.HtmlResponse, java.util.List)
+   */
+  @Override
+  protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+      throws ServletException, IOException {
+    hashRequest(request, response);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.apache.sling.api.servlets.SlingAllMethodsServlet#doDelete(org.apache.sling.api.SlingHttpServletRequest,
+   *      org.apache.sling.api.SlingHttpServletResponse)
+   */
+  @Override
+  protected void doDelete(SlingHttpServletRequest request,
+      SlingHttpServletResponse response) throws ServletException, IOException {
+    hashRequest(request, response);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.apache.sling.api.servlets.SlingAllMethodsServlet#doPost(org.apache.sling.api.SlingHttpServletRequest,
+   *      org.apache.sling.api.SlingHttpServletResponse)
+   */
+  @Override
+  protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response)
+      throws ServletException, IOException {
+    hashRequest(request, response);
+  }
+  
+  /**
+   * {@inheritDoc}
+   * @see org.apache.sling.api.servlets.SlingAllMethodsServlet#doPut(org.apache.sling.api.SlingHttpServletRequest, org.apache.sling.api.SlingHttpServletResponse)
+   */
+  @Override
+  protected void doPut(SlingHttpServletRequest request, SlingHttpServletResponse response)
+      throws ServletException, IOException {
+    hashRequest(request, response);
+  }
+
+
+  /**
+   * @param request
+   * @param response
+   * @throws ServletException 
+   * @throws IOException 
+   */
+  protected abstract void hashRequest(SlingHttpServletRequest request,
+      SlingHttpServletResponse response) throws IOException, ServletException;
+
+  /**
+   * @param servletPath 
+   * @param pathInfo 
+   * @return
+   */
+  protected String toInternalPath(String servletPath, String pathInfo, String selector) {
+    String hashedPath = PathUtils.getHashedPath(pathInfo, 4);
+    if ( hashedPath.endsWith("/") ) {
+      hashedPath = hashedPath.substring(0, hashedPath.length()-2);
+    }
+    return PathUtils.normalizePath(servletPath  
+        + PathUtils.getHashedPath(pathInfo, 4) + selector);  }
+  
+  
+  /**
+   * The user factory service, injected.
+   * 
+   * @scr.reference name="UserFactoryService"
+   *                interface="org.sakaiproject.kernel.api.user.UserFactoryService"
+   */
+  protected UserFactoryService userFactoryService;
+
+  protected void bindUserFactoryService(UserFactoryService userFactoryService) {
+    this.userFactoryService = userFactoryService;
+  }
+
+  protected void unbindUserFactoryService(UserFactoryService userFactoryService) {
+    this.userFactoryService = null;
+  }
+
+
+}
---
+++ slingtests/osgikernel/bundles/personal/src/main/java/org/sakaiproject/kernel/personal/PersonalServlet.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.kernel.personal;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.NonExistingResource;
+import org.apache.sling.api.resource.Resource;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @scr.component metatype="no" immediate="true"
+ * @scr.service interface="javax.servlet.Servlet"
+ * @scr.property name="sling.servlet.path" values="/_user/private"
+ * @scr.property name="sling.servlet.methods" value.0="GET" value.1="POST" value.2="PUT"
+ *               value.3="DELETE"
+ */
+public class PersonalServlet extends AbstractPersonalServlet {
+
+  /**
+   *
+   */
+  private static final String _USER_PRIVATE = "/_user/private";
+  /**
+   *
+   */
+  private static final long serialVersionUID = -2663916166760531044L;
+
+  protected void hashRequest(SlingHttpServletRequest request,
+      SlingHttpServletResponse response) throws IOException, ServletException {
+    /*
+     * Process the path to expand based on the user, then dispatch to the resource at that
+     * location.
+     */
+    Resource baseResource = request.getResource();
+    String uriPath = baseResource.getPath();
+    String userId = request.getRemoteUser();
+    String resourcePath = _USER_PRIVATE + userFactoryService.getUserPrivatePath(userId)
+        + uriPath.substring(_USER_PRIVATE.length());
+
+    Resource resource = request.getResourceResolver().resolve(resourcePath);
+    if (resource instanceof NonExistingResource) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND,
+          "Resource does not exist (non existant)");
+      return;
+    }
+    if (resource == null) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND,
+          "Resource does not exist (null)");
+      return;
+    }
+    request.getRequestDispatcher(resource).forward(request, response);
+
+  }
+
+}
