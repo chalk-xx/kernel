de3e0da1f598dac4fb46f237b862c1651ac94011
Ian Boston <ieb@tfd.co.uk>
2009-06-08 10:29:37 +0100

    Cleaned up imports.

--- slingtests/osgikernel/libraries/utils/src/test/java/org/sakaiproject/kernel/util/JcrUtilsTest.java
+++ slingtests/osgikernel/libraries/utils/src/test/java/org/sakaiproject/kernel/util/JcrUtilsTest.java
@@ -17,7 +17,8 @@
  */
 package org.sakaiproject.kernel.util;
 
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
