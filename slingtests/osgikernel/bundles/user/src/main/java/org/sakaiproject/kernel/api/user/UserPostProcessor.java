78d80bb462bafb12d6ca050550617d6e11cdfae4
Ian Boston <ieb@tfd.co.uk>
2009-06-16 12:01:58 +0100

    Added the ability to register multiple UserPostProcessors. We should probably audit the registration and deregistration of these as implementation components will get notification of user change events, so only trusted components should be allowed to provide impls.

--- slingtests/osgikernel/bundles/user/src/main/java/org/sakaiproject/kernel/api/user/UserPostProcessor.java
+++ slingtests/osgikernel/bundles/user/src/main/java/org/sakaiproject/kernel/api/user/UserPostProcessor.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.sakaiproject.kernel.user;
+package org.sakaiproject.kernel.api.user;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.servlets.post.Modification;
