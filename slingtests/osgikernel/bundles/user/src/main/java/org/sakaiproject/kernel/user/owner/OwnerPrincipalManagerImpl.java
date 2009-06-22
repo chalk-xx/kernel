4b63a593971b54b369c7b3a00895bd64ca3dfb73
Ian Boston <ieb@tfd.co.uk>
2009-06-22 17:58:43 +0100

    KERN-265 Fixed the creation of public and private spaces for the user with the correct createdBy properties on the nodes. The DynamicOwnerPrincipalProvider is not working correctly at the moment so this issue cant be closed just yet.

--- slingtests/osgikernel/bundles/user/src/main/java/org/sakaiproject/kernel/user/owner/OwnerPrincipalManagerImpl.java
+++ slingtests/osgikernel/bundles/user/src/main/java/org/sakaiproject/kernel/user/owner/OwnerPrincipalManagerImpl.java
@@ -15,10 +15,11 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.sakaiproject.kernel.ownerprincipalmanager;
+package org.sakaiproject.kernel.user.owner;
+
+import static org.sakaiproject.kernel.api.user.UserConstants.JCR_CREATED_BY;
 
 import org.apache.sling.jcr.jackrabbit.server.security.dynamic.DynamicPrincipalManager;
-import org.sakaiproject.kernel.api.session.SessionManagerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,52 +27,44 @@ import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 
-
-
 /**
  * The <code>OwnerPrincipalManager</code>
  * 
  * @scr.component immediate="true" label="OwnerPrincipalManagerImpl"
  *                description="Implementation of the Dynamic Principal Manager Service"
- *                name="org.sakaiproject.kernel.ownerprincipalmanager.OwnerPrincipalManagerImpl"
- * @scr.service 
- *              interface="org.apache.sling.jcr.jackrabbit.server.security.dynamic.DynamicPrincipalManager"
+ *                name=
+ *                "org.sakaiproject.kernel.ownerprincipalmanager.OwnerPrincipalManagerImpl"
+ * @scr.service interface=
+ *              "org.apache.sling.jcr.jackrabbit.server.security.dynamic.DynamicPrincipalManager"
  * @scr.property name="service.vendor" value="The Sakai Foundation"
- * @scr.property name="service.description"
- *               value="Owner Principal Manager Implementation"
- * @scr.reference name="sessionManagerService"
- *                interface="org.sakaiproject.kernel.api.session.SessionManagerService"
- *                bind="bindSessionManagerService"
- *                unbind="unbindSessionManagerService"
+ * @scr.property name="service.description" value="Owner Principal Manager Implementation"
  */
 public class OwnerPrincipalManagerImpl implements DynamicPrincipalManager {
 
+  /**
+   *
+   */
   private static Logger LOG = LoggerFactory.getLogger(OwnerPrincipalManagerImpl.class);
-  protected SessionManagerService sessionManagerService;
-  
+
   public boolean hasPrincipalInContext(String principalName, Node aclNode) {
     try {
-      if ("owner".equals(principalName))
-      {
+      if ("owner".equals(principalName)) {
         /* Request comes in at node/rep:policy */
         Node contextNode = aclNode.getParent();
+        
         LOG.info("Granting .owner privs to node owner");
-        if (contextNode.hasProperty("jcr:createdBy"))
-        {
-          Property owner = contextNode.getProperty("jcr:createdBy");
+        if (contextNode.hasProperty(JCR_CREATED_BY)) {
+          Property owner = contextNode.getProperty(JCR_CREATED_BY);
           String ownerName = owner.getString();
           LOG.info("Got node owner: " + ownerName);
-          String currentUser = sessionManagerService.getCurrentUserId();
+          String currentUser = contextNode.getSession().getUserID();
           LOG.info("Got current user: " + currentUser);
-          if (currentUser.equals(ownerName))
-          {
+          if (currentUser.equals(ownerName)) {
             return true;
-          } 
+          }
           LOG.info(ownerName + " didn't match " + currentUser);
-        }
-        else
-        {
-          LOG.info("Node: " + contextNode.getPath() + " has no jcr:createdBy property");
+        } else {
+          LOG.info("Node: {}  has no {} property", contextNode.getPath(), JCR_CREATED_BY);
         }
       }
     } catch (RepositoryException e) {
@@ -79,14 +72,5 @@ public class OwnerPrincipalManagerImpl implements DynamicPrincipalManager {
     }
     return false;
   }
-
-  protected void bindSessionManagerService(SessionManagerService sessionManagerService) {
-    this.sessionManagerService = sessionManagerService;
-  }
-
-  protected void unbindSessionManagerService(SessionManagerService sessionManagerService) {
-    this.sessionManagerService = null;
-  }
-
 
 }
