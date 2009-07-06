49900aff6e479d25f5c830afb6a9a6ed6858acdc
Aaron Zeckoski <azeckoski@gmail.com>
2009-07-06 15:53:24 +0100

    fixed up the connections test script

--- slingtests/osgikernel/bundles/connections/test.sh
+++ slingtests/osgikernel/bundles/connections/test.sh
@@ -3,16 +3,17 @@
 # forcing a login in web browser
 # http://localhost:8080/?sling:authRequestLogin=1
 # create a few users
-curl -F:name=aaron -Fpwd=aaron -FpwdConfirm=aaron http://admin:admin@localhost:8080/system/userManager/user.create.html
-curl -F:name=nico -Fpwd=nico -FpwdConfirm=nico http://admin:admin@localhost:8080/system/userManager/user.create.html
-curl -F:name=ian -Fpwd=ian -FpwdConfirm=ian http://admin:admin@localhost:8080/system/userManager/user.create.html
+curl -u admin:admin -F:name=aaron -Fpwd=aaron -FpwdConfirm=aaron http://localhost:8080/system/userManager/user.create.html
+curl -u admin:admin -F:name=nico -Fpwd=nico -FpwdConfirm=nico http://localhost:8080/system/userManager/user.create.html
+curl -u admin:admin -F:name=ian -Fpwd=ian -FpwdConfirm=ian http://localhost:8080/system/userManager/user.create.html
+# GET http://localhost:8080/system/userManager/user/aaron.json
 # now try to do a request from aaron to nico
-curl -F"types=friend" -F"types=coworker" -X POST http://aaron:aaron@localhost:8080/_user/contacts/nico.request.html
+curl -u aaron:aaron -F"types=friend" -F"types=coworker" -X POST http://localhost:8080/_user/contacts/nico.invite.html
 # now try to fetch the connection nodes
-curl http://nico:nico@localhost:8080/_user/contacts/aaron.json
-curl http://aaron:aaron@localhost:8080/_user/contacts/nico.json
+curl -u nico:nico http://localhost:8080/_user/contacts/aaron.json
+curl -u aaron:aaron http://localhost:8080/_user/contacts/nico.json
 # now try to accept it
-curl -X POST http://nico:nico@localhost:8080/_user/contacts/aaron.accept.html
+curl -u nico:nico -X POST http://localhost:8080/_user/contacts/aaron.accept.html
 # now try to fetch the updated connection nodes
-curl http://nico:nico@localhost:8080/_user/contacts/aaron.json
-curl http://aaron:aaron@localhost:8080/_user/contacts/nico.json
+curl -u nico:nico  http://localhost:8080/_user/contacts/aaron.json
+curl -u aaron:aaron  http://localhost:8080/_user/contacts/nico.json
