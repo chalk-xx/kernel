d1a5e62863d9324fbfcadcac4d34645a6148ce88
Ian Boston <ieb@tfd.co.uk>
2009-07-28 12:52:32 +0100

    Update testall to simplify feedback.

--- slingtests/osgikernel/testscripts/SlingRuby/tests/testall.sh
+++ slingtests/osgikernel/testscripts/SlingRuby/tests/testall.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-./authz-test.rb
-./contact-test.rb
-./message-test.rb
-./node-create-test.rb
-./owner-principal-test.rb
-./sling-search-test.rb
-./sling-site-test.rb
-./user-manager-test.rb
+#!/bin/sh
+TESTS="authz-test.rb contact-test.rb message-test.rb node-create-test.rb owner-principal-test.rb sling-search-test.rb sling-site-test.rb user-manager-test.rb"
+for i in $TESTS
+do
+echo $i `./$i | grep failure`
+done
