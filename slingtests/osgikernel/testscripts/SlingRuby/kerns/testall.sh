8dded9d367278bbea3126942faa8b2cdefa16d20
Ian Boston <ieb@tfd.co.uk>
2009-07-28 08:42:50 +0100

    Added scripts to run all rb tests.

---
+++ slingtests/osgikernel/testscripts/SlingRuby/kerns/testall.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+./kern-254.rb
+./kern-259.rb
+./kern-270.rb
+###./kern-275.rb
+./kern-277.rb
+####./kern-278.rb
+./kern-284.rb
+./kern-307.rb
+./kern-309.rb
---
+++ slingtests/osgikernel/testscripts/SlingRuby/tests/testall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+./authz-test.rb
+./contact-test.rb
+./message-test.rb
+./node-create-test.rb
+./owner-principal-test.rb
+./sling-search-test.rb
+./sling-site-test.rb
+./user-manager-test.rb
