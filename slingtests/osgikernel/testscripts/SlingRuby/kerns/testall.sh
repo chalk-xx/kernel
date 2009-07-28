a709e7c3effd153a5b35450c861de489d59a0067
Ian Boston <ieb@tfd.co.uk>
2009-07-28 12:49:52 +0100

    Made kern-284 executable and modified the testall script to give better feedback

--- slingtests/osgikernel/testscripts/SlingRuby/kerns/testall.sh
+++ slingtests/osgikernel/testscripts/SlingRuby/kerns/testall.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-./kern-254.rb
-./kern-259.rb
-./kern-270.rb
-###./kern-275.rb
-./kern-277.rb
-####./kern-278.rb
-./kern-284.rb
-./kern-307.rb
-./kern-309.rb
+## ./kern-254.rb ./kern-259.rb ./kern-270.rb ###./kern-275.rb ./kern-277.rb ####./kern-278.rb ./kern-284.rb ./kern-307.rb ./kern-309.rb
+TESTS="kern-254.rb kern-259.rb kern-270.rb kern-277.rb kern-278.rb kern-284.rb kern-307.rb kern-309.rb"
+for i in $TESTS
+do
+echo $i `./$i | grep failure`
+done
