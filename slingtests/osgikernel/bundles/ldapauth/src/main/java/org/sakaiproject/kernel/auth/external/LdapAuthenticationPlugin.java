386366bab4506867ce72729d6fcbe33fa558f715
Carl Hall <carl.hall@et.gatech.edu>
2009-08-03 09:11:28 -0400

    Added artifact for jldap, some initial code to create LDAP connection.

--- slingtests/osgikernel/bundles/ldapauth/src/main/java/org/sakaiproject/kernel/auth/external/LdapAuthenticationPlugin.java
+++ slingtests/osgikernel/bundles/ldapauth/src/main/java/org/sakaiproject/kernel/auth/external/LdapAuthenticationPlugin.java
@@ -1,11 +1,18 @@
 package org.sakaiproject.kernel.auth.external;
 
+import com.novell.ldap.LDAPAttribute;
+import com.novell.ldap.LDAPConnection;
+import com.novell.ldap.LDAPException;
+import com.novell.ldap.LDAPJSSESecureSocketFactory;
+import com.novell.ldap.LDAPSocketFactory;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.jcr.jackrabbit.server.security.AuthenticationPlugin;
 import org.osgi.service.component.ComponentContext;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Dictionary;
 
 import javax.jcr.Credentials;
@@ -18,32 +25,82 @@ import javax.jcr.SimpleCredentials;
 @Service
 @Component
 public class LdapAuthenticationPlugin implements AuthenticationPlugin {
-  @Property
+  @Property(value = "localhost")
   static final String LDAP_HOST = "sakai.ldap.host";
 
-  @Property
-  static final String LDAP_LOGIN_USER = "sakai.ldap.user";
+  @Property(intValue = LDAPConnection.DEFAULT_SSL_PORT)
+  static final String LDAP_PORT = "sakai.ldap.port";
+
+  @Property(boolValue = true)
+  static final String LDAP_CONNECTION_SECURE = "sakai.ldap.connection.secure";
 
   @Property
-  static final String LDAP_LOGIN_PASSWORD = "sakai.ldap.password";
+  static final String LDAP_LOGIN_DN = "sakai.ldap.loginDn";
+
+  @Property
+  static final String LDAP_LOGIN_PASSWORD = "sakai.ldap.login.password";
 
   @Property
   static final String LDAP_BASE_DN = "sakai.ldap.baseDn";
 
+  @Property
+  static final String LDAP_ATTR_PASSWORD = "sakai.ldap.attribute.password";
+
+  private boolean useSecure;
   private String host;
-  private String user;
+  private int port;
+  private String loginDn;
   private String password;
   private String baseDn;
+  private String passwordAttributeName;
+
+  private LDAPConnection conn;
 
   protected void activate(ComponentContext ctx) {
     Dictionary props = ctx.getProperties();
 
+    useSecure = Boolean.parseBoolean((String) props.get(LDAP_CONNECTION_SECURE));
     host = (String) props.get(LDAP_HOST);
-    user = (String) props.get(LDAP_LOGIN_USER);
+    port = (Integer) props.get(LDAP_PORT);
+    loginDn = (String) props.get(LDAP_LOGIN_DN);
     password = (String) props.get(LDAP_LOGIN_PASSWORD);
     baseDn = (String) props.get(LDAP_BASE_DN);
+    passwordAttributeName = (String) props.get(LDAP_ATTR_PASSWORD);
 
-    // TODO create connection to ldap
+    // create connection to ldap. this does not establish the connection.
+    if (useSecure) {
+      // Dynamically set JSSE as a security provider
+      // Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+
+      // Dynamically set the property that JSSE uses to identify
+      // the keystore that holds trusted root certificates
+      // System.setProperty("javax.net.ssl.trustStore", path);
+
+      LDAPSocketFactory factory = new LDAPJSSESecureSocketFactory();
+      LDAPConnection.setSocketFactory(factory);
+    }
+
+    conn = new LDAPConnection();
+
+    try {
+      // establish the connection to ldap
+      conn.connect(host, port);
+      conn.bind(LDAPConnection.LDAP_V3, loginDn, password.getBytes("UTF8"));
+    } catch (LDAPException le) {
+      throw new RuntimeException(le.getMessage(), le);
+    } catch (UnsupportedEncodingException uee) {
+      throw new RuntimeException(uee.getMessage(), uee);
+    }
+  }
+
+  protected void deactivate(ComponentContext ctx) {
+    if (conn.isConnected()) {
+      try {
+        conn.disconnect();
+      } catch (LDAPException e) {
+        throw new RuntimeException(e.getMessage(), e);
+      }
+    }
   }
 
   public static boolean canHandle(Credentials credentials) {
@@ -56,6 +113,14 @@ public class LdapAuthenticationPlugin implements AuthenticationPlugin {
       SimpleCredentials sc = (SimpleCredentials) credentials;
 
       // TODO check credentials against ldap instance
+      LDAPAttribute passwordAttr = new LDAPAttribute(passwordAttributeName, new String(sc
+          .getPassword()));
+
+      try {
+        auth = conn.compare(baseDn + "/" + sc.getUserID(), passwordAttr);
+      } catch (LDAPException e) {
+        throw new RepositoryException(e.getMessage(), e);
+      }
     }
     return auth;
   }
