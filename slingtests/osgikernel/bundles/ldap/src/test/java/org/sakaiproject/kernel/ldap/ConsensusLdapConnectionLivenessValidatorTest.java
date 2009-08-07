1bfc46c53ce5505f92e93c209e77f9b366084442
Carl Hall <carl.hall@et.gatech.edu>
2009-08-07 10:19:18 -0400

    Copied over unit tests from old code base.  Beginning to convert tests to easymock from jmock.

---
+++ slingtests/osgikernel/bundles/ldap/src/test/java/org/sakaiproject/kernel/ldap/ConsensusLdapConnectionLivenessValidatorTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership. The SF licenses this file to you
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.sakaiproject.kernel.ldap;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.sakaiproject.kernel.ldap.api.LdapConnectionLivenessValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsensusLdapConnectionLivenessValidatorTest extends MockObjectTestCase {
+
+	private ConsensusLdapConnectionLivenessValidator validator;
+	private Mock mockDelegateValidator1;
+	private Mock mockDelegateValidator2;
+	private Mock mockDelegateValidator3;
+	private LdapConnectionLivenessValidator delegateValidator1;
+	private LdapConnectionLivenessValidator delegateValidator2;
+	private LdapConnectionLivenessValidator delegateValidator3;
+
+	@Override
+  protected void setUp() {
+		validator = new ConsensusLdapConnectionLivenessValidator();
+		mockDelegateValidator1 = new Mock(LdapConnectionLivenessValidator.class, "mockDelegateValidator1");
+		mockDelegateValidator2 = new Mock(LdapConnectionLivenessValidator.class, "mockDelegateValidator2");
+		mockDelegateValidator3 = new Mock(LdapConnectionLivenessValidator.class, "mockDelegateValidator3");
+		delegateValidator1 = (LdapConnectionLivenessValidator) mockDelegateValidator1.proxy();
+		delegateValidator2 = (LdapConnectionLivenessValidator) mockDelegateValidator2.proxy();
+		delegateValidator3 = (LdapConnectionLivenessValidator) mockDelegateValidator3.proxy();
+	}
+
+	public void testValidatesConnectionIfNoDelegatesRegistered() {
+		PooledLDAPConnection conn = new PooledLDAPConnection();
+		assertTrue(validator.isConnectionAlive(conn));
+	}
+
+	public void testValidatesConnectionIfAllDelegatesValidateConnection() {
+		PooledLDAPConnection conn = new PooledLDAPConnection();
+		mockDelegateValidator1.expects(once()).method("isConnectionAlive").
+		    with(same(conn)).will(returnValue(true));
+		mockDelegateValidator2.expects(once()).method("isConnectionAlive").
+		    with(same(conn)).after(mockDelegateValidator1, "isConnectionAlive").
+		    will(returnValue(true));
+		mockDelegateValidator3.expects(once()).method("isConnectionAlive").
+		    with(same(conn)).after(mockDelegateValidator2, "isConnectionAlive").
+	        will(returnValue(true));
+		List<LdapConnectionLivenessValidator> delegates =
+			new ArrayList<LdapConnectionLivenessValidator>(3);
+		delegates.add(delegateValidator1);
+		delegates.add(delegateValidator2);
+		delegates.add(delegateValidator3);
+		validator.setDelegates(delegates);
+		assertTrue(validator.isConnectionAlive(conn));
+	}
+
+	public void testInvalidatesConnectionIfOneDelegateInvalidatesConnection() {
+		PooledLDAPConnection conn = new PooledLDAPConnection();
+		mockDelegateValidator1.expects(once()).method("isConnectionAlive").
+		    with(same(conn)).will(returnValue(true));
+		mockDelegateValidator2.expects(once()).method("isConnectionAlive").
+		    with(same(conn)).after(mockDelegateValidator1, "isConnectionAlive").
+		    will(returnValue(false));
+		List<LdapConnectionLivenessValidator> delegates =
+			new ArrayList<LdapConnectionLivenessValidator>(3);
+		delegates.add(delegateValidator1);
+		delegates.add(delegateValidator2);
+		delegates.add(delegateValidator3); // expects iteration to short-circuit
+		validator.setDelegates(delegates);
+		assertFalse(validator.isConnectionAlive(conn));
+	}
+
+	public void testTreatsNullDelegateListInjectionAsEmptyList() {
+		validator.setDelegates(null);
+		testValidatesConnectionIfNoDelegatesRegistered();
+	}
+
+}
---
+++ slingtests/osgikernel/bundles/ldap/src/test/java/org/sakaiproject/kernel/ldap/SearchExecutingLdapConnectionLivenessValidatorInitializationTest.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership. The SF licenses this file to you
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.sakaiproject.kernel.ldap;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.sakaiproject.kernel.api.configuration.ConfigurationService;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Tests for an unitialized {@link SearchExecutingLdapConnectionLivenessValidator}
+ * fixture. That is, it tests the initialization logic itself. This is
+ * as opposed to the fixture in {@link SearchExecutingLdapConnectionLivenessValidatorTest}
+ * which initializes the fixture in
+ * {@link SearchExecutingLdapConnectionLivenessValidatorTest#setUp()}.
+ *
+ * @author dmccallum
+ *
+ */
+public class SearchExecutingLdapConnectionLivenessValidatorInitializationTest extends MockObjectTestCase {
+
+	private static final String UNIQUE_SEARCH_FILTER_TERM = "TESTING";
+	private SearchExecutingLdapConnectionLivenessValidator validator;
+	private Mock mockServerConfigService;
+  private ConfigurationService configService;
+
+	@Override
+  protected void setUp() {
+		validator = new SearchExecutingLdapConnectionLivenessValidator() {
+			// we need this to be a predictable value
+			@Override
+      protected String generateUniqueToken() {
+				return UNIQUE_SEARCH_FILTER_TERM;
+			}
+		};
+    mockServerConfigService = new Mock(ConfigurationService.class);
+    configService = (ConfigurationService) mockServerConfigService.proxy();
+    validator.setConfigService(configService);
+	}
+
+	public void testInitHonorsExplicitlyInjectedHostName() {
+		final String EXPECTED_HOST_NAME = "EXPECTED_HOST_NAME";
+		validator.setHostName(EXPECTED_HOST_NAME);
+		validator.init();
+		assertEquals(EXPECTED_HOST_NAME, validator.getHostName());
+	}
+
+	public void testInitDefaultsHostNameToInetAddressLocalhostIfNoHostNameExplicitlyInjected()
+	throws UnknownHostException {
+		final String EXPECTED_HOST_NAME = InetAddress.getLocalHost().toString();
+    validator.setConfigService(configService);
+		validator.init();
+		assertEquals(EXPECTED_HOST_NAME, validator.getHostName());
+	}
+
+	public void testInitDefaultsHostNameToSakaiServerNameIfNoHostNameExplicitlyInjectedAndLocalHostLookupFails() {
+		validator = new SearchExecutingLdapConnectionLivenessValidator() {
+			// we need this to be a predictable value
+			@Override
+      protected String generateUniqueToken() {
+				return UNIQUE_SEARCH_FILTER_TERM;
+			}
+
+			@Override
+      protected String getLocalhostName() throws UnknownHostException {
+				throw new UnknownHostException();
+			}
+		};
+		final String EXPECTED_HOST_NAME = "EXPECTED_HOST_NAME";
+    mockServerConfigService.expects(once()).method("getProperty").will(
+        returnValue(EXPECTED_HOST_NAME));
+    validator.setConfigService(configService);
+		validator.init();
+		assertEquals(EXPECTED_HOST_NAME, validator.getHostName());
+	}
+
+	public void testInitDefaultsHostNameToInetAddressLocalhostIfNoHostNameExplicitlyInjectedAndNoSakaiServerNameInjected()
+	throws UnknownHostException {
+    validator.setConfigService(null);
+		assertNull(validator.getServerConfigService()); // sanity check
+		validator.init();
+		assertEquals(validator.getLocalhostName(),
+				validator.getHostName());
+	}
+
+	public void testDefaultsHostNameToConstantDefaultIfNeitherInitNorSetHostNameCalled() {
+		assertEquals(SearchExecutingLdapConnectionLivenessValidator.DEFAULT_HOST_NAME,
+				validator.getHostName());
+	}
+
+}
