2e2b17048f08d3f46b40ef99c94411fd8f453253
Simon Gaeremynck <sg555@celestine.dmz.caret.local>
2009-08-18 10:46:07 +0100

    Added a search for pages.

---
+++ slingtests/osgikernel/bundles/search/src/main/java/org/sakaiproject/kernel/search/processors/PageSearchResultProcessor.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.kernel.search.processors;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.io.JSONWriter;
+import org.sakaiproject.kernel.api.search.SearchResultProcessor;
+import org.sakaiproject.kernel.util.ExtendedJSONWriter;
+
+/**
+ * Formats sakai/page nodes.
+ * Options are: 
+ *   - path: The path where you want to search under.
+ *   - properties: list of properties you want to filter on.
+ *   - values: list of values for the properties
+ *   - operators: list of operators: > = <
+ * 
+ * @scr.component immediate="true" label="PageSearchResultProcessor"
+ *                description="Formatter for pages search results."
+ * @scr.property name="service.vendor" value="The Sakai Foundation"
+ * @scr.property name="sakai.search.processor" value="Page"
+ * @scr.service 
+ *              interface="org.sakaiproject.kernel.api.search.SearchResultProcessor"
+ */
+public class PageSearchResultProcessor implements SearchResultProcessor {
+  
+  public void writeNode(JSONWriter write, Node resultNode) throws JSONException, RepositoryException {    
+    write.object();
+    ExtendedJSONWriter.writeNodeContentsToWriter(write, resultNode);
+    write.key("path");
+    write.value(resultNode.getPath());
+    write.endObject(); 
+    
+  }
+}
