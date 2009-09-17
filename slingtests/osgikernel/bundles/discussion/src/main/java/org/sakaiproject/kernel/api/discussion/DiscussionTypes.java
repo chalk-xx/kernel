f5ede44334fd3a510b0bfa58e50a94c010a6bfae
Ian Boston <ieb@tfd.co.uk>
2009-09-17 15:25:48 +1000

    Added Comment to discussion types.

--- slingtests/osgikernel/bundles/discussion/src/main/java/org/sakaiproject/kernel/api/discussion/DiscussionTypes.java
+++ slingtests/osgikernel/bundles/discussion/src/main/java/org/sakaiproject/kernel/api/discussion/DiscussionTypes.java
@@ -3,6 +3,13 @@ package org.sakaiproject.kernel.api.discussion;
 public enum DiscussionTypes {
   discussion, comment;
 
+  // TODO: IMHO, hasValue should not do things like this. We have an enum, and we are
+  // encouraging abuse of the enum since Discssion DiScUsSiOn are all ok, but wont be
+  // searchable, and can't be coerced into the DiscussionType I would rather using     
+  // DiscussionTypes.valueOf(string) and catch 
+  // the InvalidArgumentException 
+
+  
   public static boolean hasValue(String s) {
     DiscussionTypes[] types = values();
     for (DiscussionTypes t : types) {
