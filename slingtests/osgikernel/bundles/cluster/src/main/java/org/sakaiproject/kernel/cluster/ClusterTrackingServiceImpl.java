80dd18b55e00e83869678945b15fe67fe4b7b78e
Ian Boston <ieb@tfd.co.uk>
2009-09-20 22:40:59 +1000

    Added cluster service with testing, not wired in or in the app bundle at the moment.
    This service tracks users in the cluster using cluster replicated caches provided by the cache manager service. It also tracks server instances in the cluster.
    Note that the server instance is unique to the JVM instance and does not persist over restarts of that app server.

---
+++ slingtests/osgikernel/bundles/cluster/src/main/java/org/sakaiproject/kernel/cluster/ClusterTrackingServiceImpl.java
@@ -0,0 +1,252 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.kernel.cluster;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.Services;
+import org.apache.sling.commons.scheduler.Scheduler;
+import org.osgi.service.component.ComponentContext;
+import org.sakaiproject.kernel.api.cluster.ClusterTrackingService;
+import org.sakaiproject.kernel.api.cluster.ClusterUser;
+import org.sakaiproject.kernel.api.memory.Cache;
+import org.sakaiproject.kernel.api.memory.CacheManagerService;
+import org.sakaiproject.kernel.api.memory.CacheScope;
+import org.sakaiproject.kernel.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The ClusterTrackingService, maintains an entry for the active server and tracks active
+ * users with a cluster replicated shared cache.
+ */
+@Component(description = "Cluster tracking, tracks app servers and users within the cluster", label = "Cluster Tracking", name = "ClusterTrackingService", immediate = true)
+@Services(value = { @Service(value = ClusterTrackingService.class),
+    @Service(value = Runnable.class) })
+@Properties(value = {
+    @Property(name = Scheduler.PROPERTY_SCHEDULER_CONCURRENT, boolValue = false),
+    @Property(name = Scheduler.PROPERTY_SCHEDULER_PERIOD, longValue = 300L) })
+public class ClusterTrackingServiceImpl implements ClusterTrackingService, Runnable {
+
+  /**
+   * The logger for the service.
+   */
+  private static final Logger LOGGER = LoggerFactory
+      .getLogger(ClusterTrackingServiceImpl.class);
+  /**
+   * The name of the cache used to track users.
+   */
+  private static final String TRACKING_CACHE = "user-tracking-cache";
+
+  /**
+   * The name of the cache used to track users.
+   */
+  private static final String SERVER_CACHE = "server-tracking-cache";
+
+  /**
+   * The Name of the Cookie used to track users.
+   */
+  private static final String SAKAI_TRACKING = "SAKAI-TRACKING";
+
+  /**
+   * The Cache Manager service, injected.
+   */
+  @Reference
+  private CacheManagerService cacheManagerService;
+
+  /**
+   * A String representing the time when the service started.
+   */
+  private String componentStartTime;
+
+  /**
+   * The Id of the server, takned from JMX normally processID@server-hostname.
+   */
+  private String serverId;
+  /**
+   * True when the service is active.
+   */
+  private boolean isActive = false;
+
+  /**
+   * @param cacheManagerService2
+   */
+  public ClusterTrackingServiceImpl(CacheManagerService cacheManagerService) {
+    this.cacheManagerService = cacheManagerService;
+  }
+
+  /**
+   * Activate the service, getting the id of the jvm instance and register the instance.
+   * 
+   * @param ctx
+   * @throws Exception
+   */
+  public void activate(ComponentContext ctx) throws Exception {
+    componentStartTime = String.valueOf(System.currentTimeMillis());
+    MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+    ObjectName name = new ObjectName("java.lang:type=Runtime");
+    serverId = (String) mbeanServer.getAttribute(name, "Name");
+    isActive = true;
+    pingInstance();
+  }
+
+  /**
+   * Remove the registration for the instance.
+   * 
+   * @param ctx
+   * @throws Exception
+   */
+  public void deactivate(ComponentContext ctx) throws Exception {
+    removeInstance(serverId);
+  }
+
+  /**
+   * Track a user, called from a filter, this will update the central cache for the cookie
+   * and set the cookie if not present.
+   * 
+   * @param request
+   *          the http request oject.
+   * @param response
+   *          the http response object which will, if there is no cookie present, and the
+   *          response is not committed, have the cookie set.
+   */
+  public void trackClusterUser(HttpServletRequest request, HttpServletResponse response) {
+    Cookie[] cookies = request.getCookies();
+    String remoteUser = request.getRemoteUser();
+    boolean tracking = false;
+    for (Cookie cookie : cookies) {
+      String cookieName = cookie.getName();
+      if (cookieName.equals(SAKAI_TRACKING)) {
+        String trackingCookie = cookie.getValue();
+        pingTracking(trackingCookie, remoteUser);
+        tracking = true;
+      }
+    }
+    if (!tracking && !response.isCommitted()) {
+      // the tracking cookie is the a sha1 hash of the thread, the server startup id and
+      // time
+      String seed = Thread.currentThread().getName() + ":" + componentStartTime + ":"
+          + System.currentTimeMillis();
+      String trackingCookie = Thread.currentThread().getName() + ":"
+          + System.currentTimeMillis();
+      try {
+        trackingCookie = serverId + "-" + StringUtils.sha1Hash(seed);
+      } catch (Exception e) {
+        LOGGER.error("Failed to hash new cookie ", e);
+      }
+
+      Cookie cookie = new Cookie(SAKAI_TRACKING, trackingCookie);
+      cookie.setMaxAge(-1);
+      cookie.setComment("Cluster User Tracking");
+      cookie.setPath("/");
+      cookie.setVersion(0);
+      response.addCookie(cookie);
+      pingTracking(trackingCookie, remoteUser);
+    }
+
+  }
+
+  /**
+   * Get the user based on a tracking id.
+   * 
+   * @param trackingCookie
+   * @return
+   */
+  public ClusterUser getUser(String trackingCookie) {
+    Cache<ClusterUserImpl> cache = getTrackingCache();
+    ClusterUserImpl cuser = cache.get(trackingCookie);
+    if (cuser == null) {
+      return null;
+    } else if (cuser.expired()) {
+      cache.remove(trackingCookie);
+      return null;
+    }
+    return cuser;
+  }
+
+  /**
+   * update the tracking for a user, if expired or the user name has changed.
+   * 
+   * @param trackingCookie
+   *          the cookie tracking.
+   * @param remoteUser
+   *          the user id.
+   */
+  private void pingTracking(String trackingCookie, String remoteUser) {
+    Cache<ClusterUserImpl> cache = getTrackingCache();
+    ClusterUserImpl cuser = cache.get(trackingCookie);
+    if (cuser == null || cuser.expired(remoteUser)) {
+      cache.put(trackingCookie, new ClusterUserImpl(remoteUser, serverId));
+    }
+  }
+
+  /**
+   * Update the server registration.
+   */
+  private void pingInstance() {
+    if (isActive) {
+      getServerCache().put(serverId, new ClusterServerImpl(serverId));
+    }
+  }
+
+  /**
+   * @param serverId
+   *          remove the instance from the cluster wide cache.
+   */
+  private void removeInstance(String serverId) {
+    getServerCache().remove(serverId);
+  }
+
+  /**
+   * @return the cache used to store server registrations.
+   */
+  private Cache<ClusterServerImpl> getServerCache() {
+    return cacheManagerService.getCache(SERVER_CACHE, CacheScope.CLUSTERREPLICATED);
+  }
+
+  /**
+   * @return the Cache used to track users
+   */
+  private Cache<ClusterUserImpl> getTrackingCache() {
+    return cacheManagerService.getCache(TRACKING_CACHE, CacheScope.CLUSTERREPLICATED);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * Invoked as a task by the Sling Scheduler, once every 5 minutes to update the last
+   * time the server was registered in the cluster cache.
+   * 
+   * @see java.lang.Runnable#run()
+   */
+  public void run() {
+    pingInstance();
+  }
+
+}
---
+++ slingtests/osgikernel/bundles/cluster/src/test/java/org/sakaiproject/kernel/cluster/TestClusterTrackingServiceImpl.java
@@ -0,0 +1,348 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.kernel.cluster;
+
+import static org.junit.Assert.*;
+import static org.easymock.EasyMock.*;
+
+import org.easymock.Capture;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.sakaiproject.kernel.api.memory.Cache;
+import org.sakaiproject.kernel.api.memory.CacheManagerService;
+import org.sakaiproject.kernel.api.memory.CacheScope;
+import org.sakaiproject.kernel.testutils.easymock.AbstractEasyMockTest;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ */
+public class TestClusterTrackingServiceImpl extends AbstractEasyMockTest {
+
+  private ClusterTrackingServiceImpl clusterTrackingServiceImpl;
+  private CacheManagerService cacheManagerService;
+  private Cache<Object> userTrackingCache;
+  private Cache<Object> serverTrackingCache;
+
+  @SuppressWarnings("unchecked")
+  @Before
+  public void before() {
+    cacheManagerService = createMock(CacheManagerService.class);
+    userTrackingCache = createMock(Cache.class);
+    serverTrackingCache = createMock(Cache.class);
+    expect(
+        cacheManagerService.getCache("user-tracking-cache", CacheScope.CLUSTERREPLICATED))
+        .andReturn(userTrackingCache).anyTimes();
+    expect(
+        cacheManagerService.getCache("server-tracking-cache",
+            CacheScope.CLUSTERREPLICATED)).andReturn(serverTrackingCache).anyTimes();
+    clusterTrackingServiceImpl = new ClusterTrackingServiceImpl(cacheManagerService);
+  }
+
+  @After
+  public void after() {
+  }
+
+  @Test
+  public void testGetServerId() throws Exception {
+    ;
+    assertNotNull(getServerId());
+  }
+
+  /**
+   * @return
+   * @throws NullPointerException
+   * @throws MalformedObjectNameException
+   * @throws ReflectionException
+   * @throws MBeanException
+   * @throws InstanceNotFoundException
+   * @throws AttributeNotFoundException
+   */
+  private String getServerId() throws MalformedObjectNameException, NullPointerException,
+      AttributeNotFoundException, InstanceNotFoundException, MBeanException,
+      ReflectionException {
+    MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+    ObjectName name = new ObjectName("java.lang:type=Runtime");
+    return (String) mbeanServer.getAttribute(name, "Name");
+  }
+
+  @Test
+  public void testActivate() throws Exception {
+    
+    // activate
+    String serverId = getServerId();
+    Capture<String> serverIdCapture = new Capture<String>();
+    Capture<ClusterServerImpl> clusterServerCapture = new Capture<ClusterServerImpl>();
+    expect(
+        serverTrackingCache.put(capture(serverIdCapture), capture(clusterServerCapture)))
+        .andReturn(new Object());
+    
+    
+    replay();
+    clusterTrackingServiceImpl.activate(null);
+    assertTrue(serverIdCapture.hasCaptured());
+    assertEquals(serverId, serverIdCapture.getValue());
+    assertTrue(clusterServerCapture.hasCaptured());
+    ClusterServerImpl clusterServerImpl = clusterServerCapture.getValue();
+    assertEquals(serverId, clusterServerImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >= clusterServerImpl.getLastModified());
+    verify();
+  }
+
+  @Test
+  public void testActivateDeactivate() throws Exception {
+    // activate
+    String serverId = getServerId();
+    Capture<String> serverIdCapture = new Capture<String>();
+    Capture<ClusterServerImpl> clusterServerCapture = new Capture<ClusterServerImpl>();
+    expect(
+        serverTrackingCache.put(capture(serverIdCapture), capture(clusterServerCapture)))
+        .andReturn(new Object());
+    
+    // deactivate 
+    serverTrackingCache.remove(serverId);
+    
+    replay();
+    clusterTrackingServiceImpl.activate(null);
+    clusterTrackingServiceImpl.deactivate(null);
+    assertTrue(serverIdCapture.hasCaptured());
+    assertEquals(serverId, serverIdCapture.getValue());
+    assertTrue(clusterServerCapture.hasCaptured());
+    ClusterServerImpl clusterServerImpl = clusterServerCapture.getValue();
+    assertEquals(serverId, clusterServerImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >= clusterServerImpl.getLastModified());
+    verify();
+  }
+  
+
+  @Test
+  public void testTrackClusterUser() throws Exception {
+    // activate
+    String serverId = getServerId();
+    Capture<String> serverIdCapture = new Capture<String>();
+    Capture<ClusterServerImpl> clusterServerCapture = new Capture<ClusterServerImpl>();
+    expect(
+        serverTrackingCache.put(capture(serverIdCapture), capture(clusterServerCapture)))
+        .andReturn(new Object());
+    
+    
+    
+    // trackClusterUser
+    HttpServletRequest request = createMock(HttpServletRequest.class);
+    HttpServletResponse response = createMock(HttpServletResponse.class);
+    
+    Cookie cookieA = new Cookie("something","someValue");
+    Cookie cookieMatching = new Cookie("SAKAI-TRACKING","trackingValue");
+    Cookie cookieB = new Cookie("somethingElse","someOtherValue");
+    
+    Cookie[] cookies = new Cookie[] {
+        cookieA,
+        cookieMatching,
+        cookieB
+    };
+    
+    
+    expect(request.getCookies()).andReturn(cookies);
+    expect(request.getRemoteUser()).andReturn("userid");
+    // nothing in the cache.
+    expect(userTrackingCache.get("trackingValue")).andReturn(null);
+    Capture<String> trackingValueCapture = new Capture<String>();
+    Capture<ClusterUserImpl> clusterUserCapture = new Capture<ClusterUserImpl>();
+    expect(userTrackingCache.put(capture(trackingValueCapture), capture(clusterUserCapture))).andReturn(new Object());
+    // deactivate 
+    serverTrackingCache.remove(serverId);
+    
+    replay();
+    clusterTrackingServiceImpl.activate(null);
+    
+    clusterTrackingServiceImpl.trackClusterUser(request, response);
+    
+    clusterTrackingServiceImpl.deactivate(null);
+    assertTrue(serverIdCapture.hasCaptured());
+    assertEquals(serverId, serverIdCapture.getValue());
+    assertTrue(clusterServerCapture.hasCaptured());
+    ClusterServerImpl clusterServerImpl = clusterServerCapture.getValue();
+    assertEquals(serverId, clusterServerImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >= clusterServerImpl.getLastModified());
+    
+    // check the user capture
+    assertTrue(trackingValueCapture.hasCaptured());
+    assertTrue(clusterUserCapture.hasCaptured());
+    assertEquals("trackingValue",trackingValueCapture.getValue());
+    ClusterUserImpl clusterUserImpl = clusterUserCapture.getValue();
+    assertEquals("userid",clusterUserImpl.getUser() );
+    assertEquals(serverId, clusterUserImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >=clusterUserImpl.getLastModified());
+    verify();
+  }
+  
+
+  @Test
+  public void testTrackClusterNewUser() throws Exception {
+    // activate
+    String serverId = getServerId();
+    Capture<String> serverIdCapture = new Capture<String>();
+    Capture<ClusterServerImpl> clusterServerCapture = new Capture<ClusterServerImpl>();
+    expect(
+        serverTrackingCache.put(capture(serverIdCapture), capture(clusterServerCapture)))
+        .andReturn(new Object());
+    
+    
+    
+    // trackClusterUser
+    HttpServletRequest request = createMock(HttpServletRequest.class);
+    HttpServletResponse response = createMock(HttpServletResponse.class);
+    
+    Cookie cookieA = new Cookie("something","someValue");
+    Cookie cookieB = new Cookie("somethingElse","someOtherValue");
+    
+    Cookie[] cookies = new Cookie[] {
+        cookieA,
+        cookieB
+    };
+    
+    
+    expect(request.getCookies()).andReturn(cookies);
+    expect(request.getRemoteUser()).andReturn("userid");
+    
+    expect(response.isCommitted()).andReturn(false);
+    Capture<Cookie> captureCookie = new Capture<Cookie>();
+    response.addCookie(capture(captureCookie));
+    expectLastCall();
+    
+    Capture<String> captureCookieValue = new Capture<String>();
+    // nothing in the cache.
+    expect(userTrackingCache.get(capture(captureCookieValue))).andReturn(null);
+    Capture<String> trackingValueCapture = new Capture<String>();
+    Capture<ClusterUserImpl> clusterUserCapture = new Capture<ClusterUserImpl>();
+    expect(userTrackingCache.put(capture(trackingValueCapture), capture(clusterUserCapture))).andReturn(new Object());
+    
+    
+    // deactivate 
+    serverTrackingCache.remove(serverId);
+    
+    replay();
+    clusterTrackingServiceImpl.activate(null);
+    
+    clusterTrackingServiceImpl.trackClusterUser(request, response);
+    
+    clusterTrackingServiceImpl.deactivate(null);
+    assertTrue(serverIdCapture.hasCaptured());
+    assertEquals(serverId, serverIdCapture.getValue());
+    assertTrue(clusterServerCapture.hasCaptured());
+    ClusterServerImpl clusterServerImpl = clusterServerCapture.getValue();
+    assertEquals(serverId, clusterServerImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >= clusterServerImpl.getLastModified());
+    
+    // check the cookie
+    assertTrue(captureCookie.hasCaptured());
+    Cookie cookie = captureCookie.getValue();
+    assertEquals("SAKAI-TRACKING", cookie.getName());
+    assertEquals("/", cookie.getPath());
+    assertEquals(-1, cookie.getMaxAge());
+    assertNotNull(cookie.getValue());
+    assertTrue(cookie.getValue().startsWith(serverId));
+    
+    assertTrue(captureCookieValue.hasCaptured());
+    assertEquals(cookie.getValue(), captureCookieValue.getValue());
+    
+    // check the user capture
+    assertTrue(trackingValueCapture.hasCaptured());
+    assertTrue(clusterUserCapture.hasCaptured());
+    assertEquals(cookie.getValue(),trackingValueCapture.getValue());
+    ClusterUserImpl clusterUserImpl = clusterUserCapture.getValue();
+    assertEquals("userid",clusterUserImpl.getUser() );
+    assertEquals(serverId, clusterUserImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >=clusterUserImpl.getLastModified());
+    verify();
+  }
+  
+  
+
+  @Test
+  public void testGetUser() throws Exception {
+    // activate
+    String serverId = getServerId();
+    Capture<String> serverIdCapture = new Capture<String>();
+    Capture<ClusterServerImpl> clusterServerCapture = new Capture<ClusterServerImpl>();
+    expect(
+        serverTrackingCache.put(capture(serverIdCapture), capture(clusterServerCapture)))
+        .andReturn(new Object());
+    
+    ClusterUserImpl clusterUserImpl = new ClusterUserImpl("remoteUser", "serverId");
+    expect(userTrackingCache.get("testCookieValue")).andReturn(clusterUserImpl);
+    
+    // deactivate 
+    serverTrackingCache.remove(serverId);
+    
+    replay();
+    clusterTrackingServiceImpl.activate(null);
+    clusterTrackingServiceImpl.getUser("testCookieValue");
+    clusterTrackingServiceImpl.deactivate(null);
+    assertTrue(serverIdCapture.hasCaptured());
+    assertEquals(serverId, serverIdCapture.getValue());
+    assertTrue(clusterServerCapture.hasCaptured());
+    ClusterServerImpl clusterServerImpl = clusterServerCapture.getValue();
+    assertEquals(serverId, clusterServerImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >= clusterServerImpl.getLastModified());
+    verify();
+  }
+
+  @Test
+  public void testGetUserNone() throws Exception {
+    // activate
+    String serverId = getServerId();
+    Capture<String> serverIdCapture = new Capture<String>();
+    Capture<ClusterServerImpl> clusterServerCapture = new Capture<ClusterServerImpl>();
+    expect(
+        serverTrackingCache.put(capture(serverIdCapture), capture(clusterServerCapture)))
+        .andReturn(new Object());
+    
+    expect(userTrackingCache.get("testCookieValue")).andReturn(null);
+    
+    // deactivate 
+    serverTrackingCache.remove(serverId);
+    
+    replay();
+    clusterTrackingServiceImpl.activate(null);
+    clusterTrackingServiceImpl.getUser("testCookieValue");
+    clusterTrackingServiceImpl.deactivate(null);
+    assertTrue(serverIdCapture.hasCaptured());
+    assertEquals(serverId, serverIdCapture.getValue());
+    assertTrue(clusterServerCapture.hasCaptured());
+    ClusterServerImpl clusterServerImpl = clusterServerCapture.getValue();
+    assertEquals(serverId, clusterServerImpl.getServerId());
+    assertTrue(System.currentTimeMillis() >= clusterServerImpl.getLastModified());
+    verify();
+  }
+
+  
+}
