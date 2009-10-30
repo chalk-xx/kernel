f96ccbcf267774f743c71614095c4432c355037b
Carl Hall <carl.hall@et.gatech.edu>
2009-10-30 17:12:29 -0400

    Added servlet to trigger on sakai/user-profile resource types.

---
+++ slingtests/osgikernel/bundles/userdirectory/src/main/java/org/sakaiproject/kernel/userdirectory/UserDirectoryServlet.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.kernel.userdirectory;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+/**
+ * <p>
+ * Servlet for looking up user information from various federated sources. This
+ * servlet is triggered by accessing a node that has
+ * resourceType="sakai/userdirectory".  This node should be a 
+ * user's space in JCR.
+ * </p>
+ * <p>
+ * <em>Example structure:</em><br/>
+ * /_user/public/ad/80/11/98/dsf/authprofile<br/>
+ * Where 'authprofile' has a property of resourceType = "sakai/user-profile"
+ * and the node 'dsf' is the username.
+ * </p>
+ * <p>
+ * Having the requested node as a first level subnode of a node that is named
+ * after the username to be looked up is required.
+ * </p>
+ */
+@SlingServlet(methods = "GET", resourceTypes = "sakai/user-profile")
+public class UserDirectoryServlet extends SlingSafeMethodsServlet {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserDirectoryServlet.class);
+  private static final long serialVersionUID = 6707040084319189872L;
+
+  @Override
+  protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+      throws ServletException, IOException {
+    Resource resource = request.getResource();
+    String path = resource.getPath();
+    String[] splitPath = StringUtils.split(path, '/');
+    String username = splitPath[splitPath.length - 2];
+
+	String msg = "Getting information for [" + username + "]";
+    LOGGER.info(msg);
+
+	response.getWriter().println(msg);
+  }
+
+}
