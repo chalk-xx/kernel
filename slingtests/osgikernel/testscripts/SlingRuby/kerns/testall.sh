8ac8cc6f4134f15539886efa0f360260fa2e6f85
Ray Davis <ray@media.berkeley.edu>
2009-10-30 17:24:17 -0700

    KERN-345 Support asymmetric contact relationships; allow new relationships to be specified on a re-invite

--- slingtests/osgikernel/testscripts/SlingRuby/kerns/testall.sh
+++ slingtests/osgikernel/testscripts/SlingRuby/kerns/testall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TESTS="kern-254.rb kern-259.rb kern-270.rb kern-275.rb kern-277.rb kern-284.rb kern-288.rb kern-289.rb kern-292.rb kern-307.rb kern-308.rb kern-309.rb kern-310.rb kern-312.rb kern-325.rb"
+TESTS="kern-254.rb kern-259.rb kern-270.rb kern-275.rb kern-277.rb kern-284.rb kern-288.rb kern-289.rb kern-292.rb kern-307.rb kern-308.rb kern-309.rb kern-310.rb kern-312.rb kern-325.rb kern-345.rb"
 for i in $TESTS
 do
 echo $i `./$i | grep failure`
