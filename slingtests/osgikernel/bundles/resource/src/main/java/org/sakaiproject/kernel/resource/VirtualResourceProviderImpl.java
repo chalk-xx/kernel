c04f09341f6b1eaa4b57df34335c4f96f2b7f70a
Ian Boston <ieb@tfd.co.uk>
2009-11-13 09:47:50 +0000

    Removed unnecessary and unused properties.

--- slingtests/osgikernel/bundles/resource/src/main/java/org/sakaiproject/kernel/resource/VirtualResourceProviderImpl.java
+++ slingtests/osgikernel/bundles/resource/src/main/java/org/sakaiproject/kernel/resource/VirtualResourceProviderImpl.java
@@ -29,8 +29,6 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceProvider;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.jcr.resource.JcrResourceConstants;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.component.ComponentContext;
 import org.sakaiproject.kernel.util.PathUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -73,8 +71,6 @@ public class VirtualResourceProviderImpl implements ResourceProvider,
       return new ArrayList<String>();
     };
   };
-
-  private ComponentContext componentContext;
 
   /**
    * {@inheritDoc}
