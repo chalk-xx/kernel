c7a07a671416598dab6cdd74b1a8ccb78fe37596
Lance Speelmon <lance@speelmon.com>
2009-12-17 13:01:54 -0500

    KERN-42 Initial commit

---
+++ slingtests/osgikernel/bundles/import/src/main/java/org/sakaiproject/kernel/importer/ImportSiteArchive.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.kernel.importer;
+
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestParameter;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+@SlingServlet(methods = { "POST" }, resourceTypes = { "sling/servlet/default" }, selectors = { "sitearchive" })
+@Properties(value = {
+    @Property(name = "service.description", value = "Imports a zip file from Sakai2 SiteArchive."),
+    @Property(name = "service.vendor", value = "The Sakai Foundation") })
+public class ImportSiteArchive extends SlingAllMethodsServlet {
+  public static final Logger LOG = LoggerFactory
+      .getLogger(ImportSiteArchive.class);
+  private final SAXParserFactory spf = SAXParserFactory.newInstance();
+  SAXParser parser = null;
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+   */
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    try {
+      parser = spf.newSAXParser();
+      resetSAXParser();
+    } catch (Exception e) {
+      throw new Error(e);
+    }
+  }
+
+  private void resetSAXParser() {
+    parser.reset();
+    spf.setValidating(false); // do not validate XML
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.apache.sling.api.servlets.SlingAllMethodsServlet#doPost(org.apache.sling.api.SlingHttpServletRequest,
+   *      org.apache.sling.api.SlingHttpServletResponse)
+   */
+  @Override
+  protected void doPost(SlingHttpServletRequest request,
+      SlingHttpServletResponse response) throws ServletException {
+    final Session session = request.getResourceResolver()
+        .adaptTo(Session.class);
+    final Node targetNode = request.getResource().adaptTo(Node.class);
+    // if (targetNode == null) {
+    // response.sendError(HttpServletResponse.SC_NOT_FOUND,
+    // "The path specified could not be found.");
+    // return;
+    // }
+    RequestParameter[] files = request.getRequestParameters("Filedata");
+    if (files == null) {
+      sendError(HttpServletResponse.SC_BAD_REQUEST,
+          "Missing Filedata parameter.", null, response);
+      return;
+    }
+    for (RequestParameter p : files) {
+      LOG.info((p.getFileName() + ": " + p.getContentType() + ": " + p
+          .getSize()));
+      try {
+        // create temporary local file of zip contents
+        final File tempZip = File.createTempFile("siteArchive", "zip");
+        tempZip.deleteOnExit(); // just in case
+        final InputStream in = p.getInputStream();
+        final FileOutputStream out = new FileOutputStream(tempZip);
+        final byte[] buf = new byte[4096];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+          out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+        // process the zip file
+        ZipFile zip = null;
+        try {
+          zip = new ZipFile(tempZip);
+        } catch (ZipException e) {
+          sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE,
+              "Invalid zip file: " + p.getFileName() + ": "
+                  + p.getContentType() + ": " + p.getSize(), null, response);
+        }
+        if (zip != null) {
+          for (ZipEntry entry : Collections.list(zip.entries())) {
+            if (entry.getName().startsWith("__MACOSX")
+                || entry.getName().endsWith(".DS_Store")) {
+              ; // skip entry
+            } else {
+              if ("content.xml".equals(entry.getName())) {
+                LOG.info("found content.xml!");
+                 parser.parse(zip.getInputStream(entry),
+                 new SiteArchiveContentHandler("/lance/import", zip));
+              }
+            }
+          }
+          zip.close();
+        }
+        // delete temporary file
+        tempZip.delete();
+      } catch (IOException e) {
+        sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e
+            .getLocalizedMessage(), e, response);
+      } catch (SAXException e) {
+        sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e
+            .getLocalizedMessage(), e, response);
+      }
+    }
+  }
+
+  private void sendError(int errorCode, String message, Throwable exception,
+      HttpServletResponse response) {
+    if (!response.isCommitted()) {
+      try {
+        LOG.error(errorCode + ": " + message, exception);
+        response.sendError(errorCode, message);
+      } catch (IOException e) {
+        throw new Error(e);
+      }
+    } else {
+      LOG.error(errorCode + ": " + message, exception);
+      throw new Error(message);
+    }
+  }
+}
---
+++ slingtests/osgikernel/bundles/import/src/main/java/org/sakaiproject/kernel/importer/SiteArchiveContentHandler.java
@@ -0,0 +1,225 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.sakaiproject.kernel.importer;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.sling.api.request.RequestParameter;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipFile;
+
+public class SiteArchiveContentHandler extends DefaultHandler {
+  private final String supportedVersions[] = { "Sakai 1.0" };
+  private Base64 base64 = new Base64();
+  private String basePath;
+  private ZipFile zip;
+  private String date;
+  private String server;
+  private String source;
+  private String system;
+  private Map<String, Resource> resources = new HashMap<String, Resource>();
+  private String currentResourceId;
+
+  public SiteArchiveContentHandler(String basePath, ZipFile zip) {
+    if (basePath == null || "".equals(basePath)) {
+      throw new IllegalArgumentException("Illegal basePath");
+    }
+    if (zip == null) {
+      throw new IllegalArgumentException("Illegal ZipFile");
+    }
+    if (basePath.endsWith("/")) { // strip trailing slash
+      int lastSlash = basePath.lastIndexOf("/");
+      this.basePath = basePath.substring(0, lastSlash);
+    } else {
+      this.basePath = basePath;
+    }
+    this.zip = zip;
+  }
+
+  /**
+   * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String,
+   *      java.lang.String, java.lang.String, org.xml.sax.Attributes)
+   */
+  @Override
+  public void startElement(String uri, String localName, String qName,
+      Attributes attributes) throws SAXException {
+    if ("archive".equalsIgnoreCase(qName)) {
+      date = attributes.getValue("date");
+      server = attributes.getValue("server");
+      source = attributes.getValue("source");
+      system = attributes.getValue("system");
+      boolean supportedVersion = false;
+      for (String version : supportedVersions) {
+        if (version.equalsIgnoreCase(system)) {
+          supportedVersion = true;
+        }
+      }
+      if (!supportedVersion) {
+        throw new Error("not a supported version: " + system);
+      }
+    }
+    if ("collection".equalsIgnoreCase(qName)
+        || "resource".equalsIgnoreCase(qName)) {
+      // grab the resource's attributes
+      Resource resource = new Resource();
+      for (int i = 0; i < attributes.getLength(); i++) {
+        resource.attributes.put(attributes.getLocalName(i).toLowerCase(),
+            attributes.getValue(i));
+      }
+      currentResourceId = resource.getId();
+      resources.put(currentResourceId, resource);
+    }
+    if ("property".equalsIgnoreCase(qName)) {
+      Resource collection = resources.get(currentResourceId);
+      final String name = attributes.getValue("name");
+      if (attributes.getValue("value") != null
+          && !"".equals(attributes.getValue("value"))) {
+        String value = null;
+        if (attributes.getValue("enc").equalsIgnoreCase("BASE64")) {
+          value = new String(base64.decode(attributes.getValue("value")));
+        } else {
+          value = attributes.getValue("value");
+        }
+        collection.properties.put(name, value);
+      }
+    }
+  }
+
+  /**
+   * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String,
+   *      java.lang.String, java.lang.String)
+   */
+  @Override
+  public void endElement(String uri, String localName, String qName)
+      throws SAXException {
+    if ("collection".equalsIgnoreCase(qName)
+        || "resource".equalsIgnoreCase(qName)) {
+      makeResource(resources.get(currentResourceId));
+    }
+  }
+
+  private void makeResource(Resource resource) {
+    final String destination = basePath + "/" + resource.getRelativeId();
+    if ("org.sakaiproject.content.types.folder".equalsIgnoreCase(resource
+        .getType())) {
+      makeDirectory(destination);
+    } else if ("org.sakaiproject.content.types.fileUpload"
+        .equalsIgnoreCase(resource.getType())
+        || "org.sakaiproject.content.types.TextDocumentType"
+            .equalsIgnoreCase(resource.getType())
+        || "org.sakaiproject.content.types.HtmlDocumentType"
+            .equalsIgnoreCase(resource.getType())
+        || "org.sakaiproject.content.types.urlResource"
+            .equalsIgnoreCase(resource.getType())) {
+      copyFile(resource.attributes.get("body-location"), destination);
+      StringBuilder sb = new StringBuilder();
+      for (String key : resource.attributes.keySet()) {
+        sb.append(key);
+        sb.append("=");
+        sb.append(resource.attributes.get(key));
+        sb.append("\n");
+      }
+      for (String key : resource.properties.keySet()) {
+        sb.append(key);
+        sb.append("=");
+        sb.append(resource.properties.get(key));
+        sb.append("\n");
+      }
+      makeFile(destination + ".properties", sb.toString());
+    } else {
+      System.err.println("Mising handler for type: " + resource.getType());
+    }
+  }
+
+  private boolean makeDirectory(String path) {
+    File file = new File(path);
+    if (file.exists() && file.isDirectory()) {
+      return true; // nothing to do
+    } else {
+      return file.mkdir();
+    }
+  }
+
+  private void makeFile(String path, String contents) {
+    File file = new File(path);
+    try {
+      final FileOutputStream out = new FileOutputStream(file);
+      out.write(contents.getBytes("UTF-8"));
+      out.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void copyFile(String zipEntryName, String destination) {
+    try {
+      final InputStream in = zip.getInputStream(zip.getEntry(zipEntryName));
+      final FileOutputStream out = new FileOutputStream(destination);
+      final byte[] buf = new byte[4096];
+      int len;
+      while ((len = in.read(buf)) > 0) {
+        out.write(buf, 0, len);
+      }
+      in.close();
+      out.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private class Resource {
+    private Map<String, String> attributes = new HashMap<String, String>();
+    private Map<String, String> properties = new HashMap<String, String>();
+
+    public String getId() {
+      return attributes.get("id");
+    }
+
+    public String getRelativeId() {
+      return attributes.get("rel-id");
+    }
+
+    public String getType() {
+      return attributes.get("resource-type");
+    }
+
+    @Override
+    public int hashCode() {
+      return this.getId().hashCode();
+    }
+
+    @Override
+    public String toString() {
+      return getRelativeId();
+    }
+  }
+}
