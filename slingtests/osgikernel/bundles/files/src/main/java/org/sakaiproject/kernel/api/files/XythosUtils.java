9d096594a1824dd48265e874dbf50cadb2cb96b2
Zach A. Thomas <zach@aeroplanesoftware.com>
2010-02-03 13:14:09 -0600

    getting closer to getting search results to come across from Xythos.

--- slingtests/osgikernel/bundles/files/src/main/java/org/sakaiproject/kernel/api/files/XythosUtils.java
+++ slingtests/osgikernel/bundles/files/src/main/java/org/sakaiproject/kernel/api/files/XythosUtils.java
@@ -1,18 +1,26 @@
 package org.sakaiproject.kernel.api.files;
 
 import com.caucho.hessian.client.HessianProxyFactory;
-import com.caucho.hessian.client.HessianRuntimeException;
 
+import org.apache.jackrabbit.commons.iterator.RowIteratorAdapter;
+import org.apache.jackrabbit.value.StringValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import edu.nyu.XythosRemote;
 
-import javax.jcr.Node;
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.Value;
 import javax.jcr.query.Query;
+import javax.jcr.query.Row;
+import javax.jcr.query.RowIterator;
 
 public class XythosUtils {
   
@@ -55,15 +63,47 @@ public class XythosUtils {
     }
   }
 
-  public static Collection searchFiles(Query query) {
+  public static RowIterator searchFiles(Query query) {
     try {
+      Collection<Row> rowCollection = new ArrayList<Row>();
+      String queryString = composeQueryForXythos(query);
       HessianProxyFactory factory = new HessianProxyFactory();
       XythosRemote xythos = (XythosRemote) factory.create(XythosRemote.class, url, XythosUtils.class.getClassLoader());
-      Collection rv = xythos.findFiles(query.getStatement());
-      return rv;
-    } catch (MalformedURLException e) {
+      Map<String,String> foo = xythos.getProperties();
+      Collection<Map<String,String>> result = xythos.findFilesWithXPath(queryString, "zach");	
+      for (Map<String,String> rowMap : result) {
+    	  rowCollection.add(rowFromMap(rowMap));
+      }
+      return new RowIteratorAdapter(rowCollection);
+    } catch (Throwable e) {
       throw new RuntimeException("MalformedURLException: " + e.getMessage());
     }
   }
+
+private static Row rowFromMap(final Map<String, String> map) {
+	return new Row() {
+		
+		@Override
+		public Value getValue(String propertyName)
+				throws ItemNotFoundException, RepositoryException {
+			return new StringValue(map.get(propertyName));
+		}
+
+		@Override
+		public Value[] getValues() throws RepositoryException {
+			List<Value> rv = new ArrayList<Value>();
+			for(String value : map.values()) {
+				rv.add(new StringValue(value));
+			}
+			return rv.toArray(new Value[rv.size()]);
+		}
+		
+	};
+}
+
+private static String composeQueryForXythos(Query query) {
+	// TODO do the right thing here
+	return "//*[(@jcr:primaryType='nt:file')]";
+}
 
 }
--- slingtests/osgikernel/bundles/files/src/main/java/org/sakaiproject/kernel/files/XythosFilesSearchResultProcessor.java
+++ slingtests/osgikernel/bundles/files/src/main/java/org/sakaiproject/kernel/files/XythosFilesSearchResultProcessor.java
@@ -1,46 +1,49 @@
 package org.sakaiproject.kernel.files;
 
-import org.apache.jackrabbit.commons.iterator.RowIteratorAdapter;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.io.JSONWriter;
+import org.sakaiproject.kernel.api.files.FileUtils;
 import org.sakaiproject.kernel.api.files.XythosUtils;
 import org.sakaiproject.kernel.api.search.AbstractSearchResultSet;
 import org.sakaiproject.kernel.api.search.Aggregator;
 import org.sakaiproject.kernel.api.search.SearchException;
 import org.sakaiproject.kernel.api.search.SearchResultProcessor;
 import org.sakaiproject.kernel.api.search.SearchResultSet;
+import org.sakaiproject.kernel.util.ExtendedJSONWriter;
+import org.sakaiproject.kernel.util.RowUtils;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Map;
-
-import javax.jcr.ItemNotFoundException;
-import javax.jcr.PropertyType;
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
-import javax.jcr.Value;
-import javax.jcr.ValueFormatException;
+import javax.jcr.Session;
 import javax.jcr.query.Query;
 import javax.jcr.query.Row;
+import javax.jcr.query.RowIterator;
 
+/**
+ * Formats message node search results
+ * 
+ * @scr.component immediate="true" label="XythosFilesSearchResultProcessor"
+ *                description="Returns search results from Xythos"
+ * @scr.property name="service.vendor" value="The Sakai Foundation"
+ * @scr.property name="sakai.search.processor" value="XythosFiles"
+ * @scr.service interface="org.sakaiproject.kernel.api.search.SearchResultProcessor"
+ */
 public class XythosFilesSearchResultProcessor implements SearchResultProcessor {
 
   public SearchResultSet getSearchResultSet(SlingHttpServletRequest request, Query query)
       throws SearchException {
-    Collection<Row> rows = new ArrayList<Row>();
-    Collection<Map<String,String>> xythosResults = XythosUtils.searchFiles(query);
-    for (Map<String,String> result : xythosResults) {
-      final String filename = result.get("sakai:filename");
-    }
-    return new AbstractSearchResultSet(new RowIteratorAdapter(xythosResults), xythosResults.size());
+    RowIterator xythosResults = XythosUtils.searchFiles(query);
+    return new AbstractSearchResultSet(xythosResults, (int) xythosResults.getSize());
   }
 
   public void writeNode(SlingHttpServletRequest request, JSONWriter write,
       Aggregator aggregator, Row row) throws JSONException, RepositoryException {
-    // TODO Auto-generated method stub
+	  Session session = request.getResourceResolver().adaptTo(Session.class);
 
+	    write.array();
+	    write.endArray();
   }
 
 }
