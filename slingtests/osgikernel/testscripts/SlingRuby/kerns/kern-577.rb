7a915c8f87d219818b12596f3fef67b0ce77bbe5
Simon Gaeremynck <gaeremyncks@gmail.com>
2010-02-05 12:15:07 +0000

    Added ruby tests for new files.

--- slingtests/osgikernel/testscripts/SlingRuby/kerns/kern-577.rb
+++ slingtests/osgikernel/testscripts/SlingRuby/kerns/kern-577.rb
@@ -29,9 +29,76 @@ class TC_MyFileTest < SlingTest
     
     # Create a site for each user.
     @s.switch_user(simon)
-    @ss.create_site("/sites", title = "Simons Site", sitepath = "/simon")
+    @ss.create_site("/sites", title = "Simons Site", sitepath = "/simon#{m}")
+    res = @s.execute_get(@s.url_for("sites/simon#{m}.json"))
+    assert_equal(200, res.code.to_i(), "Expected to get the site information.")
+    site = JSON.parse(res.body)
+    
+    # Upload a couple of files to the user his public space.
+    res = @s.execute_file_post(@s.url_for("_user/public/simon#{m}/files"), "alfa", "alfa", "This is some random content: alfaalfa.", "text/plain")
+    assert_equal(201, res.code.to_i(), "Expected to be able to upload a file.")    
+    res = @s.execute_file_post(@s.url_for("_user/public/simon#{m}/files"), "beta", "beta", "This is some random content: betabeta.", "text/plain")
+    # This will return modified..
+    assert_equal(200, res.code.to_i(), "Expected to be able to upload a file.")
+    
+    # Create a tag.
+    res = @ff.createTag("foobar", "_user/public/simon#{m}/tags/footag")
+    assert_equal(201, res.code.to_i(), "Expected to be able to create a tag.")
+    # Get tag info
+    res = @s.execute_get(@s.url_for("_user/public/simon#{m}/tags/footag.json"))
+    tag = JSON.parse(res.body)
+    assert_not_nil(tag, "No response when creating a tag.")
+    assert_not_nil(tag['jcr:uuid'], "Expected a uuid for a tag.")
+    
+    # Tag the alfa file.
+    res = @ff.tag("_user/public/simon#{m}/files/alfa", tag['jcr:uuid'])
+    assert_equal(200, res.code.to_i(), "Expected to be able to tag an uploaded file.")
+    
+    # Tag a file with a non-existing tag.
+    res = @ff.tag("_user/public/simon#{m}/files/alfa", "foobar")
+    assert_equal(400, res.code.to_i(), "Tagging something with a non existing tag should return 400.")
+    
+    # Link a file 
+    res = @ff.createlink("_user/public/simon#{m}/files/alfa", "/sites/simon#{m}/_files/alfa", nil)
+    assert_equal(200, res.code.to_i(), "Expected to be able to link this thing.")
+    
+    # Link a file and associate it with a site.
+    res = @ff.createlink("_user/public/simon#{m}/files/alfa", "/sites/simon#{m}/_files/alfa", site['jcr:uuid'])
+    assert_equal(200, res.code.to_i(), "Expected to be able to link this file.")
     
     
+    # Do a search for my files and expect 2
+    res = @ff.myfiles("*")
+    myfiles = JSON.parse(res.body)
+    assert_equal(2, myfiles["total"].to_i(), "Expected 2 files for simon.")
+    
+    # Do a search for alfa and expect 1
+    res = @ff.myfiles("alfaalfa")
+    myfiles = JSON.parse(res.body)
+    assert_equal(1, myfiles["total"].to_i(), "Expected 1 file for simon on alfa.")
+    
+    #Try uploading as anonymous
+    puts("Check that Anon is denied ")
+    @s.switch_user(SlingUsers::User.anonymous)
+    res = @s.execute_file_post(@s.url_for("sites/simon#{m}/_files"), "anon", "anon", "This is some random content: anonanon.", "text/plain") 
+    if ( res.code == "200" )
+      assert_equal("-1",res.code,"Expected not be be able to upload a file as anon user "+res.body) 
+    end 
+    
+    res = @ff.myfiles("*")
+    myfiles = JSON.parse(res.body)
+    assert_equal(0, myfiles["total"].to_i(), "Expected 0 files for anon.")
+    
+  end
+  
+  def old_functionality
+    m = Time.now.to_i.to_s
+    puts("Creating user simon"+m)
+    simon = create_user("simon"+m)
+    
+    # Create a site for each user.
+    @s.switch_user(simon)
+    @ss.create_site("/sites", title = "Simons Site", sitepath = "/simon#{m}")
     
     # Upload 2 files for user simon.
     @s.switch_user(simon)
@@ -68,7 +135,7 @@ class TC_MyFileTest < SlingTest
     @s.switch_user(SlingUsers::User.anonymous)
     res = @ff.upload("/sites/simon/_files/anon.txt", "/sites/simon/anon") 
     if ( res.code == "200" )
-       assert_equal("-1",res.code,"Expected not be be able to upload a file as anon user "+res.body) 
+      assert_equal("-1",res.code,"Expected not be be able to upload a file as anon user "+res.body) 
     end 
     
     
