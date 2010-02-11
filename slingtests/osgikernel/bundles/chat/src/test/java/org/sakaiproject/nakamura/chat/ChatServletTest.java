f64a275509356ff84e98661d84d1860fb7a823c1
Zach A. Thomas <zach@aeroplanesoftware.com>
2010-02-11 11:39:58 -0600

    KERN-621 automated tests for ChatMessageSearchResultProcessor

--- slingtests/osgikernel/bundles/chat/src/test/java/org/sakaiproject/nakamura/chat/ChatServletTest.java
+++ slingtests/osgikernel/bundles/chat/src/test/java/org/sakaiproject/nakamura/chat/ChatServletTest.java
@@ -17,11 +17,8 @@
  */
 package org.sakaiproject.nakamura.chat;
 
-import static org.easymock.EasyMock.verify;
+import static org.easymock.classextension.EasyMock.*;
 
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.assertEquals;
 
 import org.apache.sling.api.SlingHttpServletRequest;
