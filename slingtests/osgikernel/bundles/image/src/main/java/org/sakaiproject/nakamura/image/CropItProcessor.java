99be6522341c73e94de78a09f5945cfe076b7ace
Simon G <gaeremyncks@gmail.com>
2010-03-03 12:15:45 +0000

    Switched to Sanselan for reading/writing images.

--- slingtests/osgikernel/bundles/image/src/main/java/org/sakaiproject/nakamura/image/CropItProcessor.java
+++ slingtests/osgikernel/bundles/image/src/main/java/org/sakaiproject/nakamura/image/CropItProcessor.java
@@ -18,6 +18,11 @@
 
 package org.sakaiproject.nakamura.image;
 
+import org.apache.sanselan.ImageFormat;
+import org.apache.sanselan.ImageInfo;
+import org.apache.sanselan.ImageReadException;
+import org.apache.sanselan.ImageWriteException;
+import org.apache.sanselan.Sanselan;
 import org.sakaiproject.nakamura.api.jcr.JCRConstants;
 import org.sakaiproject.nakamura.util.JcrUtils;
 import org.sakaiproject.nakamura.util.PathUtils;
@@ -26,25 +31,20 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.Dimension;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.imageio.ImageIO;
 import javax.jcr.Node;
 import javax.jcr.PathNotFoundException;
-import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
-import javax.jcr.ValueFormatException;
 
 public class CropItProcessor {
 
@@ -89,7 +89,7 @@ public class CropItProcessor {
 
       if (imgNode != null) {
 
-        String sImg = imgNode.getName();
+        String imgName = imgNode.getName();
         // nt:file
         if (imgNode.isNodeType(JCRConstants.NT_FILE)) {
           imgNode = imgNode.getNode(JCRConstants.JCR_CONTENT);
@@ -99,34 +99,43 @@ public class CropItProcessor {
           // this name.
           Node parentNode = imgNode.getParent();
           if (parentNode.isNodeType(JCRConstants.NT_FILE)) {
-            sImg = parentNode.getName();
+            imgName = parentNode.getName();
           }
         } else {
           throw new ImageException(500, "Invalid image");
         }
 
-        // get the MIME type of the image
-        String sType = getMimeTypeForNode(imgNode, sImg);
+        // Read the image
+        in = imgNode.getProperty(JCRConstants.JCR_DATA).getStream();
+        BufferedInputStream bufStream = new BufferedInputStream(in);
+        // We use a buffered input stream so we can mark the start.
+        // This allows us to stream the image, rather then keep it all in-memory.
+        // We keep the first 40K in memory. This is because sanselan looks at the first
+        // "magic bytes" to deterime the ImageInfo.
+        bufStream.mark(51200);
+        try {
 
-        // check if this is a valid image
-        if (sType.equalsIgnoreCase("image/png") || sType.equalsIgnoreCase("image/jpg")
-            || sType.equalsIgnoreCase("image/bmp") || sType.equalsIgnoreCase("image/gif")
-            || sType.equalsIgnoreCase("image/jpeg")) {
+          // Guess the format and check if it is a valid one.
+          ImageInfo info = Sanselan.getImageInfo(bufStream, imgName);
+          if (info.getFormat() == ImageFormat.IMAGE_FORMAT_UNKNOWN) {
+            // This is not a valid image.
+            LOGGER.error("Can't parse this format.");
+            throw new ImageException(406, "Can't parse this format.");
+          }
 
-          // Read the image
-          in = imgNode.getProperty(JCRConstants.JCR_DATA).getStream();
-
-          BufferedImage imgBuf = ImageIO.read(in);
+          // Reset the stream so we can read it in a BufferedImage.
+          bufStream.reset();
+          BufferedImage imgBuf = Sanselan.getBufferedImage(bufStream);
 
           // Set the correct width & height.
-          width = (width <= 0) ? imgBuf.getWidth() : width;
-          height = (height <= 0) ? imgBuf.getHeight() : height;
+          width = (width <= 0) ? info.getWidth() : width;
+          height = (height <= 0) ? info.getHeight() : height;
 
-          if (x + width > imgBuf.getWidth()) {
-            width = imgBuf.getWidth() - x;
+          if (x + width > info.getWidth()) {
+            width = info.getWidth() - x;
           }
-          if (y + height > imgBuf.getHeight()) {
-            height = imgBuf.getHeight() - y;
+          if (y + height > info.getHeight()) {
+            height = info.getHeight() - y;
           }
 
           // Cut the desired piece out of the image.
@@ -142,23 +151,26 @@ public class CropItProcessor {
             int iWidth = d.width;
             int iHeight = d.height;
 
-            iWidth = (iWidth <= 0) ? imgBuf.getWidth() : iWidth;
-            iHeight = (iHeight <= 0) ? imgBuf.getHeight() : iHeight;
+            iWidth = (iWidth <= 0) ? info.getWidth() : iWidth;
+            iHeight = (iHeight <= 0) ? info.getHeight() : iHeight;
 
             // Create the image.
-            out = scaleAndWriteToStream(iWidth, iHeight, subImage, sType, sImg);
+            out = scaleAndWriteToStream(iWidth, iHeight, subImage, imgName, info);
 
-            String sPath = save + iWidth + "x" + iHeight + "_" + sImg;
+            String sPath = save + iWidth + "x" + iHeight + "_" + imgName;
             // Save new image to JCR.
-            saveImageToJCR(sPath, sType, out, imgNode, session);
+            saveImageToJCR(sPath, info.getMimeType(), out, imgNode, session);
 
             out.close();
             arrFiles[i] = sPath;
           }
-        } else {
+        } catch (ImageReadException e) {
           // This is not a valid image.
-          LOGGER.error("Unknown image type: " + sType);
-          throw new ImageException(406, "Invalid filetype: " + sType);
+          LOGGER.error("Can't parse this format.", e);
+          throw new ImageException(406, "Can't parse this format.");
+        } catch (ImageWriteException e) {
+          LOGGER.error("Can't crop this image.", e);
+          throw new ImageException(406, "Can't crop this image.");
         }
       } else {
         throw new ImageException(400, "No image file found.");
@@ -167,10 +179,10 @@ public class CropItProcessor {
     } catch (PathNotFoundException e) {
       throw new ImageException(400, "Could not find image.");
     } catch (RepositoryException e) {
-      LOGGER.warn("Unable to crop image.");
+      LOGGER.error("Unable to crop image.", e);
       throw new ImageException(500, "Unable to crop image.");
     } catch (IOException e) {
-      LOGGER.warn("Unable to read image in order to crop it.");
+      LOGGER.error("Unable to read image in order to crop it.", e);
       throw new ImageException(500, "Unable to read image in order to crop it.");
     } finally {
       // close the streams
@@ -251,48 +263,29 @@ public class CropItProcessor {
    *          The desired height of the scaled image.
    * @param img
    *          The image that you want to scale
-   * @param sType
-   *          The mime type of the image.
-   * @param sImg
+   * @param imgName
    *          Filename of the image
-   * @return Returns an outputstream of the scaled image.
+   * @param info
+   *          The {@link ImageInfo info} for this image.
+   * @return Returns an {@link ByteArrayOutputStream ByteArrayOutputStream} of the scaled
+   *         image.
    * @throws IOException
+   * @throws ImageWriteException
+   *           Failed to write the cropped image to the stream.
    */
   public static ByteArrayOutputStream scaleAndWriteToStream(int width, int height,
-      BufferedImage img, String sType, String sImg) throws IOException {
+      BufferedImage img, String imgName, ImageInfo info) throws IOException,
+      ImageWriteException {
     ByteArrayOutputStream out = null;
     try {
-
-      Map<String, Integer> mapExtensionsToRGBType = new HashMap<String, Integer>();
-      mapExtensionsToRGBType.put("image/jpg", BufferedImage.TYPE_INT_RGB);
-      mapExtensionsToRGBType.put("image/jpeg", BufferedImage.TYPE_INT_RGB);
-      mapExtensionsToRGBType.put("image/gif", BufferedImage.TYPE_INT_ARGB);
-      mapExtensionsToRGBType.put("image/png", BufferedImage.TYPE_INT_ARGB_PRE);
-      mapExtensionsToRGBType.put("image/bmp", BufferedImage.TYPE_INT_RGB);
-
-      Integer type = BufferedImage.TYPE_INT_RGB;
-      if (mapExtensionsToRGBType.containsKey(sType)) {
-        type = mapExtensionsToRGBType.get(sType);
-      }
-
-      Image imgScaled = getScaledInstance(img, width, height, type);
-
-      BufferedImage biScaled = toBufferedImage(imgScaled, type);
+      // Get a scaled image.
+      BufferedImage imgScaled = getScaledInstance(img, width, height);
 
       // Convert image to a stream
       out = new ByteArrayOutputStream();
 
-      String sIOtype = sType.split("/")[1];
-
-      // If it's a gif try to write it as a jpg
-      if (sType.equalsIgnoreCase("image/gif")) {
-        // TODO: This does not do anything. commenting out, what was the intention ?
-        // (Findbugs found it)
-        // sImg = sImg.replaceAll("\\.gif", ".png");
-        sIOtype = "png";
-      }
-
-      ImageIO.write(biScaled, sIOtype, out);
+      // Write to stream.
+      Sanselan.writeImage(imgScaled, out, info.getFormat(), null);
     } finally {
       if (out != null) {
         out.close();
@@ -300,52 +293,6 @@ public class CropItProcessor {
     }
 
     return out;
-  }
-
-  /**
-   * Tries to fetch the mime type for a node. If the node lacks on, the mimetype will be
-   * determined via the extension.
-   * 
-   * @param imgToCrop
-   *          Node of the image.
-   * @param sImg
-   *          Filename of the image.
-   * @return
-   * @throws PathNotFoundException
-   * @throws ValueFormatException
-   * @throws RepositoryException
-   */
-  public static String getMimeTypeForNode(Node imgToCrop, String sImg)
-      throws PathNotFoundException, ValueFormatException, RepositoryException {
-    String sType = "";
-
-    // Standard list of images we support.
-    Map<String, String> mapExtensionsToMimes = new HashMap<String, String>();
-    mapExtensionsToMimes.put("jpg", "image/jpeg");
-    mapExtensionsToMimes.put("gif", "image/gif");
-    mapExtensionsToMimes.put("png", "image/png");
-    mapExtensionsToMimes.put("bmp", "image/bmp");
-
-    // check the MIME type out of JCR
-    if (imgToCrop.hasProperty(JCRConstants.JCR_MIMETYPE)) {
-      Property mimeTypeProperty = imgToCrop.getProperty(JCRConstants.JCR_MIMETYPE);
-      if (mimeTypeProperty != null) {
-        sType = mimeTypeProperty.getString();
-      }
-    }
-    // If we couldn't find it in the JCR we will check the extension
-    if (sType.equals("")) {
-      String ext = getExtension(sImg);
-      if (mapExtensionsToMimes.containsKey(ext)) {
-        sType = mapExtensionsToMimes.get(ext);
-      }
-      // default = text/plain".. This means we won't handle this image..
-      else {
-        sType = "text/plain";
-      }
-    }
-
-    return sType;
   }
 
   /**
@@ -361,26 +308,6 @@ public class CropItProcessor {
   }
 
   /**
-   * Takes an Image and converts it to a BufferedImage.
-   * 
-   * @param image
-   *          The image you want to convert.
-   * @param type
-   *          The type of the image you want it to convert to. ex:
-   *          BufferedImage.TYPE_INT_ARGB)
-   * @return
-   */
-  public static BufferedImage toBufferedImage(Image image, int type) {
-    int w = image.getWidth(null);
-    int h = image.getHeight(null);
-    BufferedImage result = new BufferedImage(w, h, type);
-    Graphics2D g = result.createGraphics();
-    g.drawImage(image, 0, 0, null);
-    g.dispose();
-    return result;
-  }
-
-  /**
    * Image scaling routine as prescribed by
    * http://today.java.net/pub/a/today/2007/04/03/perils-of-image-getscaledinstance.html.
    * Image.getScaledInstance() is not very efficient or fast and this leverages the
@@ -392,7 +319,7 @@ public class CropItProcessor {
    * @return
    */
   protected static BufferedImage getScaledInstance(BufferedImage img, int targetWidth,
-      int targetHeight, int type) {
+      int targetHeight) {
     BufferedImage ret = (BufferedImage) img;
 
     // Use multi-step technique: start with original size, then
@@ -414,7 +341,7 @@ public class CropItProcessor {
         h = targetHeight;
       }
 
-      BufferedImage tmp = new BufferedImage(w, h, type);
+      BufferedImage tmp = new BufferedImage(w, h, img.getType());
       Graphics2D g2 = tmp.createGraphics();
       g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
           RenderingHints.VALUE_INTERPOLATION_BILINEAR);
