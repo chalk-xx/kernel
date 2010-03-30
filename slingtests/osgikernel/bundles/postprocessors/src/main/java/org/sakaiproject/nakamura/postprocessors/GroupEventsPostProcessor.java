9b479109671fc28e48d684f846f01b8a516164dd
Zach A. Thomas <zach@aeroplanesoftware.com>
2010-03-29 19:32:02 -0500

    switched from javadoc to annotations for scr metadata in postprocessors bundle

--- slingtests/osgikernel/bundles/postprocessors/src/main/java/org/sakaiproject/nakamura/postprocessors/GroupEventsPostProcessor.java
+++ slingtests/osgikernel/bundles/postprocessors/src/main/java/org/sakaiproject/nakamura/postprocessors/GroupEventsPostProcessor.java
@@ -1,6 +1,10 @@
 package org.sakaiproject.nakamura.postprocessors;
 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
@@ -8,14 +12,12 @@ import org.slf4j.LoggerFactory;
 
 import edu.nyu.XythosRemote;
 
-/**
- * @scr.component immediate="true" label="GroupEventsPostProcessor"
- *                description="handle events when groups and memberships are created or modified"
- * @scr.service interface="org.osgi.service.event.EventHandler"
- * @scr.property name="service.description"
- *               value="Provides a place to respond when sites are created and memberships updated"
- * @scr.property name="event.topics" value="org/sakaiproject/nakamura/api/site/event/*"
- */
+@Component(enabled = true, immediate = true, metatype = true)
+@Service(value = EventHandler.class)
+@Properties(value = {
+    @Property(name = "service.vendor", value = "New York University"),
+    @Property(name = "service.description", value = "Provides a place to respond when sites are created and memberships updated"),
+    @Property(name = "event.topics", value = "org/sakaiproject/nakamura/api/site/event/create") })
 public class GroupEventsPostProcessor implements EventHandler {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(GroupEventsPostProcessor.class);
--- slingtests/osgikernel/bundles/postprocessors/src/main/java/org/sakaiproject/nakamura/postprocessors/GroupMembershipEventsPostProcessor.java
+++ slingtests/osgikernel/bundles/postprocessors/src/main/java/org/sakaiproject/nakamura/postprocessors/GroupMembershipEventsPostProcessor.java
@@ -5,20 +5,22 @@ import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.jackrabbit.api.security.user.Group;
 import org.apache.jackrabbit.api.security.user.User;
 
 import edu.nyu.XythosRemote;
 
-/**
- * @scr.component immediate="true" label="GroupMembershipEventsPostProcessor"
- *                description="handle events when group memberships are modified"
- * @scr.service interface="org.osgi.service.event.EventHandler"
- * @scr.property name="service.description"
- *               value="Provides a place to respond when sites are created and memberships updated"
- * @scr.property name="event.topics" value="org/apache/sling/jackrabbit/usermanager/event/part"
- */
+@Component(enabled = true, immediate = true, metatype = true)
+@Service(value = EventHandler.class)
+@Properties(value = {
+    @Property(name = "service.vendor", value = "New York University"),
+    @Property(name = "service.description", value = "Provides a place to respond when sites are created and memberships updated"),
+    @Property(name = "event.topics", value = "org/apache/sling/jackrabbit/usermanager/event/part") })
 public class GroupMembershipEventsPostProcessor implements EventHandler {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(GroupMembershipEventsPostProcessor.class);
