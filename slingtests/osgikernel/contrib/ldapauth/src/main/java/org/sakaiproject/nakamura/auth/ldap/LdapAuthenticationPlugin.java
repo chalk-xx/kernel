06d11ac65120bdabd8105fb53030e0b7af2ec9d5
Carl Hall <carl@hallwaytech.com>
2010-04-27 19:42:26 -0400

    Started adding user creation to ldap auth for non-exist but verified users.

--- slingtests/osgikernel/contrib/ldapauth/src/main/java/org/sakaiproject/nakamura/auth/ldap/LdapAuthenticationPlugin.java
+++ slingtests/osgikernel/contrib/ldapauth/src/main/java/org/sakaiproject/nakamura/auth/ldap/LdapAuthenticationPlugin.java
@@ -1,23 +1,35 @@
 package org.sakaiproject.nakamura.auth.ldap;
 
 import com.novell.ldap.LDAPConnection;
+import javax.jcr.Session;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Modified;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.commons.osgi.OsgiUtil;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.apache.sling.jcr.base.util.AccessControlUtil;
 import org.apache.sling.jcr.jackrabbit.server.security.AuthenticationPlugin;
 import org.sakaiproject.nakamura.api.ldap.LdapConnectionManager;
 import org.sakaiproject.nakamura.api.ldap.LdapException;
+import org.sakaiproject.nakamura.api.user.UserConstants;
+import org.sakaiproject.nakamura.util.PathUtils;
 
+import java.security.Principal;
 import java.util.Map;
+import java.util.logging.Level;
 
 import javax.jcr.Credentials;
 import javax.jcr.RepositoryException;
 import javax.jcr.SimpleCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Authentication plugin for verifying a user against an LDAP instance.
@@ -25,7 +37,10 @@ import javax.jcr.SimpleCredentials;
 @Component(metatype = true)
 @Service(value = LdapAuthenticationPlugin.class)
 public class LdapAuthenticationPlugin implements AuthenticationPlugin {
-  
+
+  private static final Logger log = LoggerFactory
+      .getLogger(LdapAuthenticationPlugin.class);
+
   public static final String DEFAULT_LDAP_BASE_DN = "uid={}";
   @Property(value = DEFAULT_LDAP_BASE_DN)
   static final String LDAP_BASE_DN = "sakai.auth.ldap.baseDn";
@@ -37,13 +52,16 @@ public class LdapAuthenticationPlugin implements AuthenticationPlugin {
   @Reference
   private LdapConnectionManager connMgr;
 
+  @Reference
+  private SlingRepository repository;
+
   public LdapAuthenticationPlugin() {
   }
 
   LdapAuthenticationPlugin(LdapConnectionManager connMgr) {
     this.connMgr = connMgr;
   }
-  
+
   @Activate
   protected void activate(Map<?, ?> props) {
     baseDn = OsgiUtil.toString(props.get(LDAP_BASE_DN), "");
@@ -54,7 +72,7 @@ public class LdapAuthenticationPlugin implements AuthenticationPlugin {
   protected void modified(Map<?, ?> props) {
     baseDn = OsgiUtil.toString(props.get(LDAP_BASE_DN), null);
   }
-  
+
   public boolean authenticate(Credentials credentials) throws RepositoryException {
     boolean auth = false;
     if (credentials instanceof SimpleCredentials) {
@@ -66,6 +84,8 @@ public class LdapAuthenticationPlugin implements AuthenticationPlugin {
         LDAPConnection conn = connMgr.getBoundConnection(dn, pass);
         auth = true;
         connMgr.returnConnection(conn);
+
+        ensureUserExists(sc.getUserID());
       } catch (LdapException e) {
         throw new RepositoryException(e.getMessage(), e);
       }
@@ -76,5 +96,36 @@ public class LdapAuthenticationPlugin implements AuthenticationPlugin {
   protected String getBaseDn(String userId) {
     String dn = baseDn.replace("{}", userId);
     return dn;
+  }
+
+  private void ensureUserExists(final String principalName) {
+    Session session = null;
+
+    try {
+      session = repository.loginAdministrative(null);
+      
+      UserManager userManager = AccessControlUtil.getUserManager(session);
+      Authorizable authorizable = userManager.getAuthorizable(principalName);
+
+      if (authorizable == null) {
+        // create user
+        log.debug("Createing user {}", principalName);
+        userManager.createUser(principalName,
+            RandomStringUtils.random(32),
+            new Principal() {
+              public String getName() {
+                return principalName;
+              }
+            },
+            PathUtils
+                .getUserPrefix(principalName, UserConstants.DEFAULT_HASH_LEVELS));
+      }
+    } catch (RepositoryException e) {
+      log.error(e.getMessage(), e);
+    } finally {
+      if (session != null) {
+        session.logout();
+      }
+    }
   }
 }
