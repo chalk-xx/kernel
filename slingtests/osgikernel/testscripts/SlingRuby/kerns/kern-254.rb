846c82f235701c0145225aee1de54fe67156c5c5
Ian Boston <ieb@tfd.co.uk>
2010-05-20 10:01:18 +0100

    Fixed poorly written test for multiple runs.

--- slingtests/osgikernel/testscripts/SlingRuby/kerns/kern-254.rb
+++ slingtests/osgikernel/testscripts/SlingRuby/kerns/kern-254.rb
@@ -9,16 +9,18 @@ include SlingSearch
 class TC_Kern254Test < SlingTest
 
   def test_modify_user_after_group_join
-    u = create_user("testuser")
-    g1 = create_group("g-testgroup")
+    m = "1b"+Time.now.to_i.to_s
+	testuser = "testuser#{m}"
+    u = create_user(testuser)
+    g1 = create_group("g-testgroup#{m}")
     g1.add_member(@s, u.name, "user")
-    g2 = create_group("g-testgroup2")
+    g2 = create_group("g-testgroup2#{m}")
     g2.add_member(@s, u.name, "user")
     g2.add_member(@s, g1.name, "group")
     details = g2.details(@s)
     members = details["members"]
     assert_not_nil(members, "Expected a list of members")
-    assert_equal(1, members.select{|m| m == "testuser"}.size, "Expected no dupes")
+    assert_equal(1, members.select{|m| m == testuser}.size, "Expected no dupes "+members.to_s)
   end
 
 end
