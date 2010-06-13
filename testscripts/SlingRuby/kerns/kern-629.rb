d506fa743b16fa5f90c439b8df792f18b5afdef3
Ian Boston <ieb@tfd.co.uk>
2010-06-12 21:12:12 -0600

    KERN-629 Fixed teh TimeBasedACl implementation and included a basic ruby test.

--- testscripts/SlingRuby/kerns/kern-629.rb
+++ testscripts/SlingRuby/kerns/kern-629.rb
@@ -15,21 +15,93 @@ class TC_Kern629Test < Test::Unit::TestCase
   
   # Test Time Based ACLs
   def test_create_timebase_acl_rule
-	user = createUser("1")
+    user = createUser("1")
     node = createNode("1")
-    @s.set_node_acl_rule_entries(node, user, {"jcr:read" => "granted"}, {"rule" => "TestingThatRuleWorks"})
+    res = @s.set_node_acl_rule_entries(node, user, {"jcr:read" => "granted"}, {"rule" => "TestingThatRuleWorks"})
+	assert_equal("200",res.code,"Failed to add Rule ACL "+res.body)	
+	
+	acl = @s.get_node_ruleacl(node)
+	@s.log.info(acl)
+	assert_equal(1,acl.size)
+	
+	ruleace = findRuleAce(acl, user.name)
+	assert_not_nil(ruleace)
+	assert_equal(0,ruleace["order"])
+	granted = ruleace["granted"]
+	assert_equal(1,granted.size)
+	assert_equal("jcr:read",granted[0])
+	denied = ruleace["denied"]
+	assert_nil(denied)
+	assert_equal("TestingThatRuleWorks",ruleace["sakai:rule-processor"])
+	
+	
+	
+	
+  end
+  
+  def findRuleAce(acl, name)
+	acl.each do |k,v|
+	    if ( k.index("sakai-rules:"+name) == 0 && k.length > ("sakai-rules:"+name).length )
+		    return v
+		end
+	end
+	return nil
   end
   
   def test_activate_timebase_acl_active
-	user = createUser("2")
+    user = createUser("2")
     node = createNode("2")
-    @s.set_node_acl_rule_entries(node, user, {"jcr:read" => "granted"}, {"active" => ["20100304/20100404","2010-04-05T01:00:00Z/20100405T020000Z"]})
+    res = @s.set_node_acl_rule_entries(node, user, {"jcr:read" => "granted"}, {"active" => ["20100304/20100404","2010-04-05T01:00:00Z/20100405T020000Z"]})
+	assert_equal("200",res.code,"Failed to add Rule ACL "+res.body)
+	
+	
+	acl = @s.get_node_ruleacl(node)
+	@s.log.info(acl)
+	assert_equal(1,acl.size)
+	
+	ruleace = findRuleAce(acl, user.name)
+	assert_not_nil(ruleace)
+	assert_equal(0,ruleace["order"])
+	granted = ruleace["granted"]
+	assert_equal(1,granted.size)
+	assert_equal("jcr:read",granted[0])
+	denied = ruleace["denied"]
+	assert_nil(denied)
+	active = ruleace["active"]
+	assert_not_nil(active)
+	assert_equal(2, active.size())
+	assert_equal("2010-03-04/2010-04-04", active[0])
+	assert_equal("2010-04-05T01:00:00Z/2010-04-05T02:00:00Z", active[1])
+	inactive = ruleace["inactive"]
+	assert_nil(inactive)
+
   end
   
   def test_deactivate_timebase_acl
-	user = createUser("3")
+    user = createUser("3")
     node = createNode("3")
-    @s.set_node_acl_rule_entries(node, user, {"jcr:read" => "granted"}, {"inactive" => ["20100304/20100404","20100405T010000Z/20100405T020000Z"]})
+    res = @s.set_node_acl_rule_entries(node, user, {"jcr:read" => "granted"}, {"inactive" => ["20100304/20100404","20100405T010000Z/20100405T020000Z"]})
+	assert_equal("200",res.code,"Failed to add Rule ACL "+res.body)
+	acl = @s.get_node_ruleacl(node)
+	@s.log.info(acl)
+	assert_equal(1,acl.size)
+	
+	ruleace = findRuleAce(acl, user.name)
+	assert_not_nil(ruleace)
+	assert_equal(0,ruleace["order"])
+	granted = ruleace["granted"]
+	assert_equal(1,granted.size)
+	assert_equal("jcr:read",granted[0])
+	denied = ruleace["denied"]
+	assert_nil(denied)
+	active = ruleace["inactive"]
+	assert_not_nil(active)
+	assert_equal(2, active.size())
+	assert_equal("2010-03-04/2010-04-04", active[0])
+	assert_equal("2010-04-05T01:00:00Z/2010-04-05T02:00:00Z", active[1])
+	inactive = ruleace["active"]
+	assert_nil(inactive)
+	
   end
   
   def createUser(n) 
@@ -39,9 +111,9 @@ class TC_Kern629Test < Test::Unit::TestCase
   
   def createNode(n) 
     m = Time.now.to_i.to_s
-	path = "/kern-629/testNode-#{n}-#{m}"
+    path = "/kern-629/testNode-#{n}-#{m}"
     @s.create_node(path , {"foo" => "bar"})
-	return path
+    return path
   end
   
   
