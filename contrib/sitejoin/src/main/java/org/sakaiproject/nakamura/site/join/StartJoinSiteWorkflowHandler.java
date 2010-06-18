3bfa88beedb060c0d24c9b9904d0f66d3ff85420
Carl Hall <carl@hallwaytech.com>
2010-06-18 02:27:55 -0600

    Created new project for sitejoin stuff. Moved handler to new project. Added sitejoin to pom.

--- contrib/sitejoin/src/main/java/org/sakaiproject/nakamura/site/join/StartJoinSiteWorkflowHandler.java
+++ contrib/sitejoin/src/main/java/org/sakaiproject/nakamura/site/join/StartJoinSiteWorkflowHandler.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,14 +16,18 @@
  * specific language governing permissions and limitations under
  * the License.
  */
-package org.sakaiproject.nakamura.site;
+package org.sakaiproject.nakamura.site.join;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
+import org.sakaiproject.nakamura.api.message.MessagingService;
 import org.sakaiproject.nakamura.api.site.SiteService.SiteEvent;
+
+import java.util.HashMap;
 
 /**
  *
@@ -33,14 +37,27 @@ import org.sakaiproject.nakamura.api.site.SiteService.SiteEvent;
 @Property(name = EventConstants.EVENT_TOPIC, value = SiteEvent.TOPIC + "startJoinWorkflow")
 public class StartJoinSiteWorkflowHandler implements EventHandler {
 
+  @Reference
+  private MessagingService messagingService;
+
   public void handleEvent(Event event) {
     String sitePath = (String) event.getProperty(SiteEvent.SITE);
     String user = (String) event.getProperty(SiteEvent.USER);
     String group = (String) event.getProperty(SiteEvent.GROUP);
 
-    // make sure user hasn't already posted a request of any status
+    HashMap<String, Object> props = new HashMap<String, Object>();
+    props.put("sakai:type", "internal");
+    props.put("sakai:sendstate", "pending");
+    props.put("sakai:messagebox", "outbox");
+    props.put("sakai:to", siteOwner);
+    props.put("sakai:from", user);
+    props.put("sakai:subject", subject);
+    props.put("sakai:body", body);
+    props.put("_charset_", "utf-8");
+    props.put("sakai:category", "joinrequest");
 
-    // add node to /sites/thissite/requests/h/ha/hash
+    Session session;
+    messagingService.create(session, props);
   }
 
 }
