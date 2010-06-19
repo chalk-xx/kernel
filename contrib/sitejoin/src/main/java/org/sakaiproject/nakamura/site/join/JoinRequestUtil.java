7ee7b1b1be8397954e9d4f9b53a585f18f2e1136
Carl Hall <carl@hallwaytech.com>
2010-06-18 23:42:42 -0600

    Moved join request search pieces to sitejoin bundle. May remove them later. Added some meat to the join workflow handler.

---
+++ contrib/sitejoin/src/main/java/org/sakaiproject/nakamura/site/join/JoinRequestUtil.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The SF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.sakaiproject.nakamura.site.join;
+
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.jcr.base.util.AccessControlUtil;
+import org.sakaiproject.nakamura.util.PathUtils;
+
+import javax.jcr.AccessDeniedException;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+
+public class JoinRequestUtil {
+  public static final String JOIN_REQUESTS = "/joinrequests";
+
+  public static String getPath(String userId, String sitePath, Session session)
+      throws AccessDeniedException, UnsupportedRepositoryOperationException,
+      RepositoryException {
+    sitePath = PathUtils.normalizePath(sitePath);
+    UserManager userManager = AccessControlUtil.getUserManager(session);
+    Authorizable authorizable = userManager.getAuthorizable(userId);
+    String userPath = PathUtils.normalizePath(PathUtils.getSubPath(authorizable));
+    String requestPath = sitePath + JOIN_REQUESTS + userPath;
+    return requestPath;
+  }
+}
--- contrib/sitejoin/src/main/java/org/sakaiproject/nakamura/site/join/search/JoinRequestSearchResultProcessor.java
+++ contrib/sitejoin/src/main/java/org/sakaiproject/nakamura/site/join/search/JoinRequestSearchResultProcessor.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.sakaiproject.nakamura.site.search;
+package org.sakaiproject.nakamura.site.join.search;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.query.Query;
