36f92ace8c02c634e91552d995fde0be51017ef8
Carl Hall <carl@hallwaytech.com>
2010-07-08 12:34:06 -0400

    Renamed contenttype to header and changed it's functionality to return any requested header or all headers if none specified. Updated tests for better coverage.

--- contrib/header/src/main/java/org/sakaiproject/nakamura/header/HeaderProxyPostProcessor.java
+++ contrib/header/src/main/java/org/sakaiproject/nakamura/header/HeaderProxyPostProcessor.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.sakaiproject.nakamura.contenttype;
+package org.sakaiproject.nakamura.header;
 
 import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 
@@ -31,7 +31,9 @@ import org.sakaiproject.nakamura.api.proxy.ProxyPostProcessor;
 import org.sakaiproject.nakamura.api.proxy.ProxyResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Retrieves the content type of a requested URL.
@@ -41,11 +43,8 @@ import java.util.Map;
 @Properties(value = {
     @Property(name = Constants.SERVICE_VENDOR, value = "Sakai Foundation"),
     @Property(name = Constants.SERVICE_DESCRIPTION, value = "Post processor to return content type of requested URL.") })
-public class ContentTypePostProcessor implements ProxyPostProcessor {
-  public static final String NAME = "contenttype";
-
-  protected static final String CONTENT_TYPE_HEADER = "Content-Type";
-  protected static final String CONTENT_TYPE_KEY = "contentType";
+public class HeaderProxyPostProcessor implements ProxyPostProcessor {
+  public static final String NAME = "header";
 
   /**
    * {@inheritDoc}
@@ -53,21 +52,24 @@ public class ContentTypePostProcessor implements ProxyPostProcessor {
    * @see org.sakaiproject.nakamura.api.proxy.ProxyPostProcessor#process(org.apache.sling.api.SlingHttpServletResponse,
    *      org.sakaiproject.nakamura.api.proxy.ProxyResponse)
    */
-  public void process(SlingHttpServletResponse response, ProxyResponse proxyResponse)
-      throws IOException {
-    String contentType = "";
+  public void process(Map<String, Object> templateParams,
+      SlingHttpServletResponse response, ProxyResponse proxyResponse) throws IOException {
+    HashMap<String, String> headers = new HashMap<String, String>();
 
-    Map<String, String[]> headers = proxyResponse.getResponseHeaders();
-    if (headers.containsKey(CONTENT_TYPE_HEADER)) {
-      String[] header = headers.get(CONTENT_TYPE_HEADER);
-      if (header != null) {
-        contentType = headers.get(CONTENT_TYPE_HEADER)[0];
-      }
+    Map<String, String[]> responseHeaders = proxyResponse.getResponseHeaders();
+    Object requestedHeaders = templateParams.get("header");
+
+    if (requestedHeaders instanceof String[]) {
+      headers.putAll(retrieveHeaders(responseHeaders, (String[]) requestedHeaders));
+    } else {
+      headers.putAll(retrieveHeaders(responseHeaders, (String) requestedHeaders));
     }
 
     try {
       JSONObject jsonObject = new JSONObject();
-      jsonObject.put(CONTENT_TYPE_KEY, contentType);
+      for (Entry<String, String> header : headers.entrySet()) {
+        jsonObject.put(header.getKey(), header.getValue());
+      }
       String output = jsonObject.toString();
       response.getWriter().print(output);
 
@@ -86,4 +88,32 @@ public class ContentTypePostProcessor implements ProxyPostProcessor {
     return NAME;
   }
 
+  private Map<String, String> retrieveHeaders(Map<String, String[]> responseHeaders,
+      String... requestedHeaders) {
+    HashMap<String, String> output = new HashMap<String, String>();
+    if (requestedHeaders != null
+        && !(requestedHeaders.length == 1 && requestedHeaders[0] == null)) {
+      for (String requestedHeader : requestedHeaders) {
+        if (responseHeaders.containsKey(requestedHeader)) {
+          String headerValue = "";
+          String[] header = responseHeaders.get(requestedHeader);
+          if (header != null) {
+            headerValue = responseHeaders.get(requestedHeader)[0];
+
+            if (headerValue == null) {
+              headerValue = "";
+            }
+          }
+          output.put(requestedHeader, headerValue);
+        }
+      }
+    } else {
+      for (Entry<String, String[]> responseHeader : responseHeaders.entrySet()) {
+        String headerKey = responseHeader.getKey();
+        String[] headerValue = responseHeader.getValue();
+        output.put(headerKey, headerValue[0]);
+      }
+    }
+    return output;
+  }
 }
