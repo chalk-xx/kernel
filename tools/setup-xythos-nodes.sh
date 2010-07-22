4b64b98f12e7055defb6078329041eb0d8d1d476
Zach A. Thomas <zach@aeroplanesoftware.com>
2010-07-22 16:27:23 -0500

    KERN-887 missed a couple of references to 'xythos' in my previous refactor

--- tools/setup-xythos-nodes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -F"sling:resourceType=sakai/external-repository" -F"jcr:mixinTypes=mix:referenceable" -F"sakai:repository-processor=xythos" http://admin:admin@alexqa.home.nyu.edu:8080/xythos
-UUID=`curl http://alexqa.home.nyu.edu:8080/xythos.json | sed 's/.*uuid"\:"\(.*\)".*/\1/g'`
-curl -F"sling:resourceType=sakai/external-repository-search" -F"sakai:repository-ref=$UUID" -F"sakai:search-prop-q={search|*}" http://admin:admin@alexqa.home.nyu.edu:8080/var/search/xythos
