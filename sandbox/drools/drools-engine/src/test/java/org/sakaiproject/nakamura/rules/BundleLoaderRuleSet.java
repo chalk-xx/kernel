dc430dd179f5a7d972fd34e67f5a8e3e22bc150c
Carl Hall <carl@hallwaytech.com>
2010-07-23 10:15:15 -0400

    Removed @Override from implemented interface methods. This is a Java 1.6 feature.

--- sandbox/drools/drools-engine/src/test/java/org/sakaiproject/nakamura/rules/BundleLoaderRuleSet.java
+++ sandbox/drools/drools-engine/src/test/java/org/sakaiproject/nakamura/rules/BundleLoaderRuleSet.java
@@ -25,12 +25,10 @@ import java.io.InputStream;
 
 public class BundleLoaderRuleSet implements RulePackageLoader{
 
-  @Override
   public InputStream getPackageInputStream() {
    return  this.getClass().getResourceAsStream("/SLING-INF/content/var/rules/org.sakaiproject.nakamura.rules/org.sakaiproject.nakamura.rules.example/0.7-SNAPSHOT/org.sakaiproject.nakamura.rules.example-0.7-SNAPSHOT.pkg");
   }
 
-  @Override
   public ClassLoader getPackageClassLoader() {
     return this.getClass().getClassLoader();
   }
