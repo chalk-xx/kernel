19346ec97b80d18d514f48124de84b683436f70c
Ray Davis <ray@media.berkeley.edu>
2010-08-16 17:40:08 -0700

    NOJIRA Remove unnecessary parameter and check for successful deletion

--- testscripts/SlingRuby/kerns/kern-296.rb
+++ testscripts/SlingRuby/kerns/kern-296.rb
@@ -16,14 +16,15 @@ class TC_Kern296Test < Test::Unit::TestCase
     assert_not_nil(u1,"Expected Group to be created ")
     g1.add_member(@s, u1.name, "user")
     hasmember = g1.has_member(@s, u1.name)
-    assert_equal(true,hasmember,"Expected user #{u1.name} to be a member of group #{g1.name} ") 
+    assert_equal(true,hasmember,"Expected user #{u1.name} to be a member of group #{g1.name} ")
     g1.remove_member(@s,u1.name,"user")
     hasmember = g1.has_member(@s, u1.name)
-    assert_equal(false,hasmember,"Expected user #{u1.name} not to be a member of group #{g1.name} ") 
+    assert_equal(false,hasmember,"Expected user #{u1.name} not to be a member of group #{g1.name} ")
     url = SlingUsers::Group.url_for(g1.name)
-    res = @s.execute_post(@s.url_for("#{url}.delete.html"),
-                                    { "go" => 1 })
-    assert_equal("200",res.code,"Expected delete group to be sucessfull #{res.body}")
+    res = @s.execute_post(@s.url_for("#{url}.delete.html"))
+    assert_equal("200",res.code,"Expected delete group to be sucessful #{res.body}")
+	res =  @s.execute_get(@s.url_for("#{url}.json"))
+	assert_not_equal("200", res.code, "Expected group to be gone: #{res.body}")
   end
 
 end
