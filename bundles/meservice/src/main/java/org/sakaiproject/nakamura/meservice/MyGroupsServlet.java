cea4ef89d76f8fc89015c8b8f915b8911e56330e
Ray Davis <ray@media.berkeley.edu>
2010-09-05 15:57:17 -0700

    KERN-1020 Retrieve filtered profiles of the current user's groups or managed groups; servlet documentation still missing

---
+++ bundles/meservice/src/main/java/org/sakaiproject/nakamura/meservice/MyGroupsServlet.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.nakamura.meservice;
+
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.sakaiproject.nakamura.api.profile.ProfileService;
+import org.sakaiproject.nakamura.api.user.UserConstants;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+
+import javax.jcr.RepositoryException;
+
+@SlingServlet(paths = { "/system/me/groups" }, generateComponent = true, generateService = true, methods = { "GET" })
+@Reference(name="profileService", referenceInterface=ProfileService.class)
+public class MyGroupsServlet extends AbstractMyGroupsServlet {
+  private static final long serialVersionUID = 8809581334593701801L;
+
+  /**
+   * {@inheritDoc}
+   * @see org.sakaiproject.nakamura.meservice.AbstractMyGroupsServlet#getGroups(org.apache.jackrabbit.api.security.user.Authorizable, org.apache.jackrabbit.api.security.user.UserManager)
+   */
+  @Override
+  protected TreeMap<String, Group> getGroups(Authorizable member, UserManager userManager)
+      throws RepositoryException {
+    TreeMap<String, Group> groups = new TreeMap<String, Group>();
+    Iterator<Group> allGroupsIter = member.memberOf();
+    while (allGroupsIter.hasNext()) {
+      Group group = allGroupsIter.next();
+      // Until KERN-950 is fixed, we don't have a foolproof way to know whether
+      // a Jackrabbit Group should be considered a Sakai Group entity.
+      // We skip Managers-holders but otherwise just return a Profile if
+      // we find one.
+      if (!group.hasProperty(UserConstants.PROP_MANAGED_GROUP)) {
+        groups.put(group.getID(), group);
+      }
+    }
+    return groups;
+  }
+
+}
