e7b35d8b3494ae571e6bf4017b3dba5950a447a2
Carl Hall <carl@hallwaytech.com>
2010-09-07 18:26:37 -0400

    KERN-1012 Completely changed search mechanism to preload member IDs then look them up based on quantity of IDs.  This uses the property provider to look things up then pass on for post processing if the number of member IDs is over 900.

--- bundles/search/src/main/java/org/sakaiproject/nakamura/search/processors/GroupMembersSearchPropertyProvider.java
+++ bundles/search/src/main/java/org/sakaiproject/nakamura/search/processors/GroupMembersSearchPropertyProvider.java
@@ -17,15 +17,29 @@
  */
 package org.sakaiproject.nakamura.search.processors;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.jcr.base.util.AccessControlUtil;
 import org.sakaiproject.nakamura.api.search.SearchConstants;
 import org.sakaiproject.nakamura.api.search.SearchPropertyProvider;
+import org.sakaiproject.nakamura.api.user.UserConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.Value;
 
 /**
  *
@@ -37,12 +51,67 @@ import java.util.Map;
     @Property(name = SearchConstants.REG_PROVIDER_NAMES, value = "GroupMembers")
 })
 public class GroupMembersSearchPropertyProvider implements SearchPropertyProvider {
+  private static final Logger logger = LoggerFactory
+      .getLogger(GroupMembersSearchPropertyProvider.class);
 
   /**
    * {@inheritDoc}
-   * @see org.sakaiproject.nakamura.api.search.SearchPropertyProvider#loadUserProperties(org.apache.sling.api.SlingHttpServletRequest, java.util.Map)
+   *
+   * @see org.sakaiproject.nakamura.api.search.SearchPropertyProvider#loadUserProperties(org.apache.sling.api.SlingHttpServletRequest,
+   *      java.util.Map)
    */
   public void loadUserProperties(SlingHttpServletRequest request,
       Map<String, String> propertiesMap) {
+    try {
+      Session session = request.getResourceResolver().adaptTo(Session.class);
+      UserManager um = AccessControlUtil.getUserManager(session);
+
+      // get the request group name
+      String groupName = request.getParameter("group");
+      if (groupName == null) {
+        throw new IllegalArgumentException("Must provide group to search within.");
+      }
+
+      // get the authorizable associated to the requested group name
+      Group group = (Group) um.getAuthorizable(groupName);
+      if (group == null) {
+        throw new IllegalArgumentException("Unable to find group [" + groupName + "]");
+      }
+
+      Iterator<Authorizable> members = group.getDeclaredMembers();
+      ArrayList<String> memberIds = new ArrayList<String>();
+      while (members.hasNext()) {
+        memberIds.add(members.next().getID());
+      }
+
+      // get the managers group for the request group and its members to the list
+      Group managerGroup = null;
+      if (group.hasProperty(UserConstants.PROP_MANAGERS_GROUP)) {
+        Value[] values = group.getProperty(UserConstants.PROP_MANAGERS_GROUP);
+        if ((values != null) && (values.length == 1)) {
+          String managerGroupId = values[0].getString();
+          managerGroup = (Group) um.getAuthorizable(managerGroupId);
+          if (managerGroup != null) {
+            members = managerGroup.getDeclaredMembers();
+            while (members.hasNext()) {
+              memberIds.add(members.next().getID());
+            }
+          } else {
+            logger.warn("Unable to find manager's group [" + managerGroupId + "]");
+          }
+        }
+      }
+
+      if (memberIds.size() > 900) {
+        // more than the threshold; pass along for post processing
+        request.setAttribute("memberIds", memberIds);
+      } else {
+        // update the query to filter before writing nodes
+        propertiesMap.put("_groupQuery",
+            "and (rep:userId='" + StringUtils.join(memberIds, "' or rep:userId='") + "')");
+      }
+    } catch (RepositoryException e) {
+      logger.error(e.getMessage(), e);
+    }
   }
 }
--- bundles/search/src/main/java/org/sakaiproject/nakamura/search/processors/GroupMembersSearchResultProcessor.java
+++ bundles/search/src/main/java/org/sakaiproject/nakamura/search/processors/GroupMembersSearchResultProcessor.java
@@ -20,29 +20,23 @@ package org.sakaiproject.nakamura.search.processors;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
-import org.apache.jackrabbit.api.security.user.Authorizable;
-import org.apache.jackrabbit.api.security.user.Group;
-import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.commons.json.JSONException;
-import org.apache.sling.commons.json.io.JSONWriter;
-import org.apache.sling.jcr.base.util.AccessControlUtil;
-import org.sakaiproject.nakamura.api.search.Aggregator;
 import org.sakaiproject.nakamura.api.search.SearchConstants;
 import org.sakaiproject.nakamura.api.search.SearchException;
-import org.sakaiproject.nakamura.api.search.SearchResultProcessor;
 import org.sakaiproject.nakamura.api.search.SearchResultSet;
-import org.sakaiproject.nakamura.api.search.SearchServiceFactory;
 import org.sakaiproject.nakamura.api.search.SearchUtil;
-import org.sakaiproject.nakamura.util.ExtendedJSONWriter;
+import org.sakaiproject.nakamura.search.SakaiSearchRowIterator;
+import org.sakaiproject.nakamura.search.SearchResultSetImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
-import javax.jcr.Session;
 import javax.jcr.query.Query;
-import javax.jcr.query.Row;
+import javax.jcr.query.QueryResult;
 
 /**
  *
@@ -51,61 +45,11 @@ import javax.jcr.query.Row;
 @Service
 @Properties(value = {
     @Property(name = "service.vendor", value = "The Sakai Foundation"),
-    @Property(name = SearchConstants.REG_PROCESSOR_NAMES, value = "GroupMembers")
+    @Property(name = SearchConstants.REG_BATCH_PROCESSOR_NAMES, value = "GroupMembers")
 })
-public class GroupMembersSearchResultProcessor implements SearchResultProcessor {
-
-  @Reference
-  private SearchServiceFactory searchServiceFactory;
-
-  GroupMembersSearchResultProcessor(SearchServiceFactory searchServiceFactory) {
-    if ( searchServiceFactory == null ) {
-      throw new IllegalArgumentException("Search Service Factory must be set when not using as a component");
-    }
-    this.searchServiceFactory = searchServiceFactory;
-  }
-
-  public GroupMembersSearchResultProcessor() {
-  }
-
-  /**
-   * {@inheritDoc}
-   *
-   * @see org.sakaiproject.nakamura.api.search.SearchResultProcessor#writeNode(org.apache.sling.api.SlingHttpServletRequest,
-   *      org.apache.sling.commons.json.io.JSONWriter,
-   *      org.sakaiproject.nakamura.api.search.Aggregator, javax.jcr.query.Row)
-   */
-  public void writeNode(SlingHttpServletRequest request, JSONWriter write,
-      Aggregator aggregator, Row row) throws JSONException, RepositoryException {
-    Node node = row.getNode();
-
-    Session session = request.getResourceResolver().adaptTo(Session.class);
-    UserManager um = AccessControlUtil.getUserManager(session);
-
-    // get the request group name
-    String groupName = request.getParameter("group");
-    if (groupName == null) {
-      throw new IllegalArgumentException("Must provide group to search within.");
-    }
-
-    // get the authorizable associated to the requested group name
-    Group group = (Group) um.getAuthorizable(groupName);
-    if (group == null) {
-      throw new IllegalArgumentException("Unable to find group [" + groupName + "]");
-    }
-
-    String username = node.getProperty("rep:userId").getString();
-    Authorizable user = um.getAuthorizable(username);
-
-    if (group.isMember(user)) {
-      if (aggregator != null) {
-        aggregator.add(node);
-      }
-
-      int maxTraversalDepth = SearchUtil.getTraversalDepth(request);
-      ExtendedJSONWriter.writeNodeTreeToWriter(write, node, maxTraversalDepth);
-    }
-  }
+public class GroupMembersSearchResultProcessor extends NodeSearchBatchResultProcessor {
+  private static final Logger logger = LoggerFactory
+      .getLogger(GroupMembersSearchResultProcessor.class);
 
   /**
    * {@inheritDoc}
@@ -113,8 +57,49 @@ public class GroupMembersSearchResultProcessor implements SearchResultProcessor 
    * @see org.sakaiproject.nakamura.api.search.SearchResultProcessor#getSearchResultSet(org.apache.sling.api.SlingHttpServletRequest,
    *      javax.jcr.query.Query)
    */
+  @Override
   public SearchResultSet getSearchResultSet(SlingHttpServletRequest request,
       Query query) throws SearchException {
-    return searchServiceFactory.getSearchResultSet(request, query);
+    try {
+      // Get the query result.
+      QueryResult rs = query.execute();
+
+      // Extract the total hits from lucene
+      long hits = SearchUtil.getHits(rs);
+      int maxResults = (int) SearchUtil.longRequestParameter(request, SearchConstants.PARAM_MAX_RESULT_SET_COUNT, 100);
+
+      final List<String> memberIds = (List<String>) request.getAttribute("memberIds");
+
+      // Do the paging on the iterator.
+      SakaiSearchRowIterator iterator = new SakaiSearchRowIterator(rs.getRows()) {
+
+        @Override
+        protected boolean isValid(Node node) {
+          try {
+            if (memberIds != null && node.hasProperty("rep:userId")) {
+              boolean isMember = false;
+              String username = node.getProperty("rep:userId").getString();
+              if (memberIds.contains(username)) {
+                isMember = true;
+              }
+              return isMember && super.isValid(node);
+            } else {
+              return super.isValid(node);
+            }
+          } catch (RepositoryException e) {
+            return false;
+          }
+        }
+      };
+      long start = SearchUtil.getPaging(request, hits);
+      iterator.skip(start);
+
+      // Return the result set.
+      SearchResultSet srs = new SearchResultSetImpl(iterator, hits, maxResults);
+      return srs;
+    } catch (RepositoryException e) {
+      logger.error("Unable to perform query.", e);
+      throw new SearchException(500, "Unable to perform query.");
+    }
   }
 }
