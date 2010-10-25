d3a372e9e4dd2aa60f8ff0f16ef3813d9ad1b240
Ian Boston <ieb@tfd.co.uk>
2010-10-25 15:10:45 +0100

    Fixed breakage in the server caused by running the widgets integration tests

--- testscripts/SlingRuby/kerns/kern-857.rb
+++ testscripts/SlingRuby/kerns/kern-857.rb
@@ -20,7 +20,7 @@ class TC_Kern857 < Test::Unit::TestCase
     folders = @widget_options["sakai.batch.widgets.widget_folders"]["values"]
 
     # Add our own widgets to it.
-    folders = [widgetsPath]
+    folders.concat([widgetsPath])
     setWidgetServiceConfigurationPath(folders)
 
     # Create a couple of widgets in this directory.
