1d26e2b5a081bc2daea5c27e073d6a63ff47230d
Carl Hall <carl@hallwaytech.com>
2010-11-10 19:32:40 -0500

    KERN-1318 Added checks for RSS 0.91, 1.0, 2.0; ATOM 1.0. Changed 500's to 400's where applicable.

--- bundles/proxy/src/main/java/org/sakaiproject/nakamura/proxy/RSSProxyPostProcessor.java
+++ bundles/proxy/src/main/java/org/sakaiproject/nakamura/proxy/RSSProxyPostProcessor.java
@@ -21,7 +21,9 @@ import com.ctc.wstx.stax.WstxInputFactory;
 import com.ctc.wstx.stax.WstxOutputFactory;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
@@ -38,16 +40,20 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
@@ -81,6 +87,9 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
 
   private List<String> contentTypes;
 
+  private Map<String, Set<String>> formats;
+
+  @Activate
   protected void activate(Map<?, ?> props) {
     eventsThreshold = OsgiUtil.toInteger(props.get(EVENTS_THRESHOLD),
         DEFAULT_EVENTS_THRESHOLD);
@@ -98,8 +107,43 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
     contentTypes.add("application/xhtml+xml");
     contentTypes.add("application/xml");
     contentTypes.add("text/plain");
+
+    formats = new HashMap<String, Set<String>>();
+
+    // RSS 0.91
+    HashSet<String> rss091 = new HashSet<String>();
+    rss091.add("channel");
+    rss091.add("title");
+    rss091.add("description");
+    rss091.add("link");
+    rss091.add("language");
+    formats.put("rss-0.91", rss091);
+
+    // RSS 1.0
+    HashSet<String> rss10 = new HashSet<String>();
+    rss10.add("channel");
+    rss10.add("title");
+    rss10.add("description");
+    rss10.add("link");
+    formats.put("rdf", rss10);
+
+    // RSS 2.0
+    HashSet<String> rss20 = new HashSet<String>();
+    rss20.add("channel");
+    rss20.add("title");
+    rss20.add("description");
+    rss20.add("link");
+    formats.put("rss-2.0", rss20);
+
+    // ATOM 1.0
+    HashSet<String> atom10 = new HashSet<String>();
+    atom10.add("id");
+    atom10.add("title");
+    atom10.add("updated");
+    formats.put("feed", atom10);
   }
 
+  @Deactivate
   protected void deactivate(ComponentContext ctxt) {
     this.xmlInputFactory = null;
 
@@ -115,8 +159,7 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
   public void process(Map<String, Object> templateParams,
       SlingHttpServletResponse response, ProxyResponse proxyResponse) throws IOException {
     if ( proxyResponse.getResultCode() == HttpServletResponse.SC_PRECONDITION_FAILED ) {
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-          "This RSS feed is too big ");
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST, "This RSS feed is too big ");
       return;
     }
 
@@ -127,7 +170,7 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
     if (contentLengthHeader != null) {
       int length = Integer.parseInt(contentLengthHeader[0]);
       if (length > maxLength) {
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST,
             "This RSS feed is too big. The maximum for a feed is: " + maxLength);
         return;
       }
@@ -141,7 +184,7 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
         contentType = contentType.substring(0, contentType.indexOf(';'));
       }
       if (!contentTypes.contains(contentType)) {
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST,
             "This URL doesn't send a proper Content-Type back");
         return;
       }
@@ -161,15 +204,7 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
       // Create a temporary outputstream where we can write to.
       out = new ByteArrayOutputStream();
 
-
-      Map<String, Boolean> checkedElements = new HashMap<String, Boolean>();
-      checkedElements.put("rss", false);
-      checkedElements.put("channel", false);
-      checkedElements.put("title", false);
-      checkedElements.put("link", false);
-      checkedElements.put("item", false);
-      checkedElements.put("title", false);
-      checkedElements.put("link", false);
+      Set<String> checkedElements = null;
 
       XMLOutputFactory outputFactory = new WstxOutputFactory();
       writer = outputFactory.createXMLEventWriter(out);
@@ -182,25 +217,26 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
         if (!isValid) {
           if (e.getEventType() == XMLEvent.START_ELEMENT) {
             StartElement el = e.asStartElement();
-            String name = el.getName().getLocalPart().toString().toLowerCase();
-            if (checkedElements.containsKey(name)) {
-              checkedElements.put(name, true);
-            }
+            String name = el.getName().getLocalPart().toLowerCase();
+            if (checkedElements == null) {
+              // get the right format to validate against
+              String formatKey = name;
+              Attribute attr = el.getAttributeByName(new QName("version"));
+              if (attr != null) {
+                formatKey += "-" + attr.getValue();
+              }
+              checkedElements = new HashSet<String>(formats.get(formatKey));
+            } else {
+              checkedElements.remove(name);
 
-            boolean all = true;
-            for (Entry<String, Boolean> es : checkedElements.entrySet()) {
-              if (!checkedElements.get(es.getKey())) {
-                all = false;
-                break;
+              if (checkedElements.isEmpty()) {
+                isValid = true;
               }
             }
-            if (all)
-              isValid = true;
-
           }
 
           if (i > eventsThreshold) {
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,
                 "This file is too complex.");
             return;
           }
@@ -208,16 +244,16 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
         }
       }
 
-      logger.info("{} elements processed.", i);
-
       if (!isValid) {
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Invalid RSS file.");
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+            "Invalid RSS file.");
         return;
       }
 
       // Check if we are not streaming a gigantic file..
       if (out.size() > maxLength) {
-        response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "This file is too big.");
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+            "This file is too big.");
         return;
       }
 
@@ -228,16 +264,17 @@ public class RSSProxyPostProcessor implements ProxyPostProcessor {
       }
       // We always return 200 when we get to this point.
       response.setStatus(200);
-      response.setHeader("Content-Length", "" + out.size());
+      response.setHeader("Content-Length", Integer.toString(out.size()));
       // Write the cached stream to the output.
       out.writeTo(response.getOutputStream());
 
     } catch (XMLStreamException e) {
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST,
           "This is not a valid XML file.");
     } catch (Exception e) {
       logger.warn("Exception reading RSS feed.", e);
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "General exception caught.");
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+          "General exception caught.");
     } finally {
       out.close();
       reader.close();
