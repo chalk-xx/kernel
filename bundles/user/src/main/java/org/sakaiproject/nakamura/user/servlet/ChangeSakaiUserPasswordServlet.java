21cd7938163c268731162bc1612ca86e51f979dc
Ian Boston <ieb@tfd.co.uk>
2010-11-23 09:38:51 +0000

    Fixed change password functionallity. Again, Sling encode passwords in various places so the ChageUserPasswordServlet needed to be changed to present a unified view of into the authorizable manager and not allow passwords to travel too far through the infrastrucutre in unencrypted form.

--- bundles/user/src/main/java/org/sakaiproject/nakamura/user/servlet/ChangeSakaiUserPasswordServlet.java
+++ bundles/user/src/main/java/org/sakaiproject/nakamura/user/servlet/ChangeSakaiUserPasswordServlet.java
@@ -17,7 +17,21 @@
  */
 package org.sakaiproject.nakamura.user.servlet;
 
+import java.util.List;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.User;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceNotFoundException;
+import org.apache.sling.api.servlets.HtmlResponse;
+import org.apache.sling.jackrabbit.usermanager.impl.post.AbstractUserPostServlet;
 import org.apache.sling.jackrabbit.usermanager.impl.post.ChangeUserPasswordServlet;
+import org.apache.sling.servlets.post.Modification;
+import org.osgi.service.component.ComponentContext;
 import org.sakaiproject.nakamura.api.doc.BindingType;
 import org.sakaiproject.nakamura.api.doc.ServiceBinding;
 import org.sakaiproject.nakamura.api.doc.ServiceDocumentation;
@@ -65,14 +79,12 @@ import org.sakaiproject.nakamura.api.doc.ServiceSelector;
  *
  * <h4>Notes</h4>
  *
- *
  * @scr.component metatype="no" immediate="true"
  * @scr.service interface="javax.servlet.Servlet"
  * @scr.property name="sling.servlet.resourceTypes" value="sling/user"
  * @scr.property name="sling.servlet.methods" value="POST"
  * @scr.property name="sling.servlet.selectors" value="changePassword"
  */
-
 @ServiceDocumentation(name="Change Password Servlet",
     description="Changes user password. Maps on to nodes of resourceType sling/user " +
     		"like /rep:system/rep:userManager/rep:users/ae/fd/3e/username mapped to a resource " +
@@ -96,14 +108,86 @@ import org.sakaiproject.nakamura.api.doc.ServiceSelector;
         @ServiceResponse(code=200,description="Sucess sent with no body."),
         @ServiceResponse(code=404,description="User was not found."),
         @ServiceResponse(code=500,description="Failure with HTML explanation.")
-    }))   
-    
-
-public class ChangeSakaiUserPasswordServlet extends ChangeUserPasswordServlet {
+    }))
+public class ChangeSakaiUserPasswordServlet extends ChangeUserPasswordServlet	 {
 
   /**
    *
    */
   private static final long serialVersionUID = 7178297046643735984L;
+
+  /*
+   * (non-Javadoc)
+   * @see
+   * org.apache.sling.jackrabbit.usermanager.post.AbstractAuthorizablePostServlet
+   * #handleOperation(org.apache.sling.api.SlingHttpServletRequest,
+   * org.apache.sling.api.servlets.HtmlResponse, java.util.List)
+   */
+  @Override
+  protected void handleOperation(SlingHttpServletRequest request,
+          HtmlResponse htmlResponse, List<Modification> changes)
+          throws RepositoryException {
+      Authorizable authorizable = null;
+      Resource resource = request.getResource();
+      if (resource != null) {
+          authorizable = resource.adaptTo(Authorizable.class);
+      }
+
+      // check that the user was located.
+      if (authorizable == null || authorizable.isGroup()) {
+          throw new ResourceNotFoundException(
+              "User to update could not be determined.");
+      }
+
+      if ("anonymous".equals(authorizable.getID())) {
+          throw new RepositoryException(
+              "Can not change the password of the anonymous user.");
+      }
+
+      Session session = request.getResourceResolver().adaptTo(Session.class);
+      if (session == null) {
+          throw new RepositoryException("JCR Session not found");
+      }
+
+      // check that the submitted parameter values have valid values.
+      String oldPwd = request.getParameter("oldPwd");
+      if (oldPwd == null || oldPwd.length() == 0) {
+          throw new RepositoryException("Old Password was not submitted");
+      }
+      String newPwd = request.getParameter("newPwd");
+      if (newPwd == null || newPwd.length() == 0) {
+          throw new RepositoryException("New Password was not submitted");
+      }
+      String newPwdConfirm = request.getParameter("newPwdConfirm");
+      if (!newPwd.equals(newPwdConfirm)) {
+          throw new RepositoryException(
+              "New Password does not match the confirmation password");
+      }
+
+
+      try {
+    	  // this is ugly by the JR apis are not able to validate a password at a lower level and the digest 
+    	  // of the password is an out of bound agreement. :oldpassword is checked on changePassword and then 
+    	  // cleared.
+    	  authorizable.setProperty(":oldpassword", session.getValueFactory().createValue(digestPassword(oldPwd)));
+          ((User) authorizable).changePassword(digestPassword(newPwd));
+
+          changes.add(Modification.onModified(resource.getPath()
+              + "/rep:password"));
+      } catch (RepositoryException re) {
+          throw new RepositoryException("Failed to change user password.", re);
+      }
+  }
+  
+  @Override
+  protected void activate(ComponentContext context) {
+	  System.err.println("Properties are "+context.getProperties());
+	  super.activate(context);
+  }
+  
+  @Override
+	protected void deactivate(ComponentContext context) {
+		super.deactivate(context);
+	}
 
 }
