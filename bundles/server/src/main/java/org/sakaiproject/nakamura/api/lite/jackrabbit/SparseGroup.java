cefb080147574e4935d10131b7feb8e84b93a277
Ian Boston <ieb@tfd.co.uk>
2010-11-28 16:39:05 +0000

    Fixed formatting, code style and license headers in the sparse user manager.

--- bundles/server/src/main/java/org/sakaiproject/nakamura/api/lite/jackrabbit/SparseGroup.java
+++ bundles/server/src/main/java/org/sakaiproject/nakamura/api/lite/jackrabbit/SparseGroup.java
@@ -1,12 +1,21 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.sakaiproject.nakamura.api.lite.jackrabbit;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-
-import javax.jcr.RepositoryException;
-import javax.jcr.ValueFactory;
 
 import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.Group;
@@ -18,189 +27,188 @@ import org.sakaiproject.nakamura.api.lite.util.Iterables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.ValueFactory;
+
 public class SparseGroup extends SparseAuthorizable implements Group {
 
-	protected static final Logger LOGGER = LoggerFactory
-			.getLogger(SparseGroup.class);
+  protected static final Logger LOGGER = LoggerFactory.getLogger(SparseGroup.class);
 
-	public SparseGroup(
-			org.sakaiproject.nakamura.api.lite.authorizable.Group group,
-			AuthorizableManager authorizableManager,
-			AccessControlManager accessControlManager, ValueFactory valueFactory) {
-		super(group, authorizableManager, accessControlManager, valueFactory);
-		this.principal = new SparsePrincipal(group.getId(), this.getClass().getName(), SparseMapUserManager.GROUPS_PATH);
-	}
+  public SparseGroup(org.sakaiproject.nakamura.api.lite.authorizable.Group group,
+      AuthorizableManager authorizableManager, AccessControlManager accessControlManager,
+      ValueFactory valueFactory) {
+    super(group, authorizableManager, accessControlManager, valueFactory);
+    this.principal = new SparsePrincipal(group.getId(), this.getClass().getName(),
+        SparseMapUserManager.GROUPS_PATH);
+  }
 
-	public Iterator<Authorizable> getDeclaredMembers()
-			throws RepositoryException {
-		final Iterator<String> memberIterator = Iterables.of(
-				getSparseGroup().getMembers()).iterator();
-		return new Iterator<Authorizable>() {
+  public Iterator<Authorizable> getDeclaredMembers() throws RepositoryException {
+    final Iterator<String> memberIterator = Iterables.of(getSparseGroup().getMembers())
+        .iterator();
+    return new Iterator<Authorizable>() {
 
-			private SparseAuthorizable authorizble;
+      private SparseAuthorizable authorizble;
 
-			public boolean hasNext() {
-				while (memberIterator.hasNext()) {
-					try {
-						String id = memberIterator.next();
-						org.sakaiproject.nakamura.api.lite.authorizable.Authorizable a = authorizableManager
-								.findAuthorizable(id);
-						if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.Group) {
-							authorizble = new SparseGroup(
-									(org.sakaiproject.nakamura.api.lite.authorizable.Group) a,
-									authorizableManager, accessControlManager,
-									valueFactory);
-							return true;
-						} else if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.User) {
-							authorizble = new SparseUser(
-									(org.sakaiproject.nakamura.api.lite.authorizable.User) a,
-									authorizableManager, accessControlManager,
-									valueFactory);
-							return true;
-						}
-					} catch (AccessDeniedException e) {
-						LOGGER.debug(e.getMessage(), e);
-					} catch (StorageClientException e) {
-						LOGGER.debug(e.getMessage(), e);
-					}
+      public boolean hasNext() {
+        while (memberIterator.hasNext()) {
+          try {
+            String id = memberIterator.next();
+            org.sakaiproject.nakamura.api.lite.authorizable.Authorizable a = authorizableManager
+                .findAuthorizable(id);
+            if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.Group) {
+              authorizble = new SparseGroup(
+                  (org.sakaiproject.nakamura.api.lite.authorizable.Group) a,
+                  authorizableManager, accessControlManager, valueFactory);
+              return true;
+            } else if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.User) {
+              authorizble = new SparseUser(
+                  (org.sakaiproject.nakamura.api.lite.authorizable.User) a,
+                  authorizableManager, accessControlManager, valueFactory);
+              return true;
+            }
+          } catch (AccessDeniedException e) {
+            LOGGER.debug(e.getMessage(), e);
+          } catch (StorageClientException e) {
+            LOGGER.debug(e.getMessage(), e);
+          }
 
-				}
-				return false;
-			}
+        }
+        return false;
+      }
 
-			public Authorizable next() {
-				return authorizble;
-			}
+      public Authorizable next() {
+        return authorizble;
+      }
 
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-		};
-	}
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+    };
+  }
 
-	public Iterator<Authorizable> getMembers() throws RepositoryException {
-		final List<String> memberIds = new ArrayList<String>();
-		Collections.addAll(memberIds, getSparseGroup().getMembers());
-		return new Iterator<Authorizable>() {
+  public Iterator<Authorizable> getMembers() throws RepositoryException {
+    final List<String> memberIds = new ArrayList<String>();
+    Collections.addAll(memberIds, getSparseGroup().getMembers());
+    return new Iterator<Authorizable>() {
 
-			private int p;
-			private SparseAuthorizable authorizable;
+      private int p;
+      private SparseAuthorizable authorizable;
 
-			public boolean hasNext() {
-				while (p < memberIds.size()) {
-					String id = memberIds.get(p);
-					p++;
-					try {
-						org.sakaiproject.nakamura.api.lite.authorizable.Authorizable a = authorizableManager
-								.findAuthorizable(id);
-						if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.Group) {
-							authorizable = new SparseGroup(
-									(org.sakaiproject.nakamura.api.lite.authorizable.Group) a,
-									authorizableManager, accessControlManager,
-									valueFactory);
-							for (String pid : a.getPrincipals()) {
-								if (!memberIds.contains(pid)) {
-									memberIds.add(pid);
-								}
-							}
-							return true;
-						} else if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.User) {
-							authorizable = new SparseUser(
-									(org.sakaiproject.nakamura.api.lite.authorizable.User) a,
-									authorizableManager, accessControlManager,
-									valueFactory);
-							return true;
-						}
-					} catch (AccessDeniedException e) {
-						LOGGER.debug(e.getMessage(), e);
-					} catch (StorageClientException e) {
-						LOGGER.debug(e.getMessage(), e);
-					}
-				}
-				return false;
-			}
+      public boolean hasNext() {
+        while (p < memberIds.size()) {
+          String id = memberIds.get(p);
+          p++;
+          try {
+            org.sakaiproject.nakamura.api.lite.authorizable.Authorizable a = authorizableManager
+                .findAuthorizable(id);
+            if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.Group) {
+              authorizable = new SparseGroup(
+                  (org.sakaiproject.nakamura.api.lite.authorizable.Group) a,
+                  authorizableManager, accessControlManager, valueFactory);
+              for (String pid : a.getPrincipals()) {
+                if (!memberIds.contains(pid)) {
+                  memberIds.add(pid);
+                }
+              }
+              return true;
+            } else if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.User) {
+              authorizable = new SparseUser(
+                  (org.sakaiproject.nakamura.api.lite.authorizable.User) a,
+                  authorizableManager, accessControlManager, valueFactory);
+              return true;
+            }
+          } catch (AccessDeniedException e) {
+            LOGGER.debug(e.getMessage(), e);
+          } catch (StorageClientException e) {
+            LOGGER.debug(e.getMessage(), e);
+          }
+        }
+        return false;
+      }
 
-			public Authorizable next() {
-				return authorizable;
-			}
+      public Authorizable next() {
+        return authorizable;
+      }
 
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-		};
-	}
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+    };
+  }
 
-	public boolean isMember(Authorizable authorizable)
-			throws RepositoryException {
-		String id = authorizable.getID();
-		for (String s : getSparseGroup().getMembers()) {
-			if (id.equals(s)) {
-				return true;
-			}
-		}
-		final List<String> memberIds = new ArrayList<String>();
-		Collections.addAll(memberIds, getSparseGroup().getMembers());
-		int p = 0;
-		while (p < memberIds.size()) {
-			String s = memberIds.get(p);
-			p++;
-			if (id.equals(s)) {
-				return true;
-			}
+  public boolean isMember(Authorizable authorizable) throws RepositoryException {
+    String id = authorizable.getID();
+    for (String s : getSparseGroup().getMembers()) {
+      if (id.equals(s)) {
+        return true;
+      }
+    }
+    final List<String> memberIds = new ArrayList<String>();
+    Collections.addAll(memberIds, getSparseGroup().getMembers());
+    int p = 0;
+    while (p < memberIds.size()) {
+      String s = memberIds.get(p);
+      p++;
+      if (id.equals(s)) {
+        return true;
+      }
 
-			try {
-				org.sakaiproject.nakamura.api.lite.authorizable.Authorizable a = authorizableManager
-						.findAuthorizable(s);
-				if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.Group) {
-					for (String mid : ((org.sakaiproject.nakamura.api.lite.authorizable.Group) a)
-							.getMembers()) {
-						if (!memberIds.contains(mid)) {
-							memberIds.add(mid);
-						}
-					}
-				}
-			} catch (AccessDeniedException e) {
-				LOGGER.debug(e.getMessage(), e);
-			} catch (StorageClientException e) {
-				LOGGER.debug(e.getMessage(), e);
-			}
-		}
-		return false;
-	}
+      try {
+        org.sakaiproject.nakamura.api.lite.authorizable.Authorizable a = authorizableManager
+            .findAuthorizable(s);
+        if (a instanceof org.sakaiproject.nakamura.api.lite.authorizable.Group) {
+          for (String mid : ((org.sakaiproject.nakamura.api.lite.authorizable.Group) a)
+              .getMembers()) {
+            if (!memberIds.contains(mid)) {
+              memberIds.add(mid);
+            }
+          }
+        }
+      } catch (AccessDeniedException e) {
+        LOGGER.debug(e.getMessage(), e);
+      } catch (StorageClientException e) {
+        LOGGER.debug(e.getMessage(), e);
+      }
+    }
+    return false;
+  }
 
-	public boolean addMember(Authorizable authorizable)
-			throws RepositoryException {
-		String id = authorizable.getID();
-		for (String member : getSparseGroup().getMembers()) {
-			if (id.equals(member)) {
-				return false;
-			}
-		}
-		getSparseGroup().addMember(id);
-		save();
-		return false;
-	}
+  public boolean addMember(Authorizable authorizable) throws RepositoryException {
+    String id = authorizable.getID();
+    for (String member : getSparseGroup().getMembers()) {
+      if (id.equals(member)) {
+        return false;
+      }
+    }
+    getSparseGroup().addMember(id);
+    save();
+    return false;
+  }
 
-	public boolean removeMember(Authorizable authorizable)
-			throws RepositoryException {
-		String id = authorizable.getID();
-		for (String member : getSparseGroup().getMembers()) {
-			if (id.equals(member)) {
-				getSparseGroup().removeMember(id);
-				save();
-				return true;
-			}
-		}
-		return false;
-	}
+  public boolean removeMember(Authorizable authorizable) throws RepositoryException {
+    String id = authorizable.getID();
+    for (String member : getSparseGroup().getMembers()) {
+      if (id.equals(member)) {
+        getSparseGroup().removeMember(id);
+        save();
+        return true;
+      }
+    }
+    return false;
+  }
 
-	private org.sakaiproject.nakamura.api.lite.authorizable.Group getSparseGroup() {
-		return (org.sakaiproject.nakamura.api.lite.authorizable.Group) sparseAuthorizable;
-	}
-	
-	@Override
-	public boolean isGroup() {
-		return true;
-	}
+  private org.sakaiproject.nakamura.api.lite.authorizable.Group getSparseGroup() {
+    return (org.sakaiproject.nakamura.api.lite.authorizable.Group) sparseAuthorizable;
+  }
+
+  @Override
+  public boolean isGroup() {
+    return true;
+  }
 
 }
--- bundles/server/src/main/java/org/sakaiproject/nakamura/api/lite/jackrabbit/SparsePrincipal.java
+++ bundles/server/src/main/java/org/sakaiproject/nakamura/api/lite/jackrabbit/SparsePrincipal.java
@@ -1,8 +1,21 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.sakaiproject.nakamura.api.lite.jackrabbit;
-
-import java.security.Principal;
-
-import javax.jcr.RepositoryException;
 
 import org.apache.jackrabbit.api.security.principal.ItemBasedPrincipal;
 import org.apache.jackrabbit.api.security.principal.JackrabbitPrincipal;
@@ -10,59 +23,65 @@ import org.sakaiproject.nakamura.api.lite.StorageClientUtils;
 import org.sakaiproject.nakamura.api.lite.authorizable.Authorizable;
 import org.sakaiproject.nakamura.api.lite.authorizable.Group;
 
+import java.security.Principal;
+
+import javax.jcr.RepositoryException;
+
 public class SparsePrincipal implements ItemBasedPrincipal {
 
-	private String principalId;
-	private String path;
-	private String location;
-	public static final String USER_REPO_LOCATION = "/rep:security/rep:authorizables/rep:users";
-	public static final String GROUP_REPO_LOCATION = "/rep:security/rep:authorizables/rep:groups";
-	
-	public SparsePrincipal(String principalId, String location, String basePath) {
-		this.principalId = principalId;
-		this.path = basePath +"/"+StorageClientUtils.shardPath(principalId);
-		this.location = location;
+  private String principalId;
+  private String path;
+  private String location;
+  public static final String USER_REPO_LOCATION = "/rep:security/rep:authorizables/rep:users";
+  public static final String GROUP_REPO_LOCATION = "/rep:security/rep:authorizables/rep:groups";
 
-	}
+  public SparsePrincipal(String principalId, String location, String basePath) {
+    this.principalId = principalId;
+    this.path = basePath + "/" + StorageClientUtils.shardPath(principalId);
+    this.location = location;
 
-	public SparsePrincipal(Authorizable ath, String location) {
-		this(ath.getId(),location,(ath instanceof Group)?GROUP_REPO_LOCATION:USER_REPO_LOCATION);
-	}
+  }
 
-	public String getName() {
-		return principalId;
-	}
+  public SparsePrincipal(Authorizable ath, String location) {
+    this(ath.getId(), location, (ath instanceof Group) ? GROUP_REPO_LOCATION
+                                                      : USER_REPO_LOCATION);
+  }
 
-	public String getPath() throws RepositoryException {
-		return path;
-	}
-	
-	@Override
-	public String toString() {
-		return "sparse:"+principalId+" from "+location;
-	}
-	
-	   //-------------------------------------------------------------< Object >---
-    /**
-     * Two principals are equal, if their names are.
-     * @see Object#equals(Object)
-     */
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj instanceof JackrabbitPrincipal) {
-            return principalId.equals(((Principal) obj).getName());
-        }
-        return false;
+  public String getName() {
+    return principalId;
+  }
+
+  public String getPath() throws RepositoryException {
+    return path;
+  }
+
+  @Override
+  public String toString() {
+    return "sparse:" + principalId + " from " + location;
+  }
+
+  // -------------------------------------------------------------< Object >---
+  /**
+   * Two principals are equal, if their names are.
+   * 
+   * @see Object#equals(Object)
+   */
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
-
-    /**
-     * @return the hash code of the principals name.
-     * @see Object#hashCode()
-     */
-    public int hashCode() {
-        return principalId.hashCode();
+    if (obj instanceof JackrabbitPrincipal) {
+      return principalId.equals(((Principal) obj).getName());
     }
+    return false;
+  }
+
+  /**
+   * @return the hash code of the principals name.
+   * @see Object#hashCode()
+   */
+  public int hashCode() {
+    return principalId.hashCode();
+  }
 
 }
