a01e159cb4916304588c6d2ab812c3c9792ccb36
Ray Davis <ray@media.berkeley.edu>
2010-11-30 15:05:29 -0800

    KERN-1376 Add search query to collect activity feed from a pooled content path

---
+++ testscripts/SlingRuby/kerns/kern-1376.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+# Add all files in testscripts\SlingRuby\lib directory to ruby "require" search path
+require 'ruby-lib-dir.rb'
+
+require 'sling/test'
+require 'sling/file'
+include SlingUsers
+include SlingFile
+
+class TC_Kern1376Test < Test::Unit::TestCase
+  include SlingTest
+
+  def add_activity(url, appid, templateid, messagetext)
+    res = @s.execute_post("#{url}.activity.json", {
+      "sakai:activity-appid" => appid,
+      "sakai:activity-templateid" => templateid,
+      "sakai:activityMessage" => messagetext
+    })
+    assert_equal("200", res.code, "Should have added activity")
+  end
+
+  def test_get_pooled_content_activities
+    @fm = FileManager.new(@s)
+    m = Time.now.to_f.to_s.gsub('.', '')
+    manager = create_user("user-manager-#{m}")
+    @s.switch_user(manager)
+    res = @fm.upload_pooled_file("random-#{m}.txt", "Plain content", "text/plain")
+    assert_equal("201", res.code, "Expected to be able to create pooled content")
+	uploadresult = JSON.parse(res.body)
+	contentid = uploadresult["random-#{m}.txt"]
+	assert_not_nil(contentid, "Should have uploaded ID")
+	contentpath = @s.url_for("/p/#{contentid}")
+
+	# Add three activity notes.
+	add_activity(contentpath, "status", "default", "First activity")
+	add_activity(contentpath, "status", "default", "Second activity")
+	add_activity(contentpath, "status", "default", "Third activity")
+
+    res = @s.execute_get(@s.url_for("/var/search/pool/activityfeed.json"), {
+      "p" => "/p/#{contentid}"
+    })
+    assert_equal("200", res.code, "Should have found activity feed")
+    puts res.body
+    activityfeed = JSON.parse(res.body)
+    assert_equal(3, activityfeed["total"])
+    assert_equal("Third activity", activityfeed["results"][0]["sakai:activityMessage"])
+    assert_equal("Second activity", activityfeed["results"][1]["sakai:activityMessage"])
+    assert_equal("First activity", activityfeed["results"][2]["sakai:activityMessage"])
+  end
+
+end
