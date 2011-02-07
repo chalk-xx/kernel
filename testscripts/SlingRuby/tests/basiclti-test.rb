a63450dce51aae2dc1368a5e8ce783ebeafc48fc
Lance Speelmon <lance@speelmon.com>
2011-02-07 11:36:16 -0500

    Fixed an issue where the Test setup method is not bring run when in a suite (i.e. tools/runalltests.rb) KERN-1499 basiclti-test.rb failing after sparsecotent migration

--- testscripts/SlingRuby/tests/basiclti-test.rb
+++ testscripts/SlingRuby/tests/basiclti-test.rb
@@ -16,23 +16,33 @@ class TC_BasicLTI < Test::Unit::TestCase
 
   def setup
     super;
+    hackzzz();
+  end
+  
+  def hackzzz
     @now = Time.now.to_f.to_s.gsub('.', '');
     @creator = create_user("creator-test#{@now}");
+    assert_not_nil(@creator)
     @user = create_user("user-test#{@now}");
+    assert_not_nil(@user)
     @admin = SlingUsers::User.admin_user();
+    assert_not_nil(@admin)
     @anonymous = SlingUsers::User.anonymous();
+    assert_not_nil(@anonymous)
   end
   
   def teardown
     super;
-#    @s.switch_user(@admin);
-#    @created_users.each do |user|
-#      @log.info(user);
-#      puts @um.delete_user("#{user}");
-#    end
   end
   
   def test_basiclti
+    # TODO this hack works around my setup method not being called
+    # when run in a test suite (i.e. tools/runalltests.rb)
+    if @creator == nil then
+      hackzzz();
+    end
+    # hackzzz();
+
     prepare_group()
     @ltiurl = "http://dr-chuck.com/ims/php-simple/tool.php";
     @ltikey = "12345";
@@ -137,6 +147,12 @@ class TC_BasicLTI < Test::Unit::TestCase
   end
 
   def test_basiclti_virtualTool
+    # TODO this hack works around my setup method not being called
+    # when run in a test suite (i.e. tools/runalltests.rb)
+    if @creator == nil then
+      hackzzz();
+    end
+
     # verify anonymous user cannot read /var/basiclti
     @s.switch_user(@anonymous);
     resp = @s.execute_get(@s.url_for("/var.json"));
@@ -150,6 +166,7 @@ class TC_BasicLTI < Test::Unit::TestCase
     
     # verify normal user cannot read /var/basiclti
     @s.switch_user(@creator);
+    assert_not_nil(@creator, "FIXME TODO Why is @creator nil *only* when run in the test suite?");
     resp = @s.execute_get(@s.url_for("/var.json"));
     assert_equal(200, resp.code.to_i, "Should be able to read /var.");
     resp = @s.execute_get(@s.url_for("/var/basiclti.json"));
@@ -164,7 +181,18 @@ class TC_BasicLTI < Test::Unit::TestCase
     resp = @s.execute_get(@s.url_for("/var.json"));
     assert_equal(200, resp.code.to_i, "Admin should be able to read /var.");
     resp = @s.execute_get(@s.url_for("/var/basiclti.json"));
+    assert_not_nil(resp);
+    assert_not_nil(resp.body);
     assert_equal(200, resp.code.to_i, "Admin should be able to read /var/basiclti.");
+    props = JSON.parse(resp.body);
+    assert_not_nil(props);
+    assert(props.length > 0);
+    assert_not_nil(props["jcr:createdBy"]);
+    assert_not_nil(props["jcr:created"]);
+    assert_not_nil(props["jcr:primaryType"]);
+    assert_not_nil(props["jcr:mixinTypes"]);
+    assert_not_nil(props["jcr:mixinTypes"][0]);
+    assert_equal("rep:AccessControllable", props["jcr:mixinTypes"][0], "Node should be accessed controlled");
     resp = @s.execute_get(@s.url_for("/var/basiclti/sakai.singleuser.json"));
     assert_equal(200, resp.code.to_i, "Admin should be able to read /var/basiclti/sakai.singleuser.");
     resp = @s.execute_get(@s.url_for("/var/basiclti/sakai.singleuser/ltiKeys.json"));
@@ -251,6 +279,12 @@ class TC_BasicLTI < Test::Unit::TestCase
   end
   
   def test_basicltiTrustedContextId
+    # TODO this hack works around my setup method not being called
+    # when run in a test suite (i.e. tools/runalltests.rb)
+    if @creator == nil then
+      hackzzz();
+    end
+
     prepare_group()
     @ltiurl = "http://dr-chuck.com/ims/php-simple/tool.php";
     @ltikey = "12345";
@@ -319,10 +353,12 @@ class TC_BasicLTI < Test::Unit::TestCase
 
   def prepare_group()
     now = Time.now.to_f.to_s.gsub('.', '')
-    @groupid = "#{now}group"
-    @groupname = "#{now} My Fun group"
+    @groupid = "basiclti-group-#{now}"
+    @groupname = "Basic LTI Test Group #{now}"
     @s.switch_user(@creator)
-    group = create_group("g-testgroup-#{now}")
+    assert_not_nil(@creator, "FIXME TODO Why is @creator nil *only* when run in the test suite?");
+    group = create_group("g-basiclti-testgroup-#{now}")
+    assert_not_nil(group);
     @s.execute_post(@s.url_for("#{group.home_path_for(@s)}/public/authprofile"), {
       "sakai:group-id" => @groupid,
       "sakai:group-title" => @groupname,
