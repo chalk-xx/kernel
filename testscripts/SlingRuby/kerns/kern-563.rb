780d5ef399ccecf4945c75a0d68ce34fb828d2d9
Lance Speelmon <lance@speelmon.com>
2011-02-07 17:04:24 -0500

    Test failed because it pointed to /userManager instaed of lite/userManager. Also fixed up assertions to be in the right order. KERN-1521 kern-563.rb is failing after sparsecontent migration

--- testscripts/SlingRuby/kerns/kern-563.rb
+++ testscripts/SlingRuby/kerns/kern-563.rb
@@ -22,7 +22,7 @@ class TC_Kern563Test < Test::Unit::TestCase
     @s.switch_user(user)
     # find the default country locale for this user
     response = @s.execute_get(@s.url_for("/system/me"))
-    assert_equal(response.code.to_i, 200)
+    assert_equal(200, response.code.to_i)
     json = JSON.parse(response.body)
     default_country_locale = json["user"]["locale"]["country"]
     
@@ -33,7 +33,7 @@ class TC_Kern563Test < Test::Unit::TestCase
     
     # confirm default country locale still exists for this user
     res = @s.execute_get(@s.url_for("/system/me"))
-    assert_equal(res.code.to_i, 200)
+    assert_equal(200, res.code.to_i)
     json = JSON.parse(res.body)
     assert_equal(json["user"]["locale"]["country"], default_country_locale)
   end
@@ -46,13 +46,13 @@ class TC_Kern563Test < Test::Unit::TestCase
     @s.switch_user(user)
     params = {"locale" => "nl_BE"}
     
-    @s.execute_post(@s.url_for("system/userManager/user/#{userid}.update.html"), params)
+    @s.execute_post(@s.url_for("system/lite/userManager/user/#{userid}.update.html"), params)
     
     resp = @s.execute_get(@s.url_for("/system/me"))
     assert_equal(resp.code.to_i, 200)
     json = JSON.parse(resp.body)
     # Default is US, should be BE
-    assert_equal(json["user"]["locale"]["country"], "BE")
+    assert_equal("BE", json["user"]["locale"]["country"])
   end
   
 end
