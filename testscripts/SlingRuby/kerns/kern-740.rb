3f95e2c811cd86c0668b0e146c984719b3c2bb38
Ray Davis <ray@media.berkeley.edu>
2011-02-10 16:07:53 -0800

    KERN-1522 Eliminate evil usage of Time.now.to_i.to_s

--- testscripts/SlingRuby/kerns/kern-740.rb
+++ testscripts/SlingRuby/kerns/kern-740.rb
@@ -10,12 +10,12 @@ include SlingUsers
 
 class TC_Kern740Test < Test::Unit::TestCase
   include SlingTest
-  
+
   #
   # Test changing the admin password using the form auth mechanism.
   #
-  
-  def teardown 
+
+  def teardown
     # Reset the password
     @s.trustedauth = false
     admin2 = User.new("admin","2admin2")
@@ -26,9 +26,9 @@ class TC_Kern740Test < Test::Unit::TestCase
 	admin2.change_password(@s,"admin")
 	super
   end
-  
+
   def test_change_password_basicAuth
-    m = Time.now.to_i.to_s
+    m = Time.now.to_f.to_s.gsub('.', '')
     @s.trustedauth = false
 	@s.execute_get(@s.url_for("/var/cluster/user.json?Starting_Basic_AuthTest"))
 	@log.info("Changing Admin Password with Basic Auth")
@@ -38,14 +38,14 @@ class TC_Kern740Test < Test::Unit::TestCase
   end
 
   def test_change_password_basicAuthNewUser
-    m = Time.now.to_i.to_s
+    m = Time.now.to_f.to_s.gsub('.', '')
     @s.trustedauth = false
-	
+
 	u = "user"+m
 	testUser = create_user(u)
-	
+
 	@s.switch_user(testUser)
-	
+
 	# Can this new user update content in their own space.
 	res = @s.execute_get(@s.url_for("/system/me"))
 	assert_equal("200",res.code)
@@ -54,7 +54,7 @@ class TC_Kern740Test < Test::Unit::TestCase
 	assert_not_nil(props["user"],"system me request failed, expected to find a user object")
 	assert_equal(testUser.name, props["user"]["userid"],"Authentication failed, didnt get expected user")
 	homeFolderTestFile = "/~#{testUser.name}/testarea"+m
-	
+
 	res = @s.execute_post(@s.url_for(homeFolderTestFile),"testprop" => "testvalue",  "jcr:mixinTypes" => "mix:lastModified" )
 	assert_equal("201",res.code, res.body)
 	res = @s.execute_get(@s.url_for(homeFolderTestFile+".json"))
@@ -63,46 +63,46 @@ class TC_Kern740Test < Test::Unit::TestCase
 	# check the node really was last modified by the correct user.
 	assert_equal(testUser.name, props["jcr:lastModifiedBy"])
 
-	
-	
+
+
 	res = testUser.change_password(@s,"testpass2")
 	assert_equal("200",res.code,res.body)
 
 	testUser2 = User.new(testUser.name,"testpass2")
-	
+
 	@s.switch_user(testUser2)
 	res = testUser2.change_password(@s,"testpass")
 	assert_equal("200",res.code,res.body)
-	
+
 	@s.execute_get(@s.url_for("/var/cluster/user.json?Done_Trusted_AuthTest"))
   end
 
 
   def test_change_password_basicAuthNewUser
-    m = Time.now.to_i.to_s
+    m = Time.now.to_f.to_s.gsub('.', '')
     @s.trustedauth = false
-	
+
 	u = "userbasic"+m
-	
+
 	runChangePasswordForUser(u,"a")
   end
 
   def test_change_password_trustedAuthNewUser
-    m = Time.now.to_i.to_s
+    m = Time.now.to_f.to_s.gsub('.', '')
     @s.trustedauth = true
-	
+
 	u = "usertrusted"+m
-	
+
 	runChangePasswordForUser(u,"b")
   end
 
   def test_change_password_trustedAuth
-    m = Time.now.to_i.to_s
+    m = Time.now.to_f.to_s.gsub('.', '')
     @s.trustedauth = true
 	@s.execute_get(@s.url_for("/var/cluster/user.json?Starting_Trusted_AuthTest"))
-	
 
-	
+
+
 	@log.info("Changing Admin Password with Trusted Auth")
 	runChangePassword("d")
 	@log.info("Done Changing Admin Password with Trusted Auth")
@@ -111,25 +111,25 @@ class TC_Kern740Test < Test::Unit::TestCase
 
 
   def runChangePasswordForUser(u,v)
-  
+
 	testUser = create_user(u)
-	
+
 	@s.switch_user(testUser)
-	
-	
+
+
 	checkPersonalSpace(u,v)
 
-	
+
 	## This failes here for a trusted user other than admin
 	res = testUser.change_password(@s,"testpass2")
 	assert_equal("200",res.code,res.body)
 
 	testUser2 = User.new(testUser.name,"testpass2")
-	
+
 	@s.switch_user(testUser2)
 	res = testUser2.change_password(@s,"testpass")
 	assert_equal("200",res.code,res.body)
-	
+
 	@s.execute_get(@s.url_for("/var/cluster/user.json?Done_Trusted_AuthTest"))
   end
 
@@ -137,14 +137,14 @@ class TC_Kern740Test < Test::Unit::TestCase
   def runChangePassword(v)
     admin = User.new("admin","admin")
 	@s.switch_user(admin)
-	
-	
-	
-	
+
+
+
+
 	# check basic node update functionallity and make certain that the user we think is performing the operation.
-	# this should check that the session is correctly authenticated inside JCR since if not the lastModifiedBy wont be 
+	# this should check that the session is correctly authenticated inside JCR since if not the lastModifiedBy wont be
 	# correct.
-        m = Time.now.to_i.to_s
+        m = Time.now.to_f.to_s.gsub('.', '')
 	homeFolderTestFile = "/testarea/"+m+v
 	res = @s.execute_post(@s.url_for(homeFolderTestFile),"testprop" => "testvalue",  "jcr:mixinTypes" => "mix:lastModified" )
 	assert_equal("201",res.code, res.body+"\n Failed to create a node logged in as admin, must be a problem with the login, ie not really admin ?")
@@ -160,7 +160,7 @@ class TC_Kern740Test < Test::Unit::TestCase
 	assert_equal("200",res.code,res.body)
 
 	res = admin.change_password(@s,"2admin2")
-	
+
 	assert_equal("200",res.code,res.body)
 
     admin2 = User.new("admin","2admin2")
@@ -174,7 +174,7 @@ class TC_Kern740Test < Test::Unit::TestCase
     res = admin.update_properties(@s,"testproperty" => "newvalue2")
 	assert_equal("200",res.code,res.body)
   end
-  
+
   def checkPersonalSpace(u,v)
   	# Can this new user update content in their own space.
 	res = @s.execute_get(@s.url_for("/system/me"))
@@ -184,7 +184,7 @@ class TC_Kern740Test < Test::Unit::TestCase
 	assert_not_nil(props["user"],"system me request failed, expected to find a user object")
 	assert_equal(u, props["user"]["userid"],"Authentication failed, didnt get expected user")
 	homeFolderTestFile = "/~#{u}/testarea"+u+v
-	
+
 	res = @s.execute_post(@s.url_for(homeFolderTestFile),"testprop" => "testvalue",  "jcr:mixinTypes" => "mix:lastModified" )
 	assert_equal("201",res.code, res.body)
 	res = @s.execute_get(@s.url_for(homeFolderTestFile+".json"))
@@ -195,8 +195,8 @@ class TC_Kern740Test < Test::Unit::TestCase
 	assert_equal(u, props["createdBy"])
 
   end
-  
 
-  
+
+
 end
 
