2dae0a5a645c4d92a40cbb20312da18f68d553e2
Ian Boston <ieb@tfd.co.uk>
2011-03-22 14:43:43 +0000

    KERN-1684 Added integration test for file bodies attached to child resources of pooled content items

--- testscripts/SlingRuby/tests/page-create-test.rb
+++ testscripts/SlingRuby/tests/page-create-test.rb
@@ -67,7 +67,7 @@ class TC_NodeCreateTest < Test::Unit::TestCase
   end
 
 
-  def test_updatechild_pool_item
+  def test_addchild_pool_item
     testpath = "/system/pool/createfile"
     res = @s.execute_post(@s.url_for(testpath), "a" => "foo", "b" => "bar")
 	assert_equal("201",res.code)
@@ -89,16 +89,31 @@ class TC_NodeCreateTest < Test::Unit::TestCase
     assert_equal("foobar", props["aa"], "Expected property to be set")
     assert_equal("barfoo", props["bb"], "Expected property to be set")
 
-    res = @s.execute_post(@s.url_for("/p/#{poolId}/#{resourceId}"), "aaa" => "XfoobarX", "bbb" => "XbarfooX")
-	assert_equal("200",res.code)
+  end
+
+
+  def test_updatechild_pool_item_file
+    testpath = "/system/pool/createfile"
+    res = @s.execute_post(@s.url_for(testpath), "a" => "foo", "b" => "bar")
+	assert_equal("201",res.code)
+	jsonResponse = JSON.parse(res.body)
+	poolId = jsonResponse['_contentItem']
 	
-    res = @s.execute_get(@s.url_for("/p/#{poolId}/#{resourceId}.json"))
+    props = @s.get_node_props("/p/#{poolId}.json")
+    assert_equal("foo", props["a"], "Expected property to be set")
+    assert_equal("bar", props["b"], "Expected property to be set")
+
+	fBody = "File Number 1 Content"
+    res = @s.execute_file_post(@s.url_for("/p/#{poolId}.resource.html"), "file", "fileNo1", fBody, "text/plain")
+	assert_equal("201",res.code)
+	jsonResponse = JSON.parse(res.body)
+	resourceId = jsonResponse['fileNo1']
+	assert_not_nil(resourceId)
+	
+	
+    res = @s.execute_get(@s.url_for("/p/#{poolId}/#{resourceId}"))
 	assert_equal("200",res.code)
-	props = JSON.parse(res.body)
-    assert_equal("foobar", props["aa"], "Expected property to be set")
-    assert_equal("barfoo", props["bb"], "Expected property to be set")
-    assert_equal("XfoobarX", props["aaa"], "Expected property to be set")
-    assert_equal("XbarfooX", props["bbb"], "Expected property to be set")
+	assert_equal(fBody,res.body)
 
   end
 
