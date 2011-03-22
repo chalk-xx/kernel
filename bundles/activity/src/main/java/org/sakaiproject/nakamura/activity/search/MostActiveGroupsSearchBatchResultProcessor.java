cb68feed8ae57fc3c8f7b5d0617a477699eb5d61
Lance Speelmon <lance@speelmon.com>
2011-03-22 10:07:35 -0400

    KERN-1660 Added timestamp range constraint and proper sorting of results to match KERN-1636. Most active public groups feed doesn't return any results

--- bundles/activity/src/main/java/org/sakaiproject/nakamura/activity/search/MostActiveGroupsSearchBatchResultProcessor.java
+++ bundles/activity/src/main/java/org/sakaiproject/nakamura/activity/search/MostActiveGroupsSearchBatchResultProcessor.java
@@ -27,6 +27,7 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.io.JSONWriter;
+import org.sakaiproject.nakamura.api.files.FilesConstants;
 import org.sakaiproject.nakamura.api.lite.Session;
 import org.sakaiproject.nakamura.api.lite.StorageClientUtils;
 import org.sakaiproject.nakamura.api.lite.content.Content;
@@ -39,11 +40,11 @@ import org.sakaiproject.nakamura.api.search.solr.SolrSearchServiceFactory;
 import org.sakaiproject.nakamura.util.LitePersonalUtils;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
-import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @Component(immediate = true, label = "MostActiveGroupSearchBatchResultProcessor", description = "Formatter for most active groups")
 @Service
@@ -56,8 +57,8 @@ public class MostActiveGroupsSearchBatchResultProcessor implements
   private SolrSearchServiceFactory searchServiceFactory;
 
 
-  private final int DEFAULT_DAYS = 30;
-  private final int MAXIMUM_DAYS = 90;
+  // private final int DEFAULT_DAYS = 30;
+  // private final int MAXIMUM_DAYS = 90;
 
   /**
    * {@inheritDoc}
@@ -65,7 +66,7 @@ public class MostActiveGroupsSearchBatchResultProcessor implements
    */
   public void writeResults(SlingHttpServletRequest request, JSONWriter write,
       Iterator<Result> results) throws JSONException {
-      List<ResourceActivity> resources = new ArrayList<ResourceActivity>();
+      final Map<String, ResourceActivity> resources = new HashMap<String, ResourceActivity>();
       ResourceResolver resolver = request.getResourceResolver();
       final Session session = StorageClientUtils.adaptToSession(request
           .getResourceResolver().adaptTo(javax.jcr.Session.class));
@@ -75,42 +76,45 @@ public class MostActiveGroupsSearchBatchResultProcessor implements
         Resource resource = resolver.getResource(path);
         Content content = resource.adaptTo(Content.class);
 
-        int daysAgo = deriveDateWindow(request);
+      // int daysAgo = deriveDateWindow(request);
 
-        if (content.hasProperty("timestamp")) {
-          Calendar timestamp = (Calendar) content.getProperty("timestamp");
-          Calendar specifiedDaysAgo = new GregorianCalendar();
-          specifiedDaysAgo.add(Calendar.DAY_OF_MONTH, -daysAgo);
-          if (timestamp.before(specifiedDaysAgo)) {
-            // we stop counting once we get to the old stuff
-            break;
-          } else {
-            String resourceId = (String) content.getProperty("resourceId");
-            if (!resources.contains(new ResourceActivity(resourceId))) {
-              String resourcePath = LitePersonalUtils.getProfilePath(resourceId);
-              Content resourceContent = null;
-              try {
-                resourceContent = session.getContentManager().get(resourcePath);
-              } catch (Exception e) {
-                // this happens if the group is not public
-                // or if the group path simply doesn't exist
-                continue;
-              }
-              String resourceName = (String) resourceContent.getProperty("sakai:group-title");
-              resources.add(new ResourceActivity(resourceId, 0, resourceName));
-            }
-            // increment the count for this particular resource.
-            resources.get(resources.indexOf(new ResourceActivity(resourceId))).activityScore++;
-          }
+      // if (content.hasProperty("timestamp")) {
+      // Calendar timestamp = (Calendar) content.getProperty("timestamp");
+      // Calendar specifiedDaysAgo = new GregorianCalendar();
+      // specifiedDaysAgo.add(Calendar.DAY_OF_MONTH, -daysAgo);
+      // if (timestamp.before(specifiedDaysAgo)) {
+      // // we stop counting once we get to the old stuff
+      // break;
+      // } else {
+      String resourceId = (String) content.getProperty("resourceId");
+      if (!resources.containsKey(resourceId)) {
+        String resourcePath = LitePersonalUtils.getProfilePath(resourceId);
+        Content resourceContent = null;
+        try {
+          resourceContent = session.getContentManager().get(resourcePath);
+        } catch (Exception e) {
+          // this happens if the group is not public
+          // or if the group path simply doesn't exist
+          continue;
         }
+        String resourceName = (String) resourceContent.getProperty("sakai:group-title");
+        resources.put(resourceId, new ResourceActivity(resourceId, 0, resourceName,
+            (Long) resourceContent.getProperty(FilesConstants.LAST_MODIFIED)));
       }
+      // increment the count for this particular resource.
+      resources.get(resourceId).activityScore++;
+    }
+    // }
+    // }
 
       // write the most-used content to the JSONWriter
-      Collections.sort(resources, Collections.reverseOrder());
+      List<ResourceActivity> resourceActivities = new ArrayList<ResourceActivity>(
+          resources.values());
+      Collections.sort(resourceActivities, Collections.reverseOrder());
       write.object();
       write.key("groups");
       write.array();
-      for (ResourceActivity resourceActivity : resources) {
+      for (ResourceActivity resourceActivity : resourceActivities) {
           write.object();
           write.key("id");
           write.value(resourceActivity.id);
@@ -124,21 +128,21 @@ public class MostActiveGroupsSearchBatchResultProcessor implements
       write.endObject();
   }
 
-  private int deriveDateWindow(SlingHttpServletRequest request) {
-    int daysAgo = DEFAULT_DAYS;
-    String requestedDaysParam = request.getParameter("days");
-    if (requestedDaysParam != null) {
-        try {
-          int requestedDays = Integer.parseInt(requestedDaysParam);
-          if ((requestedDays > 0) && (requestedDays <= MAXIMUM_DAYS)) {
-            daysAgo = requestedDays;
-          }
-        } catch (NumberFormatException e) {
-          // malformed parameter, so we'll just stick with the default number of days
-        }
-    }
-    return daysAgo;
-  }
+  // private int deriveDateWindow(SlingHttpServletRequest request) {
+  // int daysAgo = DEFAULT_DAYS;
+  // String requestedDaysParam = request.getParameter("days");
+  // if (requestedDaysParam != null) {
+  // try {
+  // int requestedDays = Integer.parseInt(requestedDaysParam);
+  // if ((requestedDays > 0) && (requestedDays <= MAXIMUM_DAYS)) {
+  // daysAgo = requestedDays;
+  // }
+  // } catch (NumberFormatException e) {
+  // // malformed parameter, so we'll just stick with the default number of days
+  // }
+  // }
+  // return daysAgo;
+  // }
 
   /**
    * {@inheritDoc}
@@ -152,16 +156,25 @@ public class MostActiveGroupsSearchBatchResultProcessor implements
     return searchServiceFactory.getSearchResultSet(request, query);
   }
 
-  public class ResourceActivity implements Comparable<ResourceActivity>{
-    public String id;
-    public ResourceActivity(String id) {
-      this.id = id;
-    }
-    public ResourceActivity(String id, int activityScore, String name) {
+  public class ResourceActivity implements Comparable<ResourceActivity> {
+    public final String id;
+    public final String name;
+    public final Long lastModified;
+    public Integer activityScore;
+
+    public ResourceActivity(String id, int activityScore, String name, long lastModified) {
       this.id = id;
       this.activityScore = activityScore;
       this.name = name;
+      this.lastModified = lastModified;
     }
+    
+    @Override
+    public String toString() {
+      return "ResourceActivity(" + id + ", " + activityScore + ", " + name + ", "
+          + lastModified + ")";
+    }
+
     @Override
     public int hashCode() {
       final int prime = 31;
@@ -170,6 +183,7 @@ public class MostActiveGroupsSearchBatchResultProcessor implements
       result = prime * result + ((id == null) ? 0 : id.hashCode());
       return result;
     }
+
     @Override
     public boolean equals(Object obj) {
       if (this == obj)
@@ -188,11 +202,15 @@ public class MostActiveGroupsSearchBatchResultProcessor implements
         return false;
       return true;
     }
-    public String name;
-    public int activityScore;
+
     public int compareTo(ResourceActivity other) {
-      return Integer.valueOf(this.activityScore).compareTo(Integer.valueOf(other.activityScore));
+      if (this.activityScore.equals(other.activityScore)) {
+        return this.lastModified.compareTo(other.lastModified);
+      } else {
+        return this.activityScore.compareTo(other.activityScore);
+      }
     }
+
     private MostActiveGroupsSearchBatchResultProcessor getOuterType() {
       return MostActiveGroupsSearchBatchResultProcessor.this;
     }
