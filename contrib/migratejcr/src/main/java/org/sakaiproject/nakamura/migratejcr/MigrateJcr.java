03e36da785cec7a382503efc21e54429a2cc4344
Zach A. Thomas <zach@aeroplanesoftware.com>
2011-03-23 22:06:36 -0500

    KERN-1546 creating some bare bones (slingRepostitory, sparseRepository) for the migration to use.

--- contrib/migratejcr/src/main/java/org/sakaiproject/nakamura/migratejcr/MigrateJcr.java
+++ contrib/migratejcr/src/main/java/org/sakaiproject/nakamura/migratejcr/MigrateJcr.java
@@ -17,11 +17,124 @@
  */
 package org.sakaiproject.nakamura.migratejcr;
 
+import com.google.common.collect.ImmutableMap;
+
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.jcr.api.SlingRepository;
+import org.apache.sling.jcr.base.util.AccessControlUtil;
+import org.sakaiproject.nakamura.api.lite.ClientPoolException;
+import org.sakaiproject.nakamura.api.lite.Repository;
+import org.sakaiproject.nakamura.api.lite.Session;
+import org.sakaiproject.nakamura.api.lite.StorageClientException;
+import org.sakaiproject.nakamura.api.lite.accesscontrol.AccessDeniedException;
+import org.sakaiproject.nakamura.api.lite.authorizable.AuthorizableManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.osgi.service.component.ComponentContext;
+
+import java.util.Dictionary;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
 /**
  *
  */
 @Component
 public class MigrateJcr {
+  private Logger LOGGER = LoggerFactory.getLogger(MigrateJcr.class);
+  
+  @Reference
+  private SlingRepository slingRepository;
+  
+  @Reference
+  private Repository sparseRepository;
+  
+  @SuppressWarnings("unchecked")
+  @Activate
+  protected void activate(ComponentContext componentContext) {
+    Dictionary componentProps = componentContext.getProperties();
+    if (shouldMigrate(componentProps)) {
+      try {
+        migrateAuthorizables();
+        migrateContentPool();
+        cleanup();
+      } catch (Exception e) {
+        LOGGER.error("Failed data migration from JCR to Sparse.", e);
+      }
+    }
+  }
+
+  private void cleanup() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  private void migrateContentPool() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  private void migrateAuthorizables() throws RepositoryException, ClientPoolException, StorageClientException, AccessDeniedException {
+    LOGGER.info("beginning users and groups migration.");
+    javax.jcr.Session jcrSession = slingRepository.loginAdministrative(null);
+    String usersQuery = "//*[@sling:resourceType='sakai/user-home'] order by @jcr:score descending";
+    QueryManager qm = jcrSession.getWorkspace().getQueryManager();
+    Query q = qm.createQuery(usersQuery, Query.XPATH);
+    QueryResult result = q.execute();
+    NodeIterator resultNodes = result.getNodes();
+    LOGGER.info("found {} user home folders in Jackrabbit.", resultNodes.getSize());
+    while(resultNodes.hasNext()) {
+      Node authHomeNode = resultNodes.nextNode();
+      LOGGER.info(authHomeNode.getPath());
+      moveAuthorizableToSparse(authHomeNode);
+    }
+    
+    String groupsQuery = "//*[@sling:resourceType='sakai/group-home'] order by @jcr:score descending";
+    q = qm.createQuery(groupsQuery, Query.XPATH);
+    result = q.execute();
+    resultNodes = result.getNodes();
+    LOGGER.info("found {} group home folders in Jackrabbit.", resultNodes.getSize());
+    while(resultNodes.hasNext()) {
+      Node groupHomeNode = resultNodes.nextNode();
+      LOGGER.info(groupHomeNode.getPath());
+      moveAuthorizableToSparse(groupHomeNode);
+    }
+    UserManager userManager = AccessControlUtil.getUserManager(jcrSession);
+    
+  }
+
+  private void moveAuthorizableToSparse(Node authHomeNode) throws ClientPoolException, StorageClientException, AccessDeniedException, PathNotFoundException, RepositoryException {
+    Session sparseSession = sparseRepository.loginAdministrative();
+    AuthorizableManager authManager = sparseSession.getAuthorizableManager();
+    boolean isUser = "sakai/user-home".equals(authHomeNode.getProperty("sling:resourceType").getString());
+    Node profileNode = authHomeNode.getNode("public/authprofile");
+    if (isUser) {
+      String id = profileNode.getProperty("rep:userId").getString();
+      String firstName = profileNode.getProperty("firstName").getString();
+      String lastName = profileNode.getProperty("lastName").getString();
+      String email = profileNode.getProperty("email").getString();
+      authManager.createUser(id, id, null, ImmutableMap.of(
+          "firstName", (Object)firstName,
+          "lastName", (Object)lastName,
+          "email", (Object)email));
+    }
+    
+    
+  }
+
+  @SuppressWarnings("unchecked")
+  private boolean shouldMigrate(Dictionary componentProps) {
+    // TODO determine whether there is any migrating to do
+    return true;
+  }
 
 }
