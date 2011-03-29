99916441d09b059d719f7c083c0682f603cd0d0b
Ayrton De Craene <me@ayrton.be>
2011-03-29 18:20:46 +0100

    KERN-1646 now compatible with all common documents and the document-viewer

--- scripts/preview_processor.rb
+++ scripts/preview_processor.rb
@@ -2,6 +2,7 @@
 require 'fileutils'
 require 'rubygems'
 require 'docsplit'
+require 'RMagick'
 require 'ruby-lib-dir.rb'
 require 'sling/sling'
 include SlingInterface
@@ -23,46 +24,82 @@ process = JSON.parse(res.body)
 Dir.chdir DOCS_DIR
 process['results'].each do |f|
   # only copy pdf's and odt files
-  FileUtils.touch f['jcr:name'] if ['.pdf','.odt'].include? f['sakai:fileextension']
+  FileUtils.touch f['jcr:name']
 end
 
 # for all items in pending folder
 puts "pending files: #{Dir["*"].to_s}"
 
 Dir["*"].each do |id|
+  begin  
   puts "processing #{id}"
   FileUtils.rm id # clean up crew
-      
-  # making a local copy of this file.
+  
+  # some meta checking.    
   meta_file = @s.execute_get(@s.url_for("p/#{id}.json"))
-  next unless (meta_file.code == '200') # skip it
+  next unless meta_file.code == '200' # skip it
   meta = JSON.parse(meta_file.body)     
-  extension = meta['sakai:fileextension']
+  extension = meta['sakai:fileextension']  
+  next if ['.png', '.jpg', '.gif'].include? extension # skip images  
+
+  # making a local copy of this file.
   filename = id + extension  
   content_file = @s.execute_get(@s.url_for("p/#{id}"))
   File.open(filename, 'wb') { |f| f.write(content_file.body) }
 
   # generating image previews of te document
-  Docsplit.extract_images filename, :size => '920x', :format => :jpg
+  Docsplit.extract_images filename, :size => '700x', :format => :jpg
+  
+  next if Dir[id + '_*'].size == 0 # skip it to be sure
   
   Dir.mkdir PREVS_DIR + "/#{id}" unless File.directory? PREVS_DIR + "/#{id}"
   # moving these files to another directory: "thumbnails/filename/index.ext"
   Dir[id + '_*'].each_with_index do |preview, index|
-    FileUtils.mv "#{preview}", "#{PREVS_DIR}/#{id}/" + index.to_s + '.jpg'
+    FileUtils.mv "#{preview}", "#{PREVS_DIR}/#{id}/#{index}.jpg"
   end
   
   Dir.chdir PREVS_DIR + "/#{id}"  
+    
+  pagecount = Dir["*"].size
   # uploading each preview
   Dir["*"].each_with_index do |screenname,index|
      nbytes = File.size screenname 
      content = nil
      File.open(screenname, "rb" ) { |f| content = f.read(nbytes) }
-     @s.execute_file_post @s.url_for("system/pool/createfile.#{id}.page#{index}"), "thumbnail", "thumbnail", content, "image/jpeg"
-     puts "Uploaded Preview image to curl #{@s.url_for("p/#{id}.page#{index}.jpg")}"
-  end 
+     
+     # 1 based index! (necessity for the docpreviewer 3akai-ux widget)
+     # id.pagex-normal.jpg     
+     @s.execute_file_post @s.url_for("system/pool/createfile.#{id}.page#{index+1}-normal"), "thumbnail", "thumbnail", content, "image/jpeg"
+     puts "Uploaded image to curl #{@s.url_for("p/#{id}.page#{index+1}-normal.jpg")}"
+     
+     # creating a thumbnail of the preview
+     # id.pagex-small.jpg 
+     maxwidth = 180
+     maxheight = 225
+     aspectratio = maxwidth.to_f / maxheight.to_f
+
+     pic = Magick::Image.read(screenname).first
+     imgwidth = pic.columns
+     imgheight = pic.rows
+     imgratio = imgwidth.to_f / imgheight.to_f
+     imgratio > aspectratio ? scaleratio = maxwidth.to_f / imgwidth : scaleratio = maxheight.to_f / imgheight
+     thumb = pic.resize scaleratio
+
+     white_bg = Magick::Image.new maxwidth, maxheight
+     pic = white_bg.composite thumb, Magick::CenterGravity, Magick::OverCompositeOp
+     
+     imgfile = File.basename screenname, '.*' # filename without extension
+     small_screenname = imgfile + '.small.jpg'
+     pic.write small_screenname
+     
+     nbytes = File.size small_screenname 
+     File.open(small_screenname, "rb" ) { |f| content = f.read(nbytes) }
+     @s.execute_file_post @s.url_for("system/pool/createfile.#{id}.page#{index+1}-small"), "thumbnail", "thumbnail", content, "image/jpeg"
+     puts "Uploaded image to curl #{@s.url_for("p/#{id}.page#{index+1}-small.jpg")}"    
+  end  
   
-  # flagging the file as processed
-  @s.execute_post @s.url_for("p/#{id}"), { "sakai:needsprocessing" => "false" }
+  # passing on the pagecount
+  @s.execute_post @s.url_for("p/#{id}"), { "sakai:pagecount" => pagecount }
   
   # cleaning crew
   Dir.chdir MAIN_DIR
@@ -71,6 +108,12 @@ Dir["*"].each do |id|
   FileUtils.rm DOCS_DIR + "/#{filename}"
   
   Dir.chdir DOCS_DIR # otherwise it won't find the next file
+rescue
+  puts "error generating preview (ID: #{id})"
+ensure
+  # flagging the file as processed (both succeeded and failed processes)
+  @s.execute_post @s.url_for("p/#{id}"), { "sakai:needsprocessing" => "false" }
+end
 end
 
 FileUtils.remove_dir PREVS_DIR
