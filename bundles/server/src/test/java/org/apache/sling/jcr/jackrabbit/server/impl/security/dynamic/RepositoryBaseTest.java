5fbf287b9221d120d7e861881c48124ca4f6f894
Ian Boston <ieb@tfd.co.uk>
2011-03-30 14:04:25 +0100

    Fixed borken build, sorry.

--- bundles/server/src/test/java/org/apache/sling/jcr/jackrabbit/server/impl/security/dynamic/RepositoryBaseTest.java
+++ bundles/server/src/test/java/org/apache/sling/jcr/jackrabbit/server/impl/security/dynamic/RepositoryBaseTest.java
@@ -36,6 +36,7 @@ import org.mockito.MockitoAnnotations;
 import org.osgi.framework.BundleContext;
 import org.sakaiproject.nakamura.api.lite.ClientPoolException;
 import org.sakaiproject.nakamura.api.lite.StorageClientException;
+import org.sakaiproject.nakamura.util.ISO8601Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
