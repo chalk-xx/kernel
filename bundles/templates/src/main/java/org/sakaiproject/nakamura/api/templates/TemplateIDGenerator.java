73ac07ccbb52a87f6f304611a0deb43c69489a94
Ian Boston <ieb@tfd.co.uk>
2011-03-31 12:51:15 +0100

    KERN-1653, missed some late changes in the last commit

--- bundles/templates/src/main/java/org/sakaiproject/nakamura/api/templates/TemplateIDGenerator.java
+++ bundles/templates/src/main/java/org/sakaiproject/nakamura/api/templates/TemplateIDGenerator.java
@@ -2,27 +2,27 @@ package org.sakaiproject.nakamura.api.templates;
 
 import java.util.HashMap;
 
-public class TemplateIDGenerator extends HashMap<String, String>{
+public class TemplateIDGenerator extends HashMap<String, String> {
 
   private IDGenerator idGenerator;
+
   public TemplateIDGenerator(IDGenerator idGenerator) {
     this.idGenerator = idGenerator;
   }
+
   /**
-   * 
+   *
    */
   private static final long serialVersionUID = 8434260923805908997L;
-  
+
   @Override
   public String get(Object key) {
     String id = super.get(key);
-    if ( id == null) {
+    if (id == null) {
       id = idGenerator.nextId();
       super.put((String) key, id);
     }
     return id;
   }
-  
-  
 
 }
