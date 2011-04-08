1bea96d6249a810a10a062bfc2b278e1177bc343
Carl Hall <carl@hallwaytech.com>
2011-04-07 22:32:20 -0400

    KERN-1732 Corrected widget data indexer after testing more thoroughly.

--- bundles/pages/src/main/java/org/sakaiproject/nakamura/pages/search/WidgetDataIndexingHandler.java
+++ bundles/pages/src/main/java/org/sakaiproject/nakamura/pages/search/WidgetDataIndexingHandler.java
@@ -22,8 +22,10 @@ import com.google.common.collect.Lists;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.solr.client.solrj.util.ClientUtils;
 import org.apache.solr.common.SolrInputDocument;
 import org.osgi.service.event.Event;
@@ -48,6 +50,8 @@ import java.util.Collection;
  * ://confluence.sakaiproject.org/display/KERNDOC/KERN-1675+Searching+Widget+Data} for
  * more details.
  */
+@Component(immediate = true)
+@Service
 public class WidgetDataIndexingHandler implements IndexingHandler {
 
   public static final String INDEXED_FIELDS = "sakai:indexed-fields";
@@ -103,7 +107,7 @@ public class WidgetDataIndexingHandler implements IndexingHandler {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < indexedFields.length; i++) {
           Object propVal = content.getProperty(indexedFields[i]);
-          if (propVal == null) {
+          if (propVal != null) {
             sb.append(propVal).append(' ');
           }
         }
