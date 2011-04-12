441e3157e8a2c261d960e0b75fa7cd8fd85291d7
Zach A. Thomas <zach@aeroplanesoftware.com>
2011-04-12 13:44:28 -0500

    NOJIRA indexing of messages became broken when we tried to create two documents for the same content and there was a collision when both solr documents had the same id.

--- bundles/messaging/src/main/java/org/sakaiproject/nakamura/message/search/MessageIndexingHandler.java
+++ bundles/messaging/src/main/java/org/sakaiproject/nakamura/message/search/MessageIndexingHandler.java
@@ -76,6 +76,8 @@ public class MessageIndexingHandler implements IndexingHandler {
   private static final Logger logger = LoggerFactory
       .getLogger(MessageIndexingHandler.class);
 
+  private static final String AUTH_SUFFIX = "-auth";
+
   @Reference(target = "(type=sparse)")
   private ResourceIndexingService resourceIndexingService;
 
@@ -138,6 +140,7 @@ public class MessageIndexingHandler implements IndexingHandler {
             // client. the resource indexing service will add all nodes of the path and
             // we want this one to return first in the result processor.
             doc.setField(FIELD_PATH, authId);
+            doc.addField(FIELD_ID, path + AUTH_SUFFIX);
 
             // set the return to a single value field so we can group it
             doc.setField("returnpath", authId);
@@ -164,7 +167,7 @@ public class MessageIndexingHandler implements IndexingHandler {
       Event event) {
     logger.debug("GetDelete for {} ", event);
     String path = (String) event.getProperty(FIELD_PATH);
-    return ImmutableList.of("id:" + ClientUtils.escapeQueryChars(path));
+    return ImmutableList.of("id:(" + ClientUtils.escapeQueryChars(path) + " OR " + ClientUtils.escapeQueryChars(path + AUTH_SUFFIX) + ")");
   }
 
 }
