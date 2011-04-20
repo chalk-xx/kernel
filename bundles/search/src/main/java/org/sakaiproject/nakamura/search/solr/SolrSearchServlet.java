7900264588e5390b131e837b079870c667fdfaf5
Carl Hall <carl@hallwaytech.com>
2011-04-20 14:27:13 -0400

    Added check for null when retrieving service. This just showed up for the first time. Very odd.

--- bundles/search/src/main/java/org/sakaiproject/nakamura/search/solr/SolrSearchServlet.java
+++ bundles/search/src/main/java/org/sakaiproject/nakamura/search/solr/SolrSearchServlet.java
@@ -643,6 +643,11 @@ public class SolrSearchServlet extends SlingSafeMethodsServlet {
   private void addProcessor(ServiceReference serviceReference) {
     SolrSearchResultProcessor processor = (SolrSearchResultProcessor) osgiComponentContext
         .locateService(SEARCH_RESULT_PROCESSOR, serviceReference);
+    if (processor == null) {
+      LOGGER.warn("Retrieved null processor [{}]", serviceReference);
+      return;
+    }
+
     Long serviceId = (Long) serviceReference.getProperty(Constants.SERVICE_ID);
 
     processorsById.put(serviceId, processor);
@@ -697,6 +702,10 @@ public class SolrSearchServlet extends SlingSafeMethodsServlet {
   private void addBatchProcessor(ServiceReference serviceReference) {
     SolrSearchBatchResultProcessor processor = (SolrSearchBatchResultProcessor) osgiComponentContext
         .locateService(SEARCH_BATCH_RESULT_PROCESSOR, serviceReference);
+    if (processor == null) {
+      LOGGER.warn("Retrieved null processor [{}]", serviceReference);
+      return;
+    }
     Long serviceId = (Long) serviceReference.getProperty(Constants.SERVICE_ID);
 
     batchProcessorsById.put(serviceId, processor);
@@ -743,6 +752,10 @@ public class SolrSearchServlet extends SlingSafeMethodsServlet {
   private void addProvider(ServiceReference serviceReference) {
     SolrSearchPropertyProvider provider = (SolrSearchPropertyProvider) osgiComponentContext
         .locateService(SEARCH_PROPERTY_PROVIDER, serviceReference);
+    if (processor == null) {
+      LOGGER.warn("Retrieved null processor [{}]", serviceReference);
+      return;
+    }
     Long serviceId = (Long) serviceReference.getProperty(Constants.SERVICE_ID);
 
     propertyProviderById.put(serviceId, provider);
