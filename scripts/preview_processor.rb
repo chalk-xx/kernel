1a8527ed980c85cfdf782b7beddf2ed4cc213332
Ayrton De Craene <ayrton.decraene@gmail.com>
2011-05-03 16:46:08 +0100

    KERN-1646 does not depend on sakai:fileextension anymore

--- scripts/preview_processor.rb
+++ scripts/preview_processor.rb
@@ -8,6 +8,22 @@ require 'ruby-lib-dir.rb'
 require 'sling/sling'
 include SlingInterface
 
+# override the initialize_http_header method that sling.rb overrides
+# in order to properly set the referrer
+module Net::HTTPHeader
+  def initialize_http_header(initheader)
+    @header = {"Referer" => [ARGV[0]]}
+    return unless initheader
+    initheader.each do |key, value|
+      warn "net/http: warning: duplicated HTTP header: #{key}" if key?(key) and $VERBOSE
+      @header[key.downcase] = [value.strip]
+    end
+  end
+end
+
+server=ARGV[0]
+@s = Sling.new(server)
+
 # to run: ./preview_processor.rb http://localhost:8080/
 DEBUG = true
 
@@ -26,9 +42,6 @@ def resize_and_write_file filename, filename_output, max_width, max_height
   File.open(filename_output, "rb") { |f| content = f.read nbytes }
   content
 end
-
-server=ARGV[0]
-@s = Sling.new(server)
 
 res = @s.execute_get(@s.url_for("var/search/needsprocessing.json"))
 raise "Failed to retrieve list to process [#{res.code}]" unless res.code == '200'
@@ -58,7 +71,7 @@ Dir["*"].each do |id|
     meta_file = @s.execute_get @s.url_for("p/#{id}.json")
     next unless meta_file.code == '200' # skip.
     meta = JSON.parse meta_file.body
-    extension = meta['sakai:fileextension']
+    extension = File.extname meta['sakai:pooled-content-file-name']
     raise "File extension is nil" if extension.nil?
 
     # making a local copy of the file.
@@ -109,7 +122,7 @@ Dir["*"].each do |id|
 
         # 1 based index! (necessity for the docpreviewer 3akai-ux widget).
         # id.pagex-normal.jpg
-        puts @s.execute_file_post @s.url_for("system/pool/createfile.#{id}.page#{index+1}-normal"), "thumbnail", "thumbnail", content, "image/jpeg"
+        @s.execute_file_post @s.url_for("system/pool/createfile.#{id}.page#{index+1}-normal"), "thumbnail", "thumbnail", content, "image/jpeg"
         puts "Uploaded image to curl #{@s.url_for("p/#{id}.page#{index+1}-normal.jpg")}"
 
         # Creating a thumbnail of the preview.
@@ -134,11 +147,11 @@ Dir["*"].each do |id|
     @s.execute_post @s.url_for("p/#{id}"), {"sakai:pagecount" => page_count}
 
     Dir.chdir DOCS_DIR # otherwise we won't find the next file.
-  #rescue Exception => msg
-  #  puts "error generating preview/thumbnail (ID: #{id}): #{msg}"
+  rescue Exception => msg
+    puts "error generating preview/thumbnail (ID: #{id}): #{msg}"
   ensure
     # flagging the file as processed (for both succeeded and failed processes).
-    puts @s.execute_post @s.url_for("p/#{id}"), {"sakai:needsprocessing" => "false"} unless DEBUG
+    @s.execute_post @s.url_for("p/#{id}"), {"sakai:needsprocessing" => "false"} unless DEBUG
   end
 end
 
