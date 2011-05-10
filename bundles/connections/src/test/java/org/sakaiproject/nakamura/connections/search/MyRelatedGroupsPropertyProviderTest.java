d29443d48831c741e8688f47b4c6a68c4f509c8c
Carl Hall <carl@hallwaytech.com>
2011-05-10 17:30:30 -0400

    KERN-1799 Added search query, prop provider and unit test for searching related groups.

---
+++ bundles/connections/src/test/java/org/sakaiproject/nakamura/connections/search/MyRelatedGroupsPropertyProviderTest.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.nakamura.connections.search;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+import static org.sakaiproject.nakamura.api.user.UserConstants.GROUP_TITLE_PROPERTY;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.solr.client.solrj.util.ClientUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.sakaiproject.nakamura.api.lite.Repository;
+import org.sakaiproject.nakamura.api.lite.Session;
+import org.sakaiproject.nakamura.api.lite.authorizable.Authorizable;
+import org.sakaiproject.nakamura.api.lite.authorizable.AuthorizableManager;
+import org.sakaiproject.nakamura.api.lite.authorizable.Group;
+import org.sakaiproject.nakamura.api.search.solr.Query;
+import org.sakaiproject.nakamura.api.search.solr.Result;
+import org.sakaiproject.nakamura.api.search.solr.SolrSearchResultSet;
+import org.sakaiproject.nakamura.api.search.solr.SolrSearchServiceFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class MyRelatedGroupsPropertyProviderTest {
+  @Mock
+  private SolrSearchServiceFactory searchServiceFactory;
+  @Mock
+  private Repository repo;
+  @Mock
+  private SlingHttpServletRequest request;
+  @Mock
+  private Session session;
+  @Mock
+  private AuthorizableManager authMgr;
+  @Mock
+  private Authorizable auth1;
+  @Mock
+  private SolrSearchResultSet rs;
+
+  private ConnectionSearchPropertyProvider connPropProv;
+  private MyRelatedGroupsPropertyProvider provider;
+
+  @Before
+  public void setUp() throws Exception {
+    connPropProv = new ConnectionSearchPropertyProvider();
+    provider = new MyRelatedGroupsPropertyProvider(connPropProv, searchServiceFactory,
+        repo);
+    when(request.getRemoteUser()).thenReturn("user1");
+
+    when(repo.loginAdministrative()).thenReturn(session);
+    when(session.getAuthorizableManager()).thenReturn(authMgr);
+    when(authMgr.findAuthorizable("user1")).thenReturn(auth1);
+    Group group1 = mock(Group.class);
+    when(group1.getId()).thenReturn("group1");
+    when(group1.getProperty(GROUP_TITLE_PROPERTY)).thenReturn("Group 1 Test");
+    when(group1.getProperty("sakai:tag-uuid")).thenReturn(new String[] { "123-456" });
+//    Group group2 = mock(Group.class);
+//    when(group2.getId()).thenReturn("group2");
+//    when(group2.getProperty(GROUP_TITLE_PROPERTY)).thenReturn("Group 2 Test");
+//    when(group2.getProperty("sakai:tag-uuid")).thenReturn(new String[] { "456" });
+//    Group group3 = mock(Group.class);
+//    when(group3.getId()).thenReturn("group3");
+//    when(group3.getProperty(GROUP_TITLE_PROPERTY)).thenReturn("Group 3 Test");
+//    when(group3.getProperty("sakai:tag-uuid")).thenReturn(new String[] { "789" });
+    when(auth1.memberOf(authMgr)).thenReturn(Sets.newHashSet(group1).iterator());
+
+    when(searchServiceFactory.getSearchResultSet(eq(request), any(Query.class)))
+        .thenReturn(rs);
+  }
+
+  @Test
+  public void noGroups() {
+    reset(auth1);
+    Collection<Group> noGroups = Collections.emptyList();
+    when(auth1.memberOf(authMgr)).thenReturn(noGroups.iterator());
+
+    Result result1 = mock(Result.class);
+    Map<String, Collection<Object>> props1 = Maps.newHashMap();
+    Set<Result> results = Sets.newHashSet(result1);
+    when(result1.getProperties()).thenReturn(props1);
+    when(rs.getResultSetIterator()).thenReturn(results.iterator());
+
+    Map<String, String> propertiesMap = Maps.newHashMap();
+
+    provider.loadUserProperties(request, propertiesMap);
+
+    assertEquals("", propertiesMap.get("_groupQuery"));
+  }
+
+  @Test
+  public void oneGroup() {
+    Result result1 = mock(Result.class);
+    Map<String, Collection<Object>> props1 = Maps.newHashMap();
+    Set<Result> results = Sets.newHashSet(result1);
+    when(result1.getProperties()).thenReturn(props1);
+    when(rs.getResultSetIterator()).thenReturn(results.iterator());
+
+    Map<String, String> propertiesMap = Maps.newHashMap();
+
+    provider.loadUserProperties(request, propertiesMap);
+
+    assertEquals(
+        "AND (-name:(group1) AND (taguuid:(" + ClientUtils.escapeQueryChars("123-456")
+            + ") OR title:(" + ClientUtils.escapeQueryChars("Group 1 Test") + ")))",
+        propertiesMap.get("_groupQuery"));
+  }
+
+  @Test
+  public void oneGroupTwoContactGroups() {
+    Result result1 = mock(Result.class);
+    Map<String, Collection<Object>> props1 = Maps.newHashMap();
+    props1.put("group", Sets.newHashSet((Object) "group1", "group2", "group3"));
+    Set<Result> results = Sets.newHashSet(result1);
+    when(result1.getProperties()).thenReturn(props1);
+    when(rs.getResultSetIterator()).thenReturn(results.iterator());
+
+    Map<String, String> propertiesMap = Maps.newHashMap();
+
+    provider.loadUserProperties(request, propertiesMap);
+    // AND (name:(group3 OR group2) OR (-name:(group1) AND (taguuid:(123\-456) OR title:(Group\ 1\ Test))))
+    String groupQuery = propertiesMap.get("_groupQuery");
+    assertTrue(groupQuery.startsWith("AND (name:(group"));
+    assertTrue(groupQuery.endsWith(") OR (-name:(group1) AND (taguuid:("
+        + ClientUtils.escapeQueryChars("123-456") + ") OR title:("
+        + ClientUtils.escapeQueryChars("Group 1 Test") + "))))"));
+
+    // can't check the actual string because the order is not guaranteed
+    String contactGroups = groupQuery.substring(11, 27);
+    assertFalse(contactGroups.contains("group1"));
+    assertTrue(contactGroups.contains("group2"));
+    assertTrue(contactGroups.contains("group3"));
+  }
+}
