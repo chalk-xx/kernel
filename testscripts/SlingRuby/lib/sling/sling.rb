b83a39cf28088fbf418236fa6885bfcc930f72e5
Ray Davis <ray@media.berkeley.edu>
2011-07-20 13:40:59 -0700

    KERN-2078 Do not lose authentication on redirect.

--- testscripts/SlingRuby/lib/sling/sling.rb
+++ testscripts/SlingRuby/lib/sling/sling.rb
@@ -279,7 +279,13 @@ module SlingInterface
             url = URI.parse(res.header['location'])
             @log.info("Redirecting to #{url}")
             req = Net::HTTP::Get.new(url.request_uri())
-            res = createHttp(url).start {|http|  http.request(req) } 
+            # Cookie-based authn should have happened before the redirect.
+            if (!@trustedauth )
+              @user.do_request_auth(req)
+            end
+            set_cookies(req)
+            res = createHttp(url).start { |http| http.request(req) }
+            save_cookies(res)
         end
         return res
     end
