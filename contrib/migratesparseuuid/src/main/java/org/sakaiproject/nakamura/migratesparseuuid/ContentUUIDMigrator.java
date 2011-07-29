a3f2aa654f49698180f5318821d88d355adfa0c2
Erik Froese <erik@nyu.edu>
2011-07-29 14:35:02 -0400

    Use the StorageClient to directly update the content properties. The ContentManagerImpl prevents you from setting the _sparseId field directly.

--- contrib/migratesparseuuid/src/main/java/org/sakaiproject/nakamura/migratesparseuuid/ContentUUIDMigrator.java
+++ contrib/migratesparseuuid/src/main/java/org/sakaiproject/nakamura/migratesparseuuid/ContentUUIDMigrator.java
@@ -20,6 +20,7 @@ import org.sakaiproject.nakamura.api.lite.content.Content;
 import org.sakaiproject.nakamura.api.lite.content.ContentManager;
 import org.sakaiproject.nakamura.api.solr.SolrServerService;
 import org.sakaiproject.nakamura.lite.content.InternalContent;
+import org.sakaiproject.nakamura.lite.storage.StorageClientPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,9 +28,9 @@ import org.slf4j.LoggerFactory;
 public class ContentUUIDMigrator {
 
 	private static final Logger log = LoggerFactory.getLogger(ContentUUIDMigrator.class);
-	
+
 	private static final String OLD_UUID_FIELD = "_id";
-	
+
 	private final int PAGE_SIZE = 25;
 
 	@Reference
@@ -38,27 +39,30 @@ public class ContentUUIDMigrator {
 	@Reference
 	SolrServerService solrServerService;
 
+	@Reference
+	StorageClientPool storageClientPool;
+
 	@Activate
 	public void activate(Map<String,Object> props){
 		migrateUUIDs();
 	}
 
 	/**
-	 * 
+	 *
 	 * This method iterates over all of the sakai content items and renames the _id field
 	 * to _sparseId field by copying _id => _sparseId and deleting the _id field.
-	 * 
+	 *
 	 * {@link InternalContent} defines a constant for the content object's uuid field.
-	 * 
-	 * Originally that field was "_id". This clashed with MongoDB since every Mongo object has 
-	 * an _id field. Sparse tries to decouple the object id from the id that the underlying 
-	 * storage mechanism uses. 
-	 * 
+	 *
+	 * Originally that field was "_id". This clashed with MongoDB since every Mongo object has
+	 * an _id field. Sparse tries to decouple the object id from the id that the underlying
+	 * storage mechanism uses.
+	 *
 	 * See {@link http://www.mongodb.org/display/DOCS/Object+IDs}
-	 * 
+	 *
 	 * This is meant for systems running sparsemapcontent with a JDBC or Cassandra driver.
 	 * If you don't run this after upgrading sparsemapcontent you won't be able to see any
-	 * content in the system. 
+	 * content in the system.
 	 */
 	public void migrateUUIDs(){
 		Session session;
@@ -78,7 +82,7 @@ public class ContentUUIDMigrator {
 			QueryResponse response = server.query(query);
 		    long totalResults = response.getResults().getNumFound();
 		    log.info("Attempting to migrate {} content items.", totalResults);
-		    
+
 		    while (start < totalResults){
 		        query.setStart(start);
 		        SolrDocumentList resultDocs = response.getResults();
@@ -88,15 +92,16 @@ public class ContentUUIDMigrator {
 		            	continue;
 		            }
 		            Content content = cm.get(id);
-		            String oldUUID = (String)content.getProperty(OLD_UUID_FIELD);
-		            if (oldUUID != null){
-		            	content.setProperty(InternalContent.UUID_FIELD, oldUUID);
-		            	content.removeProperty(OLD_UUID_FIELD);
-		            	cm.update(content);
+		            String contentUUID = (String)content.getProperty(OLD_UUID_FIELD);
+		            if (contentUUID != null){
+                        content.setProperty(InternalContent.UUID_FIELD, contentUUID);
+                        content.removeProperty(OLD_UUID_FIELD);
+                        storageClientPool.getClient().insert("n", "cn", contentUUID, content.getPropertiesForUpdate(), false);
+                        log.debug("Updated {} ", contentUUID);
 		            }
 		        }
 		        start += resultDocs.size();
-		        log.debug("Processed {} of {}.", resultDocs.size(), totalResults);
+                log.debug("Processed {} of {}.", resultDocs.size(), totalResults);
 		    }
 			session.logout();
 
